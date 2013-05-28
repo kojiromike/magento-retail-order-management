40829c9df30e05ee3f92d23c0ce544c61ab99dd0
Michael Phang <phangm@trueaction.com>
2013-05-28 11:32:08 -0400

    changed the directory structure.

--- src/app/code/community/TrueAction/Eb2c/Tax/Helper/Data.php
+++ src/app/code/community/TrueAction/Eb2c/Tax/Helper/Data.php
@@ -2,10 +2,10 @@
 /**
  * replacement for the default magento tax helper.
  */
-class TrueAction_Taxes_Helper_Data extends Mage_Tax_Helper_Data
+class TrueAction_Eb2c_Tax_Helper_Data extends Mage_Tax_Helper_Data
 {
 	public function getShippingPrice($price, $includingTax = null, $shippingAddress = null, $ctc = null, $store = null)
 	{
-
+		return 500.00;
 	}
 }

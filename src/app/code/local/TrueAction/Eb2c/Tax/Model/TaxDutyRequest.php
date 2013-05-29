9037b871938af7128372cea4e62bb071d71a3afb
Michael Phang <phangm@trueaction.com>
2013-05-29 16:13:07 -0400

    changed the taxdutyrequest model to setup the first level of the xml request in the mage constructor.

--- src/app/code/local/TrueAction/Eb2c/Tax/Model/TaxDutyRequest.php
+++ src/app/code/local/TrueAction/Eb2c/Tax/Model/TaxDutyRequest.php
@@ -9,6 +9,9 @@ class TrueAction_Eb2c_Tax_Model_TaxDutyRequest
 	protected $_operation           = 'quote';
 	protected $_responseFormat      = 'xml';
 
+	protected $_xml                 = null;
+	protected $_shipGroups          = null;
+
 	protected function _construct()
 	{
 		$this->setApiUrl(sprintf(
@@ -17,7 +20,25 @@ class TrueAction_Eb2c_Tax_Model_TaxDutyRequest
 			$this->_region,
 			$this->_version,
 			$this->getStoreId(),
-
+			$this->_service,
+			$this->_operation,
+			$this->_responseFormat
 		));
+		$address = $this->getShippingAddress();
+		$xml = new Varien_Simplexml_Element('<TaxDutyQuoteRequest/>');
+		$xml->setNode(
+			'Currency',
+			$this->getShippingAddress()->getQuote()->getCurrencyCode()
+		)
+		$xml->setNode('BillingInformation',	'');
+			->getNode('BillingInformation')
+			->addAttribute(
+				'ref',
+				$this->getBillingAddress()->getId()
+			);
+		$this->_shipGroups = $xml->setNode('Shipping/ShipGroups', '')
+			->getNode('Shipping/ShipGroups');
+		$this->_xml = $xml;
+	}
 	}
 }

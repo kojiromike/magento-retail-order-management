89315a767d94989786584ce4c813720c1a12c8ed
Mike West <westm@trueaction.com>
2013-06-03 12:22:01 -0500

    Skeletal structure of the Item Interfaces for Quotes and Orders in Eb2c. Unit testing assures that correct objects are instantiated; also verifies 'enforcement' of the interface's abstract method.

---
+++ src/app/code/local/TrueAction/Eb2c/Core/Model/Quote/Item.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Mage_Sales_Quote_Item extended for Eb2c.
+ *
+ */
+class TrueAction_Eb2c_Core_Model_Quote_Item extends Mage_Sales_Model_Quote_Item implements TrueAction_Eb2c_Core_Model_Item_Interface
+{
+	/**
+	 * Return a dummy value to prove configuration, code and implementation skeleton are working.
+	 */
+	public function getBySku()
+	{
+		return 'All this does is prove this framework exists and can implement the Interface\n';
+	}
+}

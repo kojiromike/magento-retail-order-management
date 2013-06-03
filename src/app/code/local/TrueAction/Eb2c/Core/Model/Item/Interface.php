361071d3b581575ca29d5e52ba77348ea0130374
Mike West <westm@trueaction.com>
2013-06-03 12:27:24 -0500

    Fix comment to specify that we deal with Quote-Address-Items - fairly confident that there's no such thing as an Order-Address-Item. It's only in a quote context that an item to address relationship means anything: is it taxable, how will it ship there, etc.

--- src/app/code/local/TrueAction/Eb2c/Core/Model/Item/Interface.php
+++ src/app/code/local/TrueAction/Eb2c/Core/Model/Item/Interface.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * An interface to make item access consistent across various contexts including Orders, Quotes and Addresses
+ * An interface to make item access consistent across various contexts including Orders, Quotes and Quote Addresses
  *
  */
 interface TrueAction_Eb2c_Core_Model_Item_Interface

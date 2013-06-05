d045102839fcbcfcde912a56f607a95eacd53d29
Reginald Gabriel <gabrielr@trueaction.com>
2013-06-05 14:27:28 -0500

    EBC-7: implementing apiCall universal method that take request xml object, api url and return reponse call

--- src/app/code/local/TrueAction/Eb2c/Core/Helper/Data.php
+++ src/app/code/local/TrueAction/Eb2c/Core/Helper/Data.php
@@ -1,8 +1,29 @@
 <?php
 /**
- * 
+ * @category   TrueAction
+ * @package    TrueAction_Eb2c
+ * @copyright  Copyright (c) 2013 True Action Network (http://www.trueaction.com)
  */
-class TrueAction_Eb2c_Core_Helper_Data extends Mage_Core_Helper_Abstract 
+class TrueAction_Eb2c_Core_Helper_Data extends Mage_Core_Helper_Abstract
 {
+	/**
+	 * take a request, api url, default signature 'post' and soap curl request to eb2c webservice.
+	 *
+	 * @param $request - TrueAction_Dom_Document
+	 * @param $apiUri - string
+	 * @param $signature - string
+	 *
+	 * @return $reponse - xml
+	 */
+	public function apiCall(TrueAction_Dom_Document $request, $apiUri, $signature='POST')
+	{
+		$client = new Varien_Http_Client($apiUri);
+		if (!function_exists('curl_init')) {
+			// curl isn't install in the server, let's use Socket
+			$client->setAdapter(new Zend_Http_Client_Adapter_Socket());
+		}
+		$client->setRawData($request->saveXML(), 'text/xml');
 
+		return $client->request($signature);
+	}
 }
---
+++ src/app/code/local/TrueAction/Eb2c/Core/Test/Helper/DataTest.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @category  TrueAction
+ * @package   TrueAction_Eb2c
+ * @copyright Copyright (c) 2013 True Action (http://www.trueaction.com)
+ */
+class TrueAction_Eb2c_Core_Test_Helper_DataTest extends EcomDev_PHPUnit_Test_Case
+{
+	protected $_helper;
+
+	/**
+	 * setUp method
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+		$this->_helper = $this->_getHelper();
+		Mage::app()->getConfig()->reinit(); // re-initialize config to get fresh loaded data
+	}
+
+	/**
+	 * Get Dom instantiated object.
+	 *
+	 * @return TrueAction_Dom_Document
+	 */
+	public function getDomDocument()
+	{
+		return new TrueAction_Dom_Document('1.0', 'UTF-8');
+	}
+
+	/**
+	 * Get helper instantiated object.
+	 *
+	 * @return TrueAction_Eb2c_Core_Helper_Data
+	 */
+	protected function _getHelper()
+	{
+		if (!$this->_helper) {
+			$this->_helper = Mage::helper('eb2ccore');
+		}
+		return $this->_helper;
+	}
+
+	public function providerApiCall()
+	{
+		$domDocument = $this->getDomDocument();
+		$quantityRequestMessage = $domDocument->addElement('QuantityRequestMessage', null, 'http://api.gsicommerce.com/schema/checkout/1.0')->firstChild;
+		$quantityRequestMessage->createChild(
+			'QuantityRequest',
+			null,
+			array('lineId' => 1, 'itemId' => 'SKU-1234')
+		);
+		$quantityRequestMessage->createChild(
+			'QuantityRequest',
+			null,
+			array('lineId' => 2, 'itemId' => 'SKU-4321')
+		);
+		return array(
+			array(
+				$domDocument, 'http://eb2c.edge.mage.tandev.net/GSI%20eb2c%20Web%20Service%20Schemas%20v1.0/Inventory-Service-Quantity-1.0.xsd'
+			)
+		);
+	}
+
+	/**
+	 * testing apiCall method
+	 *
+	 * @test
+	 * @dataProvider providerApiCall
+	 */
+	public function testApiCall($request, $apiUri, $signature='POST')
+	{
+		$this->assertNotEmpty(
+			$this->_getHelper()->apiCall($request, $apiUri)
+		);
+	}
+}

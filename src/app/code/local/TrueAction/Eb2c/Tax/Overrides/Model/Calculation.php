6bfbac7ec4d944591284eefc1f9bae8cb63acbb6
Michael Phang <phangm@trueaction.com>
2013-06-12 13:30:35 -0400

    added docblock and updated the function signature.

--- src/app/code/local/TrueAction/Eb2c/Tax/Overrides/Model/Calculation.php
+++ src/app/code/local/TrueAction/Eb2c/Tax/Overrides/Model/Calculation.php
@@ -5,10 +5,18 @@
  */
 class TrueAction_Eb2c_Tax_Overrides_Model_Calculation extends Mage_Tax_Model_Calculation
 {
+	/**
+	 * generate a request object to use when calculating taxes and duties.
+	 * @param  Mage_Sale_Model_Quote_Addres $shippingAddress
+	 * @param  Mage_Sale_Model_Quote_Addres $billingAddress
+	 * @param  string                       $customerTaxClass
+	 * @param  int|Mage_Core_Model_Store    $store
+	 * @return TrueAction_Eb2c_Tax_Model_TaxDutyRequest
+	 */
 	public function getRateRequest(
-		$shippingAddress = null,
-		$billingAddress = null,
-		$customerTaxClass = null,
+		Mage_Sale_Model_Quote_Addres $shippingAddress = null,
+		Mage_Sale_Model_Quote_Addres $billingAddress = null,
+		$customerTaxClass = '',
 		$store = null
 	) {
 		return new TrueAction_Eb2c_Tax_Model_TaxDutyRequest(array(

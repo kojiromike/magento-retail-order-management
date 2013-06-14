d0067447aedbc9ba6d6bb22375d27c3c9746ac10
Michael Phang <phangm@trueaction.com>
2013-06-13 21:16:09 -0400

    fixed _buildPersonName to use the correct getters.

--- src/app/code/local/TrueAction/Eb2c/Tax/Model/TaxDutyRequest.php
+++ src/app/code/local/TrueAction/Eb2c/Tax/Model/TaxDutyRequest.php
@@ -103,15 +103,15 @@ class TrueAction_Eb2c_Tax_Model_TaxDutyRequest extends Mage_Core_Model_Abstract
 	protected function _buildPersonName(TrueAction_Dom_Element $parent, Mage_Sales_Model_Quote_Address $address)
 	{
 		$honorific  = $address->getPrefix();
-		$middleName = $address->getMiddleName();
+		$middleName = $address->getMiddlename();
 		if ($honorific) {
 			$parent->createChild('Honorific', $honorific);
 		}
-		$parent->createChild('LastName', $address->getLastName());
+		$parent->createChild('LastName', $address->getLastname());
 		if ($middleName) {
 			$parent->createChild('MiddleName', $middleName);
 		}
-		$parent->createChild('FirstName', $address->getFirstName());
+		$parent->createChild('FirstName', $address->getFirstname());
 	}
 
 	/**

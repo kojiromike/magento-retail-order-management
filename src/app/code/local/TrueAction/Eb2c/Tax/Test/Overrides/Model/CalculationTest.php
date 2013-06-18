78bebc7d04814646e0109bb8042b583c350f6829
Michael Phang <phangm@trueaction.com>
2013-06-18 14:05:24 -0400

    updated the assertions and added a (non-working) test to verify the DOM using an xsd.

--- src/app/code/local/TrueAction/Eb2c/Tax/Test/Overrides/Model/CalculationTest.php
+++ src/app/code/local/TrueAction/Eb2c/Tax/Test/Overrides/Model/CalculationTest.php
@@ -29,10 +29,21 @@ class TrueAction_Eb2c_Tax_Test_Overrides_Model_CalculationTest extends EcomDev_P
 	 */
 	private $cls         = null;
 
+	/**
+	 * path to the xsd file to validate against.
+	 * @var string
+	 */
+	private static $xsdFile = '';
 
 	private $tdRequest    = null;
 	private $destinations = null;
 	private $shipGroups   = null;
+
+	public static function setUpBeforeClass()
+	{
+		self::$xsdFile = dirname(__FILE__) .
+			'/CalculationTest/fixtures/TaxDutyFee-QuoteRequest-1.0.xsd';
+	}
 
 	public function setUp()
 	{
@@ -62,13 +73,13 @@ class TrueAction_Eb2c_Tax_Test_Overrides_Model_CalculationTest extends EcomDev_P
 		$request = $calc->getRateRequest($shipAddress, $billaddress, 'someclass', null);
 		$doc = $request->getDocument();
 		$xpath = new DOMXPath($doc);
-		$node = $xpath->query('/TaxDutyRequest/Currency')->item(0);
+		$node = $xpath->query('/TaxDutyQuoteRequest/Currency')->item(0);
 		$this->assertSame('USD', $node->textContent);
 
-		$node = $xpath->query('/TaxDutyRequest/BillingInformation')->item(0);
-		$this->assertSame('dest_3', $node->getAttribute('ref'));
-		$parent = $xpath->query('/TaxDutyRequest/Shipping/Destinations/MailingAddress')->item(0);
-		$this->assertSame('dest_3', $parent->getAttribute('id'));
+		$node = $xpath->query('/TaxDutyQuoteRequest/BillingInformation')->item(0);
+		$this->assertSame('4', $node->getAttribute('ref'));
+		$parent = $xpath->query('/TaxDutyQuoteRequest/Shipping/Destinations/MailingAddress')->item(0);
+		$this->assertSame('4', $parent->getAttribute('id'));
 
 		// check the PersonName
 		$node = $xpath->query('PersonName/LastName', $parent)->item(0);
@@ -99,11 +110,29 @@ class TrueAction_Eb2c_Tax_Test_Overrides_Model_CalculationTest extends EcomDev_P
 		$this->assertSame('21229', $node->textContent);
 
 		// verify the email address
-		$parent = $xpath->query('/TaxDutyRequest/Shipping/Destinations')->item(0);
+		$parent = $xpath->query('/TaxDutyQuoteRequest/Shipping/Destinations')->item(0);
 		$node = $xpath->query('Email', $parent)->item(0);
-		$this->assertSame('dest_email_3', $node->getAttribute('id'));
+		$this->assertSame('foo@example.com', $node->getAttribute('id'));
 
 		$node = $xpath->query('Email/EmailAddress', $parent)->item(0);
 		$this->assertSame('foo@example.com', $node->textContent);
+		print $doc->saveXML();
+	}
+
+	/**
+	 * @test
+	 * @loadFixture base.yaml
+	 * @loadFixture testGetRateRequest.yaml
+	 */
+	public function testGetRateRequestXsd()
+	{
+		$quote = Mage::getModel('sales/quote')->loadByIdWithoutStore(2);
+		$shipAddress = $quote->getShippingAddress();
+		$billaddress = $quote->getBillingAddress();
+
+		$calc = new TrueAction_Eb2c_Tax_Overrides_Model_Calculation();
+		$request = $calc->getRateRequest($shipAddress, $billaddress, 'someclass', null);
+		$doc = $request->getDocument();
+		$this->assertTrue($doc->schemaValidate(self::$xsdFile));
 	}
 }

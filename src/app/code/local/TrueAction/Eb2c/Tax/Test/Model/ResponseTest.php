e7157078cb00367b92f0c7527f4fd18e410b0c72
Michael Phang <phangm@trueaction.com>
2013-06-19 15:47:47 -0400

    changed the response test to setup and use a mock of the request object when testing.

--- src/app/code/local/TrueAction/Eb2c/Tax/Test/Model/ResponseTest.php
+++ src/app/code/local/TrueAction/Eb2c/Tax/Test/Model/ResponseTest.php
@@ -6,33 +6,48 @@ class TrueAction_Eb2c_Tax_Test_Model_ResponseTest extends EcomDev_PHPUnit_Test_C
 {
 	public static $_xml = '';
 	public static $cls;
+	public static $reqXml;
 	public $itemResults;
+	public $request;
 
 	public static function setUpBeforeClass()
 	{
 		self::$cls = new ReflectionClass(
 			'TrueAction_Eb2c_Tax_Model_Response'
 		);
-		$responseFile = dirname(__FILE__) .
-			'/ResponseTest/fixtures/response.xml';
-		$file = fopen($responseFile, 'r');
-		self::$_xml = fread($file, filesize($responseFile));
+		$path = dirname(__FILE__) . '/ResponseTest/fixtures/response.xml';
+		$file = fopen($path, 'r');
+		self::$_xml = fread($file, filesize($path));
 		fclose($file);
+		$path = dirname(__FILE__) . '/ResponseTest/fixtures/request.xml';
+		$file = fopen($path, 'r');
+		self::$reqXml = fread($file, filesize($path));
 	}
 
 	public function setUp()
 	{
 		$this->itemResults = self::$cls->getProperty('_itemResults');
 		$this->itemResults->setAccessible(true);
+		$reqDoc = new TrueAction_Dom_Document('1.0', 'utf8');
+		$reqDoc->loadXML($requestXml);
+		$request = $this->getModelMock(
+			'TrueAction_Eb2c_Tax_Model_TaxDutyRequest',
+			array('getDocument')
+		);
+		$request->expects($this->any())
+			->method('getDocument')
+			->will($this->returnValue($reqDoc));
+		$this->request = $request;
 	}
 
 	/**
 	 * @test
 	 */
-	public function runTests()
+	public function testConstruction()
 	{
 		$response = new TrueAction_Eb2c_Tax_Model_Response(array(
-			'xml' => self::$_xml
+			'xml' => self::$_xml,
+			'request' => $this->request
 		));
 		$this->assertSame(2, count($this->itemResults->getValue($response)));
 	}

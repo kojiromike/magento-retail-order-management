74434024f1fd769a9e219a209356f492e5ea7a43
Scott VanBrug <vanbrugs@trueaction.com>
2013-06-19 18:23:08 -0400

    declare the validation resonpse model and add an incomplete test

---
+++ src/app/code/local/TrueAction/Eb2c/Address/Model/Validation/Response.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Responsible for handling the AddressValidationResponse message from EB2C.
+ */
+class TrueAction_Eb2c_Address_Model_Validation_Response
+	extends Mage_Core_Model_Abstract
+{
+
+}

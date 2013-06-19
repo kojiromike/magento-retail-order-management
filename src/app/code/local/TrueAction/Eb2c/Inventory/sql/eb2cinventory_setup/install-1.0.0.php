77dd4a1998df90307594a7e73590e1344f87d81d
Mike West <westm@trueaction.com>
2013-06-19 15:59:40 -0400

    Eb2c Inventory fields added EAV Magento-approved style.

--- src/app/code/local/TrueAction/Eb2c/Inventory/sql/eb2cinventory_setup/install-1.0.0.php
+++ src/app/code/local/TrueAction/Eb2c/Inventory/sql/eb2cinventory_setup/install-1.0.0.php
@@ -1,66 +1,146 @@
 <?php
 /**
  * @category   TrueAction
- * @package    TrueAction_Eb2c
+ * @package    Eb2c_Inventory
  * @copyright  Copyright (c) 2013 True Action Network (http://www.trueaction.com)
  */
 
-$installer = $this;
+$installer = new Mage_Sales_Model_Resource_Setup('core_setup');
 $installer->startSetup();
 try{
-	/**
-	 * Create table 'eb2cinventory/details'
-	 */
-	$table = $installer->getConnection()
-		->newTable($installer->getTable('eb2cinventory/details'))
-		->addColumn('detail_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
-			'identity' => true,
-			'unsigned' => true,
-			'nullable' => false,
-			'primary' => true,
-		), 'Detail Id')
-		->addColumn('item_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
-			'unsigned' => true,
-		), 'Quote Item Id')
-		->addColumn('created_at', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
-		), 'Created At')
-		->addColumn('updated_at', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
-		), 'Updated At')
-		->addColumn('display', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
-		), 'eb2c display')
-		->addColumn('creation_time', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
-		), 'eb2c creation time')
-		->addColumn('delivery_window_from', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
-		), 'eb2c delivery window from')
-		->addColumn('delivery_window_to', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
-		), 'eb2c delivery window to')
-		->addColumn('shipping_window_from', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
-		), 'eb2c shipping window from')
-		->addColumn('shipping_window_to', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
-		), 'eb2c shipping window to')
-		->addColumn('ship_from_address_line1', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
-		), 'eb2c ship from address line1')
-		->addColumn('ship_from_address_city', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
-		), 'eb2c ship from address city')
-		->addColumn('ship_from_address_main_division', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
-		), 'eb2c ship from address main division')
-		->addColumn('ship_from_address_country_code', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
-		), 'eb2c ship from address country code')
-		->addColumn('ship_from_address_postal_code', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
-		), 'eb2c ship from address postal code')
-		->addColumn('reservation_id', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
-		), 'Reservation Id')
-		->addColumn('reservation_expires', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
-		), 'Reservation Expiration Date')
-		->addColumn('qty_reserved', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
-		), 'Reserved Qty')
-		->addForeignKey($installer->getFkName('eb2cinventory/details', 'item_id', 'sales/quote_item', 'item_id'),
-			'item_id', $installer->getTable('sales/quote_item'), 'item_id',
-		Varien_Db_Ddl_Table::ACTION_SET_NULL, Varien_Db_Ddl_Table::ACTION_CASCADE)
-		->setComment('Eb2c Inventory Details');
-	$installer->getConnection()->createTable($table);
+	$entities = array(
+		'order_item',
+		'quote_address_item',
+		'quote_item',
+	);
 
-	$installer->endSetup();
+	$eb2cinventory_attributes = array(
+		array(
+			'name'=>'reservation_id',		
+			'options'=> array (
+				'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+				'visible'=> true,
+				'required' => false,
+			)
+		),
+		array(
+			'name'=>'reservation_expires',	
+			'options'=> array (
+				'type' => Varien_Db_Ddl_Table::TYPE_DATETIME,
+				'visible'=> true,
+				'required' => false,
+			)
+		),
+		array(
+			'name'=>'qty_reserved',			
+			'options'=> array (
+				'type' => Varien_Db_Ddl_Table::TYPE_INTEGER,
+				'visible'=> true,
+				'required' => false,
+			)
+		),
+		array(
+			'name'=>'creation_time',	
+			'options'=> array (
+				'type' => Varien_Db_Ddl_Table::TYPE_DATETIME,
+				'visible'=> true,
+				'required' => false,
+			)
+		),
+		array(
+			'name'=>'display',	
+			'options'=> array (
+				'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+				'visible'=> true,
+				'required' => false,
+			)
+		),
+		array(
+			'name'=>'delivery_window_from',	
+			'options'=> array (
+				'type' => Varien_Db_Ddl_Table::TYPE_DATETIME,
+				'visible'=> true,
+				'required' => false,
+			)
+		),
+		array(
+			'name'=>'delivery_window_to',	
+			'options'=> array (
+				'type' => Varien_Db_Ddl_Table::TYPE_DATETIME,
+				'visible'=> true,
+				'required' => false,
+			)
+		),
+		array(
+			'name'=>'shipping_window_from',	
+			'options'=> array (
+				'type' => Varien_Db_Ddl_Table::TYPE_DATETIME,
+				'visible'=> true,
+				'required' => false,
+			)
+		),
+		array(
+			'name'=>'shipping_window_to',	
+			'options'=> array (
+				'type' => Varien_Db_Ddl_Table::TYPE_DATETIME,
+				'visible'=> true,
+				'required' => false,
+			)
+		),
+		array(
+			'name'=>'ship_from_address_line_1',		
+			'options'=> array (
+				'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+				'visible'=> true,
+				'required' => false,
+			)
+		),
+		array(
+			'name'=>'ship_from_address_city',		
+			'options'=> array (
+				'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+				'visible'=> true,
+				'required' => false,
+			)
+		),
+		array(
+			'name'=>'ship_from_address_main_division',		
+			'options'=> array (
+				'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+				'visible'=> true,
+				'required' => false,
+			)
+		),
+		array(
+			'name'=>'ship_from_address_country_code',		
+			'options'=> array (
+				'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+				'visible'=> true,
+				'required' => false,
+			)
+		),
+		array(
+			'name'=>'ship_from_address_postal_code',		
+			'options'=> array (
+				'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+				'visible'=> true,
+				'required' => false,
+			)
+		),
+	);
+
+	$pfx = 'eb2c_';
+
+	foreach ($entities as $entity) {
+		foreach( $eb2cinventory_attributes as $a) {
+			$installer->addAttribute(
+				$entity, 
+				$pfx.$a['name'],
+				$a['options']
+			);
+		}
+	}
 } catch (Exception $e) {
 	Mage::logException($e);
 }
+$installer->endSetup();

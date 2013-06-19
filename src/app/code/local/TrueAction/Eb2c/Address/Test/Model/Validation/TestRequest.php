9f6ce89be1314b522f425f481887374880571641
Scott VanBrug <vanbrugs@trueaction.com>
2013-06-19 18:22:35 -0400

    flush out the request model and accompanying tests. give the model methods for generating the request message xml and the api uri, essentially what is necessary to send the request. add incomplete tests methods for the two public methods on the model, not yet sure best means of testing this yet.

--- src/app/code/local/TrueAction/Eb2c/Address/Test/Model/Validation/TestRequest.php
+++ src/app/code/local/TrueAction/Eb2c/Address/Test/Model/Validation/TestRequest.php
@@ -4,65 +4,85 @@
  * Test the generation of the xml request to the EB2C address validation service
  */
 
-class AddressStub extends Mage_Customer_Model_Address_Abstract {
-	public function getStreetFull()
-	{
-		return array('123 Main St', 'Ste 6', 'Next to Gas Station', 'Across from Post Office');
-	}
-	public function getCity()
-	{
-		return 'King of Prussia';
-	}
-	public function getRegionCode()
-	{
-		return 'PA';
-	}
-	public function getCountry()
-	{
-		'US';
-	}
-	public function getPostcode()
-	{
-		'19406';
-	}
-}
-
-class QuoteStub {
-	protected $_shippingAddress;
-
-	public function __construct() {
-		$this->_shippingAddress = new AddressStub();
-	}
-
-	public function getShippingAddress()
-	{
-		return $this->_shippingAddress;
-	}
-
-}
-
-class TrueAction_Eb2c_Address_Test_Model_Validate_Request extends EcomDev_PHPUnit_Test_Case
+class TrueAction_Eb2c_Address_Test_Model_Validate_TestRequest
+	extends EcomDev_PHPUnit_Test_Case
 {
 
+	/**
+	 * Parts of an address to use when building out the address request
+	 */
+	protected $_addressParts = array(
+		'line1' => '123 Main St',
+		'line2' => 'STE 6',
+		'line3' => 'Foo',
+		'line4' => 'Bar',
+		'city' => 'Auburn',
+		'region_id' => '51',
+		'region_code' => 'PA',
+		'country_id' => 'US',
+		'postcode' => '13021',
+	);
+
+	/**
+	 * Create a Mage_Customer_Model_Address_Abstract stub for testing
+	 */
+	protected function _createAddressStub()
+	{
+		$parts = $this->_addressParts;
+
+		$stub = $this->getMock('Mage_Customer_Model_Address_Abstract',
+			array('getStreet', 'getCity', 'getRegionCode', 'getCountry', 'getPostcode'));
+
+		$stub->expects($this->any())
+			->method('getStreet')
+			->will($this->returnValue(
+				array($parts['line1'], $parts['line2'], $parts['line3'], $parts['line4'])
+			));
+		$stub->expects($this->any())
+			->method('getCity')
+			->will($this->returnValue($parts['city']));
+		$stub->expects($this->any())
+			->method('getRegionCode')
+			->will($this->returnValue($parts['region_code']));
+		$stub->expects($this->any())
+			->method('getCountry')
+			->will($this->returnValue($parts['country_id']));
+		$stub->expects($this->any())
+			->method('getPostcode')
+			->will($this->returnValue($parts['postcode']));
+		return $stub;
+	}
+
+	/**
+	 * Create a TrueAction_Eb2c_Address_Model_Validation_Request to test against.
+	 * Ensures that all of the necessary setup of the object is done.
+	 */
 	protected function _createRequest()
 	{
 		$request = Mage::getModel('eb2caddress/validation_request');
-		$request->setQuote(new QuoteStub());
+		$address = $this->_createAddressStub();
+		$request->setAddress($address);
 		return $request;
 	}
 
 	/**
 	 * @test
-	 * @loadFixtures requestConfig
+	 * @loadFixture requestConfig
 	 */
-	public function testRequestHeader()
+	public function testRequestApiUri()
 	{
 		$request = $this->_createRequest();
-		$request->createRequestMessage();
-		$this->assertTrue($header->hasChildNodes());
-		$this->assertEquals($header->childNodes->length, 1);
-		$this->assertEquals($header->firstChild->tagName, 'MaxAddress');
-		$this->assertEquals($header->firstChild->nodeValue, 5);
+		$this->markTestSkipped('Not sure of exact spec for this functionality. Need to define where exactly each part of the API path should be coming from: class consts, module settings, core settings, etc.');
+	}
+
+	/**
+	 * @test
+	 */
+	public function testRequestMessage()
+	{
+		$request = $this->_createRequest();
+		$message = $request->getMessage();
+		$this->markTestSkipped('Need to determine a sane way of actually testing the XML generation.');
 	}
 
 }

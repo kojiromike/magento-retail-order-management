ae1a2a8def9adc523b6b66cca090b944fe55a5b0
Michael Phang <phangm@trueaction.com>
2013-06-19 15:34:01 -0400

    added _validateResponseItem and _checkPathValues to check each item from the response updated.
    added docblocks.

--- src/app/code/local/TrueAction/Eb2c/Tax/Model/Response.php
+++ src/app/code/local/TrueAction/Eb2c/Tax/Model/Response.php
@@ -16,6 +16,12 @@ class TrueAction_Eb2c_Tax_Model_Response extends Mage_Core_Model_Abstract
 	 */
 	protected $_itemResults = array();
 
+	/**
+	 * skus of OrderItem elements that passed validation
+	 * @var array(string)
+	 */
+	protected $_validSkus = array();
+
 	protected function _construct()
 	{
 		$this->_doc = new TrueAction_Dom_Document('1.0', 'utf8');
@@ -23,11 +29,20 @@ class TrueAction_Eb2c_Tax_Model_Response extends Mage_Core_Model_Abstract
 		$this->_parseResults();
 	}
 
+	/**
+	 * get the result records of the request
+	 * @return array(TrueAction_Eb2c_Tax_Model_Tax)
+	 */
 	public function getResults()
 	{
 		return $this->_itemResults;
 	}
 
+	/**
+	 * get the address using the value from the ref attribute.
+	 * @param  string $idRef
+	 * @return Mage_Sales_Model_Quote_Address
+	 */
 	protected function _getAddress($idRef)
 	{
 		$address = Mage::getModel('sales/quote_address')->load($idRef);
@@ -40,8 +55,22 @@ class TrueAction_Eb2c_Tax_Model_Response extends Mage_Core_Model_Abstract
 		return $address;
 	}
 
-	protected function _createTaxRecord($type, $amount, TrueAction_Dom_Element $tax, $address, $quoteItem)
-	{
+	/**
+	 * create a Tax record using data extracted from the arguments
+	 * @param  int                            $type       @see TrueAction_Eb2c_Tax_Model_Tax
+	 * @param  string                         $amount
+	 * @param  TrueAction_Dom_Element         $tax
+	 * @param  Mage_Sales_Model_Quote_Address $address
+	 * @param  Mage_Sales_Model_Quote_Item    $quoteItem
+	 * @return TrueAction_Eb2c_Tax_Model_Tax
+	 */
+	protected function _createTaxRecord(
+		$type,
+		$amount,
+		TrueAction_Dom_Element $tax,
+		Mage_Sales_Model_Quote_Address $address,
+		Mage_Sales_Model_Quote_Item $quoteItem
+	) {
 		$xpath = new DOMXPath($tax->ownerDocument);
 		$record = new TrueAction_Eb2c_Tax_Model_Tax(array(
 			'type' => $type,
@@ -62,14 +91,10 @@ class TrueAction_Eb2c_Tax_Model_Response extends Mage_Core_Model_Abstract
 		$this->_itemResults[] = $record;
 	}
 
-	protected function _getQuoteItem(TrueAction_Dom_Element $item)
-	{
-		// get item sku
-		// get Quote_Item
-		// verify line number
-	}
-
-	protected function _parseResults()
+	/**
+	 * generate tax quote records with data extracted from the response.
+	 */
+	protected function _extractResults()
 	{
 		$xpath = new DOMXPath($this->_doc);
 		$mailingAddresses = $xpath->query(
@@ -114,5 +139,114 @@ class TrueAction_Eb2c_Tax_Model_Response extends Mage_Core_Model_Abstract
 		}
 		// foreach destination
 		// verify data
+	}
+
+	/**
+	 * compare an OrderItem element with the corresponding element in the request
+	 * to make sure we got back what we sent.
+	 * throw a mismatch error exception if a required field does not match what was sent.
+	 * log a debug message for any optional data that does not match.
+	 * @param  TrueAction_Dom_Element $itemNode OrderItem element from the request
+	 * @return string                           the OrderItem's ItemId value (sku)
+	 * @throws TrueAction_Eb2c_Tax_Model_Response_MismatchError
+	 */
+	protected function _validateResponseItem(TrueAction_Dom_Element $itemNode)
+	{
+		$requestDoc = $this->getRequest()->getDocument();
+		$requestXpath = new DOMXPath($requestDoc);
+		$xpath = new DOMXPath($this->_doc);
+		$sku = $xpath->evaluate('ItemId/text()', $itemNode);
+		$reqNode = $requestXpath->query('//OrderItem/ItemId[.=' . $sku)->items(0);
+		$isValid = true;
+		// if we get back an order item we didn't send log it as a debug message and
+		// ignore it.
+		if (!$reqNode) {
+			$isValid = false;
+			Mage::log(
+				sprintf('%s: sku "%s" not found in the request.', 'TaxDutyQuoteResponse', $sku),
+				Zend_Log::DEBUG
+			);
+			$sku = '';
+		} else {
+			$path = 'Quantity';
+			if (!$this->_checkPathValues($path, $xpath, $itemNode, $requestXpath, $reqNode, true)) {
+				$isValid = false;
+				throw new TrueAction_Eb2c_Tax_Model_Response_Mismatch("$sku @ $path");
+			}
+			$path = 'Pricing/Merchandise/UnitPrice';
+			if (!$this->_checkPathValues($path, $xpath, $itemNode, $requestXpath, $reqNode, true)) {
+				$isValid = false;
+				throw new TrueAction_Eb2c_Tax_Model_Response_Mismatch("$sku @ $path");
+			}
+			$path = 'Pricing/Shipping/Amount';
+			if (!$this->_checkPathValues($path, $xpath, $itemNode, $requestXpath, $reqNode, true)) {
+				$isValid = false;
+				throw new TrueAction_Eb2c_Tax_Model_Response_Mismatch("$sku @ $path");
+			}
+			if ($reqNode->getAttribute('LineNumber') !== $itemNode->getAttribute('LineNumber'))
+			{
+				Mage::log(
+					sprintf(
+						'%s: %s "%s" does not match request "%s"',
+						'TaxDutyQuoteResponse',
+						'LineNumber',
+						$resValue,
+						$reqValue
+					),
+					Zend_Log::WARN
+				);
+			}
+			$path = 'ItemDesc';
+			$isValid &= $this->_checkPathValues($path, $xpath, $itemNode, $requestXpath, $reqNode);
+			$path = 'HTSCode';
+			$isValid &= $this->_checkPathValues($path, $xpath, $itemNode, $requestXpath, $reqNode);
+			$path = 'Pricing/Merchandise/Amount';
+			$isValid &= $this->_checkPathValues($path, $xpath, $itemNode, $requestXpath, $reqNode);
+		}
+		if ($isValid) {
+			$this->_validSkus[] = $sku;
+		} else {
+			$sku = '';
+		}
+		return $sku;
+	}
+
+	/**
+	 * compare the value of an element in the response document to an element in the
+	 * request document.
+	 * @param  string  $path
+	 * @param  [type]  $xpath      [description]
+	 * @param  [type]  $itemNode   [description]
+	 * @param  [type]  $reqXpath   [description]
+	 * @param  [type]  $reqNode    [description]
+	 * @param  boolean $isRequired [description]
+	 * @return [type]              [description]
+	 */
+	protected function _checkPathValues(
+		$path,
+		$xpath,
+		$itemNode,
+		$reqXpath,
+		$reqNode,
+		$isRequired = false
+	) {
+		$resValue = $xpath->evaluate($path . '/text()', $itemNode);
+		$reqValue = $reqXpath->evaluate($path . '/text()', $reqNode);
+		$isMatching = true;
+		if ($resValue !== $reqValue) {
+			$isMatching = false;
+			$message = sprintf(
+				'TaxDutyQuoteResponse: %s "%s" does not match request "%s"',
+				$path,
+				$resValue,
+				$reqValue
+			);
+			if ($isRequired) {
+				Mage::log($message, Zend_Log::WARN);
+			} else {
+				Mage::log($message, Zend_Log::DEBUG);
+			}
+		}
+		return $isMatching;
 	}
 }

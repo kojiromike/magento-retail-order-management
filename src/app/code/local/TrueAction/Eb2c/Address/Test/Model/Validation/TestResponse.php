2fb55e2655f0fa95cd75bd75fbcbae851ff9dbd3
Scott VanBrug <vanbrugs@trueaction.com>
2013-06-19 18:23:08 -0400

    declare the validation resonpse model and add an incomplete test

---
+++ src/app/code/local/TrueAction/Eb2c/Address/Test/Model/Validation/TestResponse.php
@@ -0,0 +1,12 @@
+<?php
+
+class TrueAction_Eb2c_Address_Test_Model_Validation_TestResponse
+	extends EcomDev_PHPUnit_Test_Case
+{
+
+	public function testProcessResponse()
+	{
+		$this->markTestIncomplete('Not yet implement.');
+	}
+
+}

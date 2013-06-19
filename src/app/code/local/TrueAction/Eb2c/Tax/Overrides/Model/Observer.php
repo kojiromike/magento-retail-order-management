ef718aa4176fd9be7c248dd40ffd7c36d35a57fe
Michael Phang <phangm@trueaction.com>
2013-06-19 15:51:13 -0400

    added an override observer to react to the cart's contents being manipulated.

---
+++ src/app/code/local/TrueAction/Eb2c/Tax/Overrides/Model/Observer.php
@@ -0,0 +1,53 @@
+<?php
+class TrueAction_Eb2c_Tax_Overrides_Model_Observer extends Mage_Tax_Model_Observer
+{
+	public function salesEventItemAdded(Varien_Event_Observer $observer)
+	{
+		Mage::log('salesEventItemAdded');
+		$quote = $observer->getEvent()->getQuoteItem()->getQuote();
+		$this->_fetchTaxDutyInfo($quote);
+	}
+
+	public function cartEventProductUpdated(Varien_Event_Observer $observer)
+	{
+		Mage::log('cartEventProductUpdated');
+		$quote = $observer->getEvent()->getQuoteItem()->getQuote();
+		$this->_fetchTaxDutyInfo($quote);
+	}
+
+	public function salesEventItemRemoved(Varien_Event_Observer $observer)
+	{
+		Mage::log('salesEventItemRemoved');
+		$quote = $observer->getEvent()->getQuoteItem()->getQuote();
+		$this->_fetchTaxDutyInfo($quote);
+	}
+
+	protected function _fetchTaxDutyInfo($quote)
+	{
+		$request = Mage::helper('tax')->getCalculator()
+			->getRateRequest(
+				$quote->getBillingAddress(),
+				$quote->getShippingAddress(),
+				'', null
+			);
+		$response = Mage::helper('tax')->sendRequest($request);
+	}
+
+    /**
+     * Reset extra tax amounts on quote addresses before recollecting totals
+     *
+     * @param Varien_Event_Observer $observer
+     * @return Mage_Tax_Model_Observer
+     */
+    public function quoteCollectTotalsBefore(Varien_Event_Observer $observer)
+    {
+		Mage::log('quoteCollectTotalsBefore');
+        /* @var $quote Mage_Sales_Model_Quote */
+        $quote = $observer->getEvent()->getQuote();
+        foreach ($quote->getAllAddresses() as $address) {
+            $address->setExtraTaxAmount(0);
+            $address->setBaseExtraTaxAmount(0);
+        }
+        return $this;
+    }
+}

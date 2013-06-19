d58131348ea58a84bc43c2b6c36973a47ca32352
Scott VanBrug <vanbrugs@trueaction.com>
2013-06-19 18:18:50 -0400

    config model for address validation store configs

--- src/app/code/local/TrueAction/Eb2c/Address/Model/Config.php
+++ src/app/code/local/TrueAction/Eb2c/Address/Model/Config.php
@@ -3,9 +3,9 @@
 /**
  * Configuration model to be registered with the eb2c core config helper.
  */
-class TrueAction_Eb2c_Address_Model_Config extends TrueAction_Eb2c_Core_Model_Config_Abstract
+class TrueAction_Eb2c_Address_Model_Config
+	extends TrueAction_Eb2c_Core_Model_Config_Abstract
 {
-
 	protected $_configPaths = array(
 		'max_address_suggestions' => 'eb2c/address_validation/max_suggestions',
 	);

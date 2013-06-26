33d7db69b8274afefbb73ca5a7d5053ecd86e1f3
Michael Phang <phangm@trueaction.com>
2013-06-26 08:26:09 -0400

    rewrote the calculator so that the totaling models do not need to handle the tax response data directly.
    the tax response is now stored in the checkout session.

--- src/app/code/local/TrueAction/Eb2c/Tax/Overrides/Model/Calculation.php
+++ src/app/code/local/TrueAction/Eb2c/Tax/Overrides/Model/Calculation.php
@@ -1,77 +1,113 @@
 <?php
 /**
- * replacement for the magento tax caclulation model that uses the eb2c api to
- * determine the tax/duty rates.
- * NOTE:
- * this class is meant to be used as a singleton
+ *
  */
 class TrueAction_Eb2c_Tax_Overrides_Model_Calculation extends Mage_Tax_Model_Calculation
 {
-	private $callCounts = 0;
-	private $callCounts2 = 0;
-	/**
-	 * generate a request object to use when calculating taxes and duties.
-	 * @param  Mage_Sale_Model_Quote_Addres $shippingAddress
-	 * @param  Mage_Sale_Model_Quote_Addres $billingAddress
-	 * @param  string                       $customerTaxClass
-	 * @param  int|Mage_Core_Model_Store    $store
-	 * @return TrueAction_Eb2c_Tax_Model_TaxDutyRequest
-	 */
-	public function getRateRequest(
-		$shippingAddress = null,
-		$billingAddress = null,
-		$customerTaxClass = '',
-		$store = null
-	) {
-		// if ($billingAddress && $billingAddress->getQuote()->getTdRequest()) {
-		// 	$request = $billingAddress->getQuote()->getTdRequest();
-		// } else {
-			$request = new TrueAction_Eb2c_Tax_Model_TaxDutyRequest(array(
-				'shipping_address'   => $shippingAddress,
-				'billing_address'    => $billingAddress,
-				'customer_tax_class' => $customerTaxClass,
-				'store'              => $store
-			));
-			// $billingAddress->getQuote()->setTdRequest($request);
-		// }
-		return $request;
+	public function __construct()
+	{
+		parent::__construct();
+		if (!$this->hasTaxResponse()) {
+			$checkout = Mage::getSingleton('checkout/session');
+			if ($checkout->hasEb2cTaxResponse()) {
+				parent::setTaxResponse($checkout->getEb2cTaxResponse());
+			}
+		}
 	}
 
 	/**
-	 * get the tax rate for the specific request.
-	 * @param  TaxDutyRequest $request
+	 * get a request object for the quote.
+	 * @param  Mage_Sales_Model_Quote $quote
+	 * @return TrueAction_Eb2c_Tax_Model_Request
+	 */
+	public function getTaxRequest(Mage_Sales_Model_Quote $quote)
+	{
+		return Mage::getModel('eb2ctax/request', array('quote' => $quote));
+	}
+
+	/**
+	 * store the tax response from eb2c
+	 * @param TrueAction_Eb2c_Tax_Model_Response $response
+	 */
+	public function setTaxResponse(TrueAction_Eb2c_Tax_Model_Response $response)
+	{
+		parent::setTaxResponse($response);
+		Mage::getSingleton('checkout/session')->setEb2cTaxResponse($response);
+	}
+
+	/**
+	 * calculate tax amount for an item with the values from the response.
+	 * @param  Mage_Sales_Model_Quote_Item $item
 	 * @return float
 	 */
-	public function getRate($request)
+	public function getTaxforItem(Mage_Sales_Model_Quote_Item $item)
 	{
-		$rate = 1;
-		// if ($request->isUsable()) {
-		// 	$key = $request->getUniqueKey();
-		// 	if (isset($this->_rateCache[$key])) {
-		// 		$rate = $this->_rateCache[$key];
-		// 	} else {
-		// 		$doc = $request->getDocument();
-		// 		Mage::helper('eb2ctax')->sendRequest($request);
-		// 	}
-		// }
-		// check cache
-		// if in cache return rate from cache
-		// else generate xml from request and return result of rate.
-		//
-		return $rate;
+		$response = $this->getTaxResponse();
+		$itemResponse = ($response) ?
+			$response->getResponseForItem($item) : null;
+		$tax = 0.0;
+		if ($itemResponse) {
+			$taxQuotes = $itemResponse->getTaxQuotes();
+			foreach ($taxQuotes as $taxQuote) {
+				$tax += $taxQuote->getCalculatedTax();
+			}
+		}
+		return $tax;
 	}
-
-
 
 	/**
-	 * compare two requests to see if they yield the same rates.
-	 * return true if requests are same; false otherwise
-	 * @param  TaxDutyRequest $first  [description]
-	 * @param  TaxDutyRequest $second [description]
-	 * @return bool
+	 * calculate tax for an amount with the rates from the response for the item.
+	 * @param  float                       $amount
+	 * @param  Mage_Sales_Model_Quote_Item $item
+	 * @param  boolean                     $amountInlcudesTax
+	 * @param  boolean                     $round
+	 * @return float
 	 */
-	public function compareRequests($first, $second)
-	{
-		return false;
+	public function getTaxforItemAmount(
+		$amount,
+		Mage_Sales_Model_Quote_Item $item,
+		$amountInlcudesTax = false,
+		$round = true
+	) {
+		$response = $this->getTaxResponse();
+		$itemResponse = ($response) ?
+			$response->getResponseForItem($item) : null;
+		$tax = 0.0;
+		if ($itemResponse) {
+			$taxQuotes = $itemResponse->getTaxQuotes();
+			foreach ($taxQuotes as $taxQuote) {
+				$tax += $this->calcTaxAmount(
+					$amount,
+					$taxQuote->getEffectiveRate(),
+					$amountInlcudesTax,
+					$round
+				);
+			}
+		}
+		return $tax;
 	}
+
+    /**
+     * Calculate rated tax abount based on price and tax rate.
+     * If you are using price including tax $priceIncludeTax should be true.
+     *
+     * @param   float $price
+     * @param   float $taxRate
+     * @param   boolean $priceIncludeTax
+     * @return  float
+     */
+    public function calcTaxAmount($price, $taxRate, $priceIncludeTax=false, $round=true)
+    {
+        if ($priceIncludeTax) {
+            $amount = $price*(1-1/(1+$taxRate));
+        } else {
+            $amount = $price*$taxRate;
+        }
+
+        if ($round) {
+            return $this->round($amount);
+        }
+
+        return $amount;
+    }
 }
--- src/app/code/local/TrueAction/Eb2c/Tax/Test/Overrides/Model/CalculationTest.php
+++ src/app/code/local/TrueAction/Eb2c/Tax/Test/Overrides/Model/CalculationTest.php
@@ -51,91 +51,82 @@ class TrueAction_Eb2c_Tax_Test_Overrides_Model_CalculationTest extends EcomDev_P
         $_SESSION = array();
         $_baseUrl = Mage::getStoreConfig('web/unsecure/base_url');
         $this->app()->getRequest()->setBaseUrl($_baseUrl);
-		$this->cls = new ReflectionClass(
-			'TrueAction_Eb2c_Tax_Model_Request'
+
+		$taxQuote  = $this->getModelMock('eb2ctax/response_quote',
+			array('getEffectiveRate', 'getCalculatedTax'));
+		$taxQuote->expects($this->any())
+			->method('getEffectiveRate')
+			->will($this->returnValue(.5));
+		$taxQuote->expects($this->any())
+			->method('getCalculatedTax')
+			->will($this->returnValue(0.38));
+		$taxQuote2  = $this->getModelMock('eb2ctax/response_quote',
+			array('getEffectiveRate', 'getCalculatedTax'));
+		$taxQuote2->expects($this->any())
+			->method('getEffectiveRate')
+			->will($this->returnValue(0.01));
+		$taxQuote2->expects($this->any())
+			->method('getCalculatedTax')
+			->will($this->returnValue(10.60));
+		$taxQuotes = array($taxQuote, $taxQuote2);
+		$orderItem = $this->getModelMock(
+			'eb2ctax/response_orderitem',
+			array('getTaxQuotes')
 		);
-		$this->doc = $this->cls->getProperty('_doc');
-		$this->doc->setAccessible(true);
+		$orderItem->expects($this->any())
+			->method('getTaxQuotes')
+			->will($this->returnValue($taxQuotes));
+		$orderItems = array($orderItem);
+		$response = $this->getModelMock(
+			'eb2ctax/response',
+			array('getResponseForItem')
+		);
+		$response->expects($this->any())
+			->method('getResponseForItem')
+			->will($this->returnValue($orderItem));
+		$this->response = $response;
+		$item = $this->getModelMock('sales/quote_item', array('getSku'));
+		$item->expects($this->any())
+			->method('getSku')
+			->will($this->returnValue('somesku'));
+		$this->item = $item;
 	}
 
 	/**
 	 * @test
-	 * @loadFixture base.yaml
-	 * @loadFixture testGetRateRequest.yaml
 	 */
-	public function testGetRateRequest()
+	public function testCalcTaxForItem()
 	{
-		$quote = Mage::getModel('sales/quote')->loadByIdWithoutStore(2);
-		$shipAddress = $quote->getShippingAddress();
-		$billaddress = $quote->getBillingAddress();
-
-		$calc = new TrueAction_Eb2c_Tax_Overrides_Model_Calculation();
-		$request = $calc->getRateRequest($shipAddress, $billaddress, 'someclass', null);
-		$doc = $request->getDocument();
-		$xpath = new DOMXPath($doc);
-		$node = $xpath->query('/TaxDutyQuoteRequest/Currency')->item(0);
-		$this->assertSame('USD', $node->textContent);
-		$node = $xpath->query('/TaxDutyQuoteRequest/VATInclusivePricing')->item(0);
-		$this->assertSame('0', $node->textContent);
-		$node = $xpath->query('/TaxDutyQuoteRequest/CustomerTaxId')->item(0);
-		$this->assertSame('', $node->textContent);
-		$node = $xpath->query('/TaxDutyQuoteRequest/BillingInformation')->item(0);
-		$this->assertSame('4', $node->getAttribute('ref'));
-		$parent = $xpath->query('/TaxDutyQuoteRequest/Shipping/Destinations/MailingAddress')->item(0);
-		$this->assertSame('4', $parent->getAttribute('id'));
-
-		// check the PersonName
-		$node = $xpath->query('PersonName/LastName', $parent)->item(0);
-		$this->assertSame('Guy', $node->textContent);
-		$node = $xpath->query('PersonName/FirstName', $parent)->item(0);
-		$this->assertSame('Test', $node->textContent);
-		$node = $xpath->query('PersonName/Honorific', $parent)->item(0);
-		$this->assertNull($node);
-		$node = $xpath->query('PersonName/MiddleName', $parent)->item(0);
-		$this->assertNull($node);
-
-		// verify the AddressNode
-		$node = $xpath->query('Address/Line1', $parent)->item(0);
-		$this->assertSame('1 RoseDale st', $node->textContent);
-		$node = $xpath->query('Address/Line2', $parent)->item(0);
-		$this->assertNull($node);
-		$node = $xpath->query('Address/Line3', $parent)->item(0);
-		$this->assertNull($node);
-		$node = $xpath->query('Address/Line4', $parent)->item(0);
-		$this->assertNull($node);
-		$node = $xpath->query('Address/City', $parent)->item(0);
-		$this->assertSame('BaltImore', $node->textContent);
-		$node = $xpath->query('Address/MainDivision', $parent)->item(0);
-		$this->assertSame('MD', $node->textContent);
-		$node = $xpath->query('Address/CountryCode', $parent)->item(0);
-		$this->assertSame('US', $node->textContent);
-		$node = $xpath->query('Address/PostalCode', $parent)->item(0);
-		$this->assertSame('21229', $node->textContent);
-
-		// verify the email address
-		$parent = $xpath->query('/TaxDutyQuoteRequest/Shipping/Destinations')->item(0);
-		$node = $xpath->query('Email', $parent)->item(0);
-		$this->assertSame('foo@example.com', $node->getAttribute('id'));
-
-		$node = $xpath->query('Email/EmailAddress', $parent)->item(0);
-		$this->assertSame('foo@example.com', $node->textContent);
-		print $doc->saveXML();
+		$calc = Mage::getSingleton('tax/calculation');
+		$calc->setTaxResponse($this->response);
+		$value = $calc->getTaxForItem($this->item);
+		$this->assertSame(10.98, $value);
 	}
 
 	/**
 	 * @test
-	 * @loadFixture base.yaml
-	 * @loadFixture testGetRateRequest.yaml
 	 */
-	public function testGetRateRequestXsd()
+	public function testCalcTaxForItemAmount()
 	{
-		$quote = Mage::getModel('sales/quote')->loadByIdWithoutStore(2);
-		$shipAddress = $quote->getShippingAddress();
-		$billaddress = $quote->getBillingAddress();
+		$calc = Mage::getSingleton('tax/calculation');
+		$calc->setTaxResponse($this->response);
+		$value = $calc->getTaxForItemAmount(1, $this->item);
+		$this->assertSame(0.51, $value);
+		$value = $calc->getTaxForItemAmount(1.51, $this->item, true);
+		$this->assertSame(0.51, $value);
+		$value = $calc->getTaxForItemAmount(1.1, $this->item, false, false);
+		$this->assertSame(0.561, $value);
+	}
 
-		$calc = new TrueAction_Eb2c_Tax_Overrides_Model_Calculation();
-		$request = $calc->getRateRequest($shipAddress, $billaddress, 'someclass', null);
-		$doc = $request->getDocument();
-		$this->assertTrue($doc->schemaValidate(self::$xsdFile));
+	/**
+	 * @test
+	 */
+	public function testSessionStore()
+	{
+		$calc = Mage::getModel('tax/calculation');
+		$calc->setTaxResponse($this->response);
+		$calc2 = Mage::getModel('tax/calculation');
+		$this->assertNotNull($calc2->getTaxResponse());
+		$this->assertSame($calc->getTaxResponse(), $calc2->getTaxResponse());
 	}
 }

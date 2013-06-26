be416d538380fcb802375db6d6fd82b37bf59b95
Michael Phang <phangm@trueaction.com>
2013-06-26 08:37:53 -0400

    removed code that reads the orderitem nodes out to the orderitem model.

--- src/app/code/local/TrueAction/Eb2c/Tax/Model/Response.php
+++ src/app/code/local/TrueAction/Eb2c/Tax/Model/Response.php
@@ -106,35 +106,13 @@ class TrueAction_Eb2c_Tax_Model_Response extends Mage_Core_Model_Abstract
 			// foreach item
 			$items = $xpath->query('//a:Items/a:OrderItem', $shipGroup);
 			foreach ($items as $item) {
-				$validSku = $this->_validateResponseItem($item);
-				if ($validSku) {
-					$quoteItem = $this->getRequest()->getItemBySku($validSku);
-					Mage::log('got item ' . $validSku);
-					// get item quantity
-					// get merchandise unitprice
-					$type = TrueAction_Eb2c_Tax_Model_Tax::ITEM_TYPE;
-					$taxes = $xpath->query('Pricing/a:Merchandise/a:TaxData/a:Taxes/a:Tax');
-					foreach ($taxes as $tax) {
-						// foreach pricing/merchandise/taxdata/taxes/tax
-						$this->_createTaxRecord($type, $amount, $tax, $address, $quoteItem);
-					}
-					// get shipping amount
-					$amount = $this->_verifyShippingAmount();
-					$type = TrueAction_Eb2c_Tax_Model_Tax::SHIPPING_TYPE;
-					$taxes = $xpath->query('a:Pricing/a:Shipping/a:TaxData/a:Taxes/a:Tax');
-					foreach ($taxes as $tax) {
-						// foreach pricing/shipping/taxdata/
-						$this->_createTaxRecord($type, $amount, $tax, $address, $quoteItem);
-					}
-					$amount = $this->_verifyDutyAmount();
-					$type = TrueAction_Eb2c_Tax_Model_Tax::DUTY_TYPE;
-					$taxes = $xpath->query('a:Pricing/a:Duty/a:TaxData/a:Taxes/a:Tax');
-					foreach ($taxes as $tax) {
-						// foreach pricing/shipping/taxdata/
-						$this->_createTaxRecord($type, $amount, $tax, $address, $quoteItem);
-					}
-				} else {
-					Mage::log("Skipping item '%s'", Zend_Log::DEBUG);
+				$orderItem = Mage::getModel('eb2ctax/response_orderitem', array(
+					'node' => $item,
+					'namespace_uri' => $this->_namespaceUri
+				));
+				if ($orderItem->isValid()) {
+					$itemKey = $orderItem->getSku();
+					$this->_responseItems[$itemKey] = $orderItem;
 				}
 			}
 		}
@@ -143,116 +121,11 @@ class TrueAction_Eb2c_Tax_Model_Response extends Mage_Core_Model_Abstract
 	}
 
 	/**
-	 * compare an OrderItem element with the corresponding element in the request
-	 * to make sure we got back what we sent.
-	 * throw a mismatch error exception if a required field does not match what was sent.
-	 * log a debug message for any optional data that does not match.
-	 * @param  TrueAction_Dom_Element $itemNode OrderItem element from the request
-	 * @param  string                           ShipGroup's id
-	 * @return string                           the OrderItem's ItemId value (sku)
-	 * @throws TrueAction_Eb2c_Tax_Model_Response_MismatchError
 	 */
-	protected function _validateResponseItem(TrueAction_Dom_Element $itemNode, $shipGrpId)
 	{
-		$n = $this->_namespaceAlias . ':';
-		$requestDoc = $this->getRequest()->getDocument();
-		$requestXpath = new DOMXPath($requestDoc);
-		$requestXpath->registerNamespace('a', $this->_namespaceUri);
 		$xpath = new DOMXPath($this->_doc);
 		$xpath->registerNamespace('a', $this->_namespaceUri);
-		$sku = $xpath->evaluate("a:ItemId/text()", $itemNode);
-		$reqNode = $requestXpath->query(
-			'//a:ShipGroup[@id=' . $shipGrpId . '//a:OrderItem/a:ItemId[.=' . $sku . ']'
-		)->items(0);
-		$isValid = true;
-		// if we get back an order item we didn't send log it as a debug message and
-		// ignore it.
-		if (!$reqNode) {
-			$isValid = false;
-			Mage::log(
-				sprintf('TaxDutyQuoteResponse: sku "%s" not found in the request.', $sku),
-				Zend_Log::DEBUG
-			);
-			$sku = '';
-		} else {
-			$path = 'a:Quantity';
-			if (!$this->_checkPathValues($path, $xpath, $itemNode, $requestXpath, $reqNode, true)) {
-				$isValid = false;
-				throw new TrueAction_Eb2c_Tax_Model_Response_Mismatch("$sku @ $path");
-			}
-			$path = 'a:Pricing/a:Merchandise/a:UnitPrice';
-			if (!$this->_checkPathValues($path, $xpath, $itemNode, $requestXpath, $reqNode, true)) {
-				$isValid = false;
-				throw new TrueAction_Eb2c_Tax_Model_Response_Mismatch("$sku @ $path");
-			}
-			$path = 'a:Pricing/a:Shipping/a:Amount';
-			if (!$this->_checkPathValues($path, $xpath, $itemNode, $requestXpath, $reqNode, true)) {
-				$isValid = false;
-				throw new TrueAction_Eb2c_Tax_Model_Response_Mismatch("$sku @ $path");
-			}
-			if ($reqNode->getAttribute("LineNumber") !== $itemNode->getAttribute('LineNumber'))
-			{
-				Mage::log(
-					sprintf(
-						'TaxDutyQuoteResponse: %s "%s" does not match request "%s"',
-						'LineNumber',
-						$resValue,
-						$reqValue
-					),
-					Zend_Log::WARN
-				);
-			}
-			$path = 'a:ItemDesc';
-			$isValid &= $this->_checkPathValues($path, $xpath, $itemNode, $requestXpath, $reqNode);
-			$path = 'a:HTSCode';
-			$isValid &= $this->_checkPathValues($path, $xpath, $itemNode, $requestXpath, $reqNode);
-			$path = 'a:Pricing/a:Merchandise/a:Amount';
-			$isValid &= $this->_checkPathValues($path, $xpath, $itemNode, $requestXpath, $reqNode);
-		}
-		if ($isValid) {
-			$this->_validSkus[] = $sku;
-		} else {
-			$sku = '';
-		}
-		return $sku;
-	}
 
-	/**
-	 * compare the value of an element in the response document to an element in the
-	 * request document.
-	 * @param  string  $path
-	 * @param  [type]  $xpath      [description]
-	 * @param  [type]  $itemNode   [description]
-	 * @param  [type]  $reqXpath   [description]
-	 * @param  [type]  $reqNode    [description]
-	 * @param  boolean $isRequired [description]
-	 * @return [type]              [description]
-	 */
-	protected function _checkPathValues(
-		$path,
-		$xpath,
-		$itemNode,
-		$reqXpath,
-		$reqNode,
-		$isRequired = false
-	) {
-		$resValue = $xpath->evaluate("{$path}/text()", $itemNode);
-		$reqValue = $reqXpath->evaluate("{$path}/text()", $reqNode);
-		$isMatching = true;
-		if ($resValue !== $reqValue) {
-			$isMatching = false;
-			$message = sprintf(
-				'TaxDutyQuoteResponse: %s "%s" does not match request "%s"',
-				$path,
-				$resValue,
-				$reqValue
-			);
-			if ($isRequired) {
-				Mage::log($message, Zend_Log::WARN);
-			} else {
-				Mage::log($message, Zend_Log::DEBUG);
-			}
 		}
-		return $isMatching;
 	}
 }

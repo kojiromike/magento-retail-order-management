138e8c4cfbb85bd761b7f432623bc562d0f9c2d6
Mike West <westm@trueaction.com>
2013-06-27 15:54:29 -0400

    Broken config array fixed

--- src/app/code/local/TrueAction/Eb2c/Order/Model/Config.php
+++ src/app/code/local/TrueAction/Eb2c/Order/Model/Config.php
@@ -4,12 +4,10 @@
  */
 class TrueAction_Eb2c_Order_Model_Config extends TrueAction_Eb2c_Core_Model_Config_Abstract
 {
-	const EB2C_ORDER_PATH = 'eb2c/order/';
-
 	protected $_configPaths = array(
-		'developer_mode' => self:EB2C_ORDER_PATH . 'developer_mode';
-		'create_uri' => self::EB2C_ORDER_PATH . 'create_uri',
-		'cancel_uri' => self::EBC_ORDER_PATH . 'cancel_uri',
-		'gsi_payments_enabled' => self::EBC_ORDER_PATH . 'gsi_payments_enabled',
+		'developer_mode' => 'eb2c/order/developer_mode',
+		'create_uri' => 'eb2c/order/create_uri',
+		'cancel_uri' => 'eb2c/order/cancel_uri',
+		'gsi_payments_enabled' => 'eb2c/order/gsi_payments_enabled',
 	);
 }

3290d0abe64d45b8e92165175b5267d6a4ac8fd3
Michael Phang <phangm@trueaction.com>
2013-07-08 15:47:58 -0400

    added tests for _getAddress and _loadAddress and a test for the case where a single sku was split across multiple shipgroups.
    ensured the correct quote was being loaded by the setUp function.
    simplified the setupbeforeclass function.
    updated the fixtures to have correct destination id's.

--- src/app/code/local/TrueAction/Eb2c/Tax/Test/Model/ResponseTest.php
+++ src/app/code/local/TrueAction/Eb2c/Tax/Test/Model/ResponseTest.php
@@ -21,12 +21,9 @@ class TrueAction_Eb2c_Tax_Test_Model_ResponseTest extends EcomDev_PHPUnit_Test_C
 			'TrueAction_Eb2c_Tax_Model_Response'
 		);
 		$path = dirname(__FILE__) . '/ResponseTest/fixtures/response.xml';
-		$file = fopen($path, 'r');
-		self::$respXml = fread($file, filesize($path));
-		fclose($file);
+		self::$respXml = file_get_contents($path);
 		$path = dirname(__FILE__) . '/ResponseTest/fixtures/request.xml';
-		$file = fopen($path, 'r');
-		self::$reqXml = fread($file, filesize($path));
+		self::$reqXml = file_get_contents($path);
 	}
 
 	public function setUp()
@@ -35,7 +32,7 @@ class TrueAction_Eb2c_Tax_Test_Model_ResponseTest extends EcomDev_PHPUnit_Test_C
 		$_SESSION = array();
 		$_baseUrl = Mage::getStoreConfig('web/unsecure/base_url');
 		$this->app()->getRequest()->setBaseUrl($_baseUrl);
-		$quote = Mage::getModel('sales/quote')->loadByIdWithoutStore(2);
+		$quote = Mage::getModel('sales/quote')->loadByIdWithoutStore(3);
 		$this->request = Mage::getModel('eb2ctax/request', array(
 			'quote' => $quote
 		));
@@ -52,6 +49,8 @@ class TrueAction_Eb2c_Tax_Test_Model_ResponseTest extends EcomDev_PHPUnit_Test_C
 	 * Testing getResponseForItem method
 	 *
 	 * @test
+	 * @loadFixture base.yaml
+	 * @loadFixture testItemSplitAcrossShipgroups.yaml
 	 */
 	public function testGetResponseForItem()
 	{
@@ -64,7 +63,11 @@ class TrueAction_Eb2c_Tax_Test_Model_ResponseTest extends EcomDev_PHPUnit_Test_C
 		$item->expects($this->any())
 			->method('getSku')
 			->will($this->returnValue('gc_virtual1'));
-		$responseItem = $response->getResponseForItem($item);
+		$addressMock1 = $this->getModelMock('sales/quote_address');
+		$addressMock1->expects($this->any())
+			->method('getId')
+			->will($this->returnValue(1));
+		$responseItem = $response->getResponseForItem($item, $addressMock1);
 		$this->assertNotNull($responseItem);
 		$this->assertSame(3, count($responseItem->getTaxQuotes()));
 	}
@@ -73,6 +76,8 @@ class TrueAction_Eb2c_Tax_Test_Model_ResponseTest extends EcomDev_PHPUnit_Test_C
 	 * Testing getResponseItems method
 	 *
 	 * @test
+	 * @loadFixture base.yaml
+	 * @loadFixture testItemSplitAcrossShipgroups.yaml
 	 */
 	public function testGetResponseItems()
 	{
@@ -90,6 +95,8 @@ class TrueAction_Eb2c_Tax_Test_Model_ResponseTest extends EcomDev_PHPUnit_Test_C
 	 * Testing isValid method
 	 *
 	 * @test
+	 * @loadFixture base.yaml
+	 * @loadFixture testItemSplitAcrossShipgroups.yaml
 	 */
 	public function testIsValid()
 	{
@@ -97,17 +104,15 @@ class TrueAction_Eb2c_Tax_Test_Model_ResponseTest extends EcomDev_PHPUnit_Test_C
 			'xml' => self::$respXml,
 			'request' => $this->request
 		));
-
-		$this->assertSame(
-			false,
-			$response->isValid()
-		);
+		$this->assertTrue($response->isValid());
 	}
 
 	/**
 	 * Testing _construct method - valid request/response match xml
 	 *
 	 * @test
+	 * @loadFixture base.yaml
+	 * @loadFixture testItemSplitAcrossShipgroups.yaml
 	 */
 	public function testConstructValidRequestResponseMatch()
 	{
@@ -119,7 +124,7 @@ class TrueAction_Eb2c_Tax_Test_Model_ResponseTest extends EcomDev_PHPUnit_Test_C
 		$addressMock = $this->getModelMock('sales/quote_address', array('getId'));
 		$addressMock->expects($this->any())
 			->method('getId')
-			->will($this->returnValue(0));
+			->will($this->returnValue(1));
 
 		$responseReflector = new ReflectionObject($response);
 		$addressProperty = $responseReflector->getProperty('_address');
@@ -138,6 +143,8 @@ class TrueAction_Eb2c_Tax_Test_Model_ResponseTest extends EcomDev_PHPUnit_Test_C
 	 * Testing _construct method - invalid request/response match xml
 	 *
 	 * @test
+	 * @loadFixture base.yaml
+	 * @loadFixture testItemSplitAcrossShipgroups.yaml
 	 */
 	public function testConstructInvalidRequestResponseMatch()
 	{
@@ -166,6 +173,8 @@ class TrueAction_Eb2c_Tax_Test_Model_ResponseTest extends EcomDev_PHPUnit_Test_C
 	 * Testing _construct method - invalid request/response match xml because of MailingAddress[@id="2"] element is different
 	 *
 	 * @test
+	 * @loadFixture base.yaml
+	 * @loadFixture testItemSplitAcrossShipgroups.yaml
 	 */
 	public function testConstructMailingAddressMisMatch()
 	{
@@ -190,16 +199,74 @@ class TrueAction_Eb2c_Tax_Test_Model_ResponseTest extends EcomDev_PHPUnit_Test_C
 		);
 	}
 
+
 	/**
 	 * @test
 	 * @loadFixture base.yaml
-	 * @loadFixture singleShippingNotSameAsBilling.yaml
+	 * @loadFixture testItemSplitAcrossShipgroups.yaml
 	 */
-	public function testItemSplitAccrossShipgroups()
+	public function testLoadAddress()
 	{
 		$xmlPath = dirname(__FILE__) . '/ResponseTest/fixtures/responseSplitAcrossShipGroups.xml';
-		$docObject = new TrueAction_Dom_Document('1.0', 'UTF-8');
-		$docObject->loadXML();
+		$response = Mage::getModel(
+			'eb2ctax/response',
+			array(
+				'xml' => file_get_contents($xmlPath)
+			)
+		);
+		$rf = new ReflectionObject($response);
+		$loadAddress = $rf->getMethod('_loadAddress');
+		$loadAddress->setAccessible(true);
+		$loadAddress->invoke($response, 1);
+
+		$address = $rf->getProperty('_address');
+		$address->setAccessible(true);
+		$a = $address->getValue($response);
+		$this->assertInstanceOf('Mage_Sales_Model_Quote_Address', $a);
+		$this->assertSame('1', $a->getId());
+	}
+
+
+	/**
+	 * @test
+	 * @loadFixture base.yaml
+	 * @loadFixture testItemSplitAcrossShipgroups.yaml
+	 */
+	public function testGetAddress()
+	{
+		$xmlPath = dirname(__FILE__) . '/ResponseTest/fixtures/responseSplitAcrossShipGroups.xml';
+		$response = Mage::getModel(
+			'eb2ctax/response',
+			array(
+				'xml' => file_get_contents($xmlPath)
+			)
+		);
+		$rf = new ReflectionObject($response);
+		$docRf = $rf->getProperty('_doc');
+		$docRf->setAccessible(true);
+		$doc = $docRf->getValue($response);
+		$x = new DOMXPath($doc);
+		$x->registerNamespace('a', $doc->documentElement->namespaceURI);
+		$shipGroups = $x->query('//a:ShipGroup');
+		// NOTE: THIS IS DEPENDENT ON THE ORDER OF THE SHIPGROUPS IN THE XML
+		foreach ($shipGroups as $index => $shipGroup) {
+			$this->assertNotNull($shipGroup);
+			$getAddress = $rf->getMethod('_getAddress');
+			$getAddress->setAccessible(true);
+			$a = $getAddress->invoke($response, $shipGroup);
+			$this->assertInstanceOf('Mage_Sales_Model_Quote_Address', $a);
+			$this->assertSame((string)($index + 1), $a->getId());
+		}
+	}
+
+	/**
+	 * @test
+	 * @loadFixture base.yaml
+	 * @loadFixture testItemSplitAcrossShipgroups.yaml
+	 */
+	public function testItemSplitAcrossShipgroups()
+	{
+		$xmlPath = dirname(__FILE__) . '/ResponseTest/fixtures/responseSplitAcrossShipGroups.xml';
 		$response = Mage::getModel(
 			'eb2ctax/response',
 			array(
@@ -207,29 +274,27 @@ class TrueAction_Eb2c_Tax_Test_Model_ResponseTest extends EcomDev_PHPUnit_Test_C
 			)
 		);
 
-		$addressMock1 = $this->getModelMock('sales/quote_address');
+		$addressMock1 = $this->getModelMock('sales/quote_address', array('getId'));
 		$addressMock1->expects($this->any())
 			->method('getId')
 			->will($this->returnValue(1));
-		$addressMock1->expects($this->any())
-			->method('getShippingRate')
-			->will($this->returnValue('flatrate_flatrate'));
-		$addressMock2 = $this->getModelMock('sales/quote_address');
+		$addressMock2 = $this->getModelMock('sales/quote_address', array('getId'));
 		$addressMock2->expects($this->any())
 			->method('getId')
 			->will($this->returnValue(2));
-		$addressMock2->expects($this->any())
-			->method('getShippingRate')
-			->will($this->returnValue('flatrate_flatrate'));
 
-		$itemMock = $this->getModelMock('sales/quote_item');
+		$itemMock = $this->getModelMock('sales/quote_item', array('getSku'));
 		$itemMock->expects($this->any())
 			->method('getSku')
 			->will($this->returnValue('gc_virtual1'));
-
+		$responseItems = $response->getResponseItems();
+		xdebug_break();
+		$this->assertNotEmpty($response->getResponseItems());
 		$itemResponse = $response->getResponseForItem($itemMock, $addressMock1);
-		$this->assertSame(1, $itemResponse->getQantity());
+		$this->assertNotNull($itemResponse);
+		$this->assertSame('1', $itemResponse->getLineNumber());
 		$itemResponse = $response->getResponseForItem($itemMock, $addressMock2);
-		$this->assertSame(2, $itemResponse->getQantity());
+		$this->assertNotNull($itemResponse);
+		$this->assertSame('2', $itemResponse->getLineNumber());
 	}
 }

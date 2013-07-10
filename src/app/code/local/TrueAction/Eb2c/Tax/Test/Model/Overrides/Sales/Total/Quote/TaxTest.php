462a6a13cf67ba6980cdec02ab6612030e6d5b86
Michael Phang <phangm@trueaction.com>
2013-07-10 10:03:54 -0400

    added incomplete tax tests.

--- src/app/code/local/TrueAction/Eb2c/Tax/Test/Model/Overrides/Sales/Total/Quote/TaxTest.php
+++ src/app/code/local/TrueAction/Eb2c/Tax/Test/Model/Overrides/Sales/Total/Quote/TaxTest.php
@@ -32,17 +32,89 @@ class TrueAction_Eb2c_Tax_Test_Model_Overrides_Sales_Total_Quote_TaxTest extends
 		Mage::helper('tax')->getCalculator()->setTaxResponse($response);
 	}
 
-	/**
-	 * @test
-	 */
+	public function testCollect()
+	{
+		$this->markTestIncomplete('need to add some assertions');
+		$productMock = $this->getModelMock('catalog/product', array('isVirtual'));
+		$productMock->expects($this->any())
+			->method('isVirtual')
+			->will($this->returnValue(true));
+
+		$itemMock = $this->getModelMock('sales/quote_item', array('getQty', 'getCalculationPriceOriginal', 'getStore', 'getId', 'getProduct', 'getHasChildren', 'isChildrenCalculated', 'getChildren'));
+		$itemMock->expects($this->any())
+			->method('getId')
+			->will($this->returnValue(1));
+		$itemMock->expects($this->any())
+			->method('getProduct')
+			->will($this->returnValue($productMock));
+		$itemMock->expects($this->any())
+			->method('getHasChildren')
+			->will($this->returnValue(true));
+		$itemMock->expects($this->any())
+			->method('getCalculationPriceOriginal')
+			->will($this->returnValue(100));
+		$itemMock->expects($this->any())
+			->method('isChildrenCalculated')
+			->will($this->returnValue(true));
+		$itemMock->expects($this->any())
+			->method('getChildren')
+			->will($this->returnValue(array($itemMock)));
+		$itemMock->expects($this->any())
+			->method('getStore')
+			->will($this->returnValue(Mage::app()->getStore()));
+		$itemMock->expects($this->any())
+			->method('getQty')
+			->will($this->returnValue(1));
+
+		$addressMock = $this->getModelMock('sales/quote_address', array('getQuote', 'getAllNonNominalItems', 'getId'));
+		$addressMock->expects($this->any())
+			->method('getId')
+			->will($this->returnValue(1));
+		$addressMock->expects($this->any())
+			->method('getAllNonNominalItems')
+			->will($this->returnValue(array($itemMock)));
+
+		$quoteMock = $this->getModelMock('sales/quote', array('getStore', 'getId', 'getItemsCount', 'getBillingAddress', 'getAllVisibleItems'));
+		$quoteMock->expects($this->any())
+			->method('getId')
+			->will($this->returnValue(1));
+		$quoteMock->expects($this->any())
+			->method('getItemsCount')
+			->will($this->returnValue(1));
+		$quoteMock->expects($this->any())
+			->method('getBillingAddress')
+			->will($this->returnValue($addressMock));
+		$quoteMock->expects($this->any())
+			->method('getAllVisibleItems')
+			->will($this->returnValue(array($itemMock)));
+		$quoteMock->expects($this->any())
+			->method('getStore')
+			->will($this->returnValue(Mage::app()->getStore()));
+
+		$addressMock->expects($this->any())
+			->method('getQuote')
+			->will($this->returnValue($quoteMock));
+
+		$tax = Mage::getModel('tax/sales_total_quote_tax');
+		$tax->collect($addressMock);
+		// assert the item->getTaxAmount is as expected.
+	}
+
 	public function testCalcRowTaxAmount()
 	{
-		$this->markTestSkipped('Missing fixture – items array is empty.');
+		$this->markTestIncomplete('Missing fixture – items array is empty.');
 		$calc  = Mage::helper('tax')->getCalculator();
 		$quote = Mage::getModel('sales/quote')->loadByIdWithoutStore(1);
 		$items = $quote->getShippingAddress()->getAllVisibleItems();
 		$item = $items[0];
 		$this->calcRowTaxAmount->invoke($this->tax, $item);
 		$this->assertEquals(0.0, $item->getTaxPercent());
+	}
+
+	public function testCollectWithDiscounts()
+	{
+		// need to create coupon code scenario
+		// assert tax amount is altered by discount amount in case where discount calc'd after tax.
+		$this->markTestIncomplete();
 	}
 }

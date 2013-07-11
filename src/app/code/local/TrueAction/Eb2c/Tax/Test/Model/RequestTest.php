e09f6ebc4a9948b2ee5cd467a6c9e4f8441680fc
Michael Phang <phangm@trueaction.com>
2013-07-11 10:45:04 -0400

    re-added an old request test.

--- src/app/code/local/TrueAction/Eb2c/Tax/Test/Model/RequestTest.php
+++ src/app/code/local/TrueAction/Eb2c/Tax/Test/Model/RequestTest.php
@@ -389,4 +389,70 @@ class TrueAction_Eb2c_Tax_Test_Model_RequestTest extends EcomDev_PHPUnit_Test_Ca
 			'_' . $quote->getBillingAddress()->getEmail();
 		$this->assertTrue(isset($destinations[$virtualId]));
 	}
+
+	/**
+	 * @test
+	 * @loadFixture base.yaml
+	 * @loadFixture testGetRateRequest.yaml
+	 */
+	public function testGetRateRequest()
+	{
+		$this->markTestIncomplete('needs to be updated');
+		$quote = Mage::getModel('sales/quote')->loadByIdWithoutStore(2);
+		$shipAddress = $quote->getShippingAddress();
+		$billaddress = $quote->getBillingAddress();
+
+		$calc = new TrueAction_Eb2c_Tax_Overrides_Model_Calculation();
+		$request = $calc->getRateRequest($shipAddress, $billaddress, 'someclass', null);
+		$doc = $request->getDocument();
+		$xpath = new DOMXPath($doc);
+		$node = $xpath->query('/TaxDutyQuoteRequest/Currency')->item(0);
+		$this->assertSame('USD', $node->textContent);
+		$node = $xpath->query('/TaxDutyQuoteRequest/VATInclusivePricing')->item(0);
+		$this->assertSame('0', $node->textContent);
+		$node = $xpath->query('/TaxDutyQuoteRequest/CustomerTaxId')->item(0);
+		$this->assertSame('', $node->textContent);
+		$node = $xpath->query('/TaxDutyQuoteRequest/BillingInformation')->item(0);
+		$this->assertSame('4', $node->getAttribute('ref'));
+		$parent = $xpath->query('/TaxDutyQuoteRequest/Shipping/Destinations/MailingAddress')->item(0);
+		$this->assertSame('4', $parent->getAttribute('id'));
+
+		// check the PersonName
+		$node = $xpath->query('PersonName/LastName', $parent)->item(0);
+		$this->assertSame('Guy', $node->textContent);
+		$node = $xpath->query('PersonName/FirstName', $parent)->item(0);
+		$this->assertSame('Test', $node->textContent);
+		$node = $xpath->query('PersonName/Honorific', $parent)->item(0);
+		$this->assertNull($node);
+		$node = $xpath->query('PersonName/MiddleName', $parent)->item(0);
+		$this->assertNull($node);
+
+		// verify the AddressNode
+		$node = $xpath->query('Address/Line1', $parent)->item(0);
+		$this->assertSame('1 RoseDale st', $node->textContent);
+		$node = $xpath->query('Address/Line2', $parent)->item(0);
+		$this->assertNull($node);
+		$node = $xpath->query('Address/Line3', $parent)->item(0);
+		$this->assertNull($node);
+		$node = $xpath->query('Address/Line4', $parent)->item(0);
+		$this->assertNull($node);
+		$node = $xpath->query('Address/City', $parent)->item(0);
+		$this->assertSame('BaltImore', $node->textContent);
+		$node = $xpath->query('Address/MainDivision', $parent)->item(0);
+		$this->assertSame('MD', $node->textContent);
+		$node = $xpath->query('Address/CountryCode', $parent)->item(0);
+		$this->assertSame('US', $node->textContent);
+		$node = $xpath->query('Address/PostalCode', $parent)->item(0);
+		$this->assertSame('21229', $node->textContent);
+
+		// verify the email address
+		$parent = $xpath->query('/TaxDutyQuoteRequest/Shipping/Destinations')->item(0);
+		$node = $xpath->query('Email', $parent)->item(0);
+		$this->assertSame('foo@example.com', $node->getAttribute('id'));
+
+		$node = $xpath->query('Email/EmailAddress', $parent)->item(0);
+		$this->assertSame('foo@example.com', $node->textContent);
+		print $doc->saveXML();
+	}
+
 }

8a3b69748065eddcaac2f74cc1c1690f95a157d0
Michael Phang <phangm@trueaction.com>
2013-07-12 15:27:18 -0400

    made sure to pass a reference of the array into _extractItemDiscountData.

--- src/app/code/local/TrueAction/Eb2c/Tax/Model/Request.php
+++ src/app/code/local/TrueAction/Eb2c/Tax/Model/Request.php
@@ -364,7 +364,7 @@ class TrueAction_Eb2c_Tax_Model_Request extends Mage_Core_Model_Abstract
 			'shipping_amount' => $address->getShippingAmount(),
 			'shipping_tax_class' => $this->_getShippingTaxClass(),
 		);
-		$this->_extractItemDiscountData($item, $address, $data);
+		$this->_extractItemDiscountData($item, $address, &$data);
 		return $data;
 	}
 

60fc4e635912f7d85abfe02ba619ecc8fd7df0c5
Michael Phang <phangm@trueaction.com>
2013-07-17 09:27:39 -0400

    moved the calculate shipping tax function.

--- src/app/code/local/TrueAction/Eb2c/Tax/Overrides/Model/Sales/Total/Quote/Tax.php
+++ src/app/code/local/TrueAction/Eb2c/Tax/Overrides/Model/Sales/Total/Quote/Tax.php
@@ -87,93 +87,6 @@ class TrueAction_Eb2c_Tax_Overrides_Model_Sales_Total_Quote_Tax extends Mage_Tax
 		return $this;
 	}
 
-/// most of this function can be merged up into the collect function
-////////////////
-	/**
-	 * Tax caclulation for shipping price
-	 *
-	 * @param   Mage_Sales_Model_Quote_Address $address
-	 * @param   Varien_Object $taxRateRequest
-	 * @return  Mage_Tax_Model_Sales_Total_Quote
-	 */
-	protected function _calculateShippingTax(Mage_Sales_Model_Quote_Address $address, $taxRateRequest = null)
-	{
-		// this should be moved to the request if needed
-		// $taxRateRequest->setProductClassId($this->_config->getShippingTaxClass($this->_store));
-
-		// dont need
-		// $rate           = $this->_calculator->getRate($taxRateRequest);
-
-//////////////////////////////////////////////////////////////////////////
-// figure out these as they may need to be moved to the request.
-		$shipping       = $address->getShippingTaxable();
-		$baseShipping   = $address->getBaseShippingTaxable();
-//////////////////////////////////////////////////////////////////////////
-		//$rateKey        = (string)$rate;
-
-		$hiddenTax      = null;
-		$baseHiddenTax  = null;
-		/// this needs to be moved to before generating the request
-		switch ($this->_helper->getCalculationSequence($this->_store)) {
-			case Mage_Tax_Model_Calculation::CALC_TAX_BEFORE_DISCOUNT_ON_EXCL:
-			case Mage_Tax_Model_Calculation::CALC_TAX_BEFORE_DISCOUNT_ON_INCL:
-				$tax        = $this->_calculator->calcTaxAmount($shipping, $rate, $inclTax, false);
-				$baseTax    = $this->_calculator->calcTaxAmount($baseShipping, $rate, $inclTax, false);
-				break;
-			case Mage_Tax_Model_Calculation::CALC_TAX_AFTER_DISCOUNT_ON_EXCL:
-			case Mage_Tax_Model_Calculation::CALC_TAX_AFTER_DISCOUNT_ON_INCL:
-				$discountAmount     = $address->getShippingDiscountAmount();
-				$baseDiscountAmount = $address->getBaseShippingDiscountAmount();
-				$tax = $this->_calculator->calcTaxAmount(
-					$shipping - $discountAmount,
-					$rate,
-					$inclTax,
-					false
-				);
-				$baseTax = $this->_calculator->calcTaxAmount(
-					$baseShipping - $baseDiscountAmount,
-					$rate,
-					$inclTax,
-					false
-				);
-				break;
-		}
-
-		////// can be removed since all of the transforms take place
-		//     after the tax value is figured out
-		// if ($this->_config->getAlgorithm($this->_store) == Mage_Tax_Model_Calculation::CALC_TOTAL_BASE) {
-		//     $this->_addAmount(max(0, $tax));
-		//     $this->_addBaseAmount(max(0, $baseTax));
-		//     $tax        = $this->_deltaRound($tax, $rate, $inclTax);
-		//     $baseTax    = $this->_deltaRound($baseTax, $rate, $inclTax, 'base');
-		// } else {
-		//     $tax        = $this->_calculator->round($tax);
-		//     $baseTax    = $this->_calculator->round($baseTax);
-		//     $this->_addAmount(max(0, $tax));
-		//     $this->_addBaseAmount(max(0, $baseTax));
-		// }
-
-		//////// after taxes so can be removed
-		// if ($inclTax && !empty($discountAmount)) {
-		//     $hiddenTax      = $this->_calculator->calcTaxAmount($discountAmount, $rate, $inclTax, false);
-		//     $baseHiddenTax  = $this->_calculator->calcTaxAmount($baseDiscountAmount, $rate, $inclTax, false);
-		//     $this->_hiddenTaxes[] = array(
-		//         'rate_key'   => $rateKey,
-		//         'value'      => $hiddenTax,
-		//         'base_value' => $baseHiddenTax,
-		//         'incl_tax'   => $inclTax,
-		//     );
-		// }
-
-		// this needs to be the sum of the shipping tax lines
-		$address->setShippingTaxAmount(max(0, $tax));
-		$address->setBaseShippingTaxAmount(max(0, $baseTax));
-		$applied = $this->_calculator->getAppliedRates($taxRateRequest);
-		$this->_saveAppliedTaxes($address, $applied, $tax, $baseTax, $rate);
-
-		return $this;
-	}
-
 	/**
 	 * Calculate address total tax based on row total
 	 *
@@ -307,6 +220,85 @@ class TrueAction_Eb2c_Tax_Overrides_Model_Sales_Total_Quote_Tax extends Mage_Tax
 			$item->setRowTotalInclTax($subtotal + $rowTax + $taxCompensation);
 			$item->setBaseRowTotalInclTax($baseSubtotal + $baseRowTax + $item->getBaseDiscountTaxCompensation());
 		}
+
+		return $this;
+	}
+
+	/// most of this function can be merged up into the collect function
+	////////////////
+	/**
+	 * Tax caclulation for shipping price
+	 *
+	 * @param   Mage_Sales_Model_Quote_Address $address
+	 * @param   Varien_Object $taxRateRequest
+	 * @return  Mage_Tax_Model_Sales_Total_Quote
+	 */
+	protected function _calculateShippingTax(Mage_Sales_Model_Quote_Address $address, $taxRateRequest = null)
+	{
+		// this should be moved to the request if needed
+		// $taxRateRequest->setProductClassId($this->_config->getShippingTaxClass($this->_store));
+
+		// dont need
+
+		// $rate           = $this->_calculator->getRate($taxRateRequest);
+
+		//////////////////////////////////////////////////////////////////////////
+		// figure out these as they may need to be moved to the request.
+		$shipping       = $address->getShippingTaxable();
+		$baseShipping   = $address->getBaseShippingTaxable();
+		//////////////////////////////////////////////////////////////////////////
+		//$rateKey        = (string)$rate;
+
+		$hiddenTax      = null;
+		$baseHiddenTax  = null;
+		/// this needs to be moved to before generating the request
+		switch ($this->_helper->getCalculationSequence($this->_store)) {
+			case Mage_Tax_Model_Calculation::CALC_TAX_BEFORE_DISCOUNT_ON_EXCL:
+			case Mage_Tax_Model_Calculation::CALC_TAX_BEFORE_DISCOUNT_ON_INCL:
+				$tax        = $this->_calculator->calcTaxAmount($shipping, $rate, $inclTax, false);
+				$baseTax    = $this->_calculator->calcTaxAmount($baseShipping, $rate, $inclTax, false);
+				break;
+			case Mage_Tax_Model_Calculation::CALC_TAX_AFTER_DISCOUNT_ON_EXCL:
+			case Mage_Tax_Model_Calculation::CALC_TAX_AFTER_DISCOUNT_ON_INCL:
+				$discountAmount     = $address->getShippingDiscountAmount();
+				$baseDiscountAmount = $address->getBaseShippingDiscountAmount();
+				$tax = $this->_calculator->calcTaxAmount(
+					$shipping - $discountAmount,
+					$rate,
+					$inclTax,
+					false
+				);
+				$baseTax = $this->_calculator->calcTaxAmount(
+					$baseShipping - $baseDiscountAmount,
+					$rate,
+					$inclTax,
+					false
+				);
+				break;
+		}
+
+		//     $tax        = $this->_calculator->round($tax);
+		//     $baseTax    = $this->_calculator->round($baseTax);
+		//     $this->_addAmount(max(0, $tax));
+		//     $this->_addBaseAmount(max(0, $baseTax));
+
+		//////// after taxes so can be removed
+		// if ($inclTax && !empty($discountAmount)) {
+		//     $hiddenTax      = $this->_calculator->calcTaxAmount($discountAmount, $rate, $inclTax, false);
+		//     $baseHiddenTax  = $this->_calculator->calcTaxAmount($baseDiscountAmount, $rate, $inclTax, false);
+		//     $this->_hiddenTaxes[] = array(
+		//         'rate_key'   => $rateKey,
+		//         'value'      => $hiddenTax,
+		//         'base_value' => $baseHiddenTax,
+		//         'incl_tax'   => $inclTax,
+		//     );
+		// }
+
+		// this needs to be the sum of the shipping tax lines
+		$address->setShippingTaxAmount(max(0, $tax));
+		$address->setBaseShippingTaxAmount(max(0, $baseTax));
+		$applied = $this->_calculator->getAppliedRates($taxRateRequest);
+		$this->_saveAppliedTaxes($address, $applied, $tax, $baseTax, $rate);
 
 		return $this;
 	}

97c7a248d6888fe5f147595070a75aea4c6efd65
Michael Phang <phangm@trueaction.com>
2013-07-18 17:07:40 -0400

    added new test functions.

--- src/app/code/local/TrueAction/Eb2c/Tax/Test/Model/Overrides/Sales/Total/Quote/TaxTest.php
+++ src/app/code/local/TrueAction/Eb2c/Tax/Test/Model/Overrides/Sales/Total/Quote/TaxTest.php
@@ -57,6 +57,108 @@ class TrueAction_Eb2c_Tax_Test_Model_Overrides_Sales_Total_Quote_TaxTest extends
 		}
 	}
 
+
+	/**
+	 * @loadFixture base.yaml
+	 * @loadFixture singleShipNotBillVirt.yaml
+	 * @loadFixture calcTaxBefore.yaml
+	 * @loadExpectation taxtest.yaml
+	 */
+	public function testCalcTaxForAddress()
+	{
+		$quote = Mage::getModel('sales/quote')->loadByIdWithoutStore(4);
+		$responseXml = file_get_contents(substr(__FILE__, 0, -4) . '/fixtures/singleShipNotBillVirtRes.xml');
+		$request = Mage::getModel('eb2ctax/request', array('quote' => $quote));
+		$response = Mage::getModel('eb2ctax/response', array('xml' => $responseXml, 'request' => $request));
+		Mage::helper('tax')->getCalculator()->setTaxResponse($response);
+		$address = $quote->getShippingAddress();
+		// set the total_abstract::_address member or else will get a
+		// 'Address model is not defined.' exception thrown.
+		$this->address->setValue($this->tax, $address);
+		$this->calcTaxForAddress->invoke($this->tax, $address);
+	}
+
+	public function testIsFlatShipping()
+	{
+		$addressMock = $this->getModelMock('sales/quote_address', array('getShippingMethod'));
+		$addressMock->expects($this->any())
+			->method('getShippingMethod')
+			->will($this->returnValue('flatrate_flatrate'));
+		$this->assertTrue($this->isFlatShipping->invoke($this->tax, $addressMock));
+	}
+
+	public function testIsFlatShippingIsFalse()
+	{
+		$addressMock = $this->getModelMock('sales/quote_address', array('getShippingMethod'));
+		$addressMock->expects($this->any())
+			->method('getShippingMethod')
+			->will($this->returnValue('someother_shippingrate'));
+		$this->assertFalse($this->isFlatShipping->invoke($this->tax, $addressMock));
+	}
+
+	public function testCollectChildItem()
+	{
+		$productMock = $this->getModelMock('catalog/product', array('isVirtual'));
+		$productMock->expects($this->any())
+			->method('isVirtual')
+			->will($this->returnValue(true));
+
+		$addressMock = $this->getModelMock('sales/quote_address', array('getQuote', 'getAllNonNominalItems', 'getId'));
+		$addressMock->expects($this->any())
+			->method('getId')
+			->will($this->returnValue(1));
+		$addressMock->expects($this->any())
+			->method('getAllNonNominalItems')
+			->will(
+				$this->returnValue(array(
+					$this->_mockChildItem(),
+					$this->_mockParentItem(),
+				))
+			);
+
+		$quoteMock = $this->getModelMock('sales/quote', array('getStore', 'getId', 'getItemsCount', 'getBillingAddress', 'getAllVisibleItems'));
+		$quoteMock->expects($this->any())
+			->method('getStore')
+			->will($this->returnValue(Mage::app()->getStore()));
+
+		$addressMock->expects($this->any())
+			->method('getQuote')
+			->will($this->returnValue($quoteMock));
+
+		$calc = Mage::helper('tax')->getCalculator();
+		/// make sure the mock is setup
+		$this->_mockCalculator();
+		$this->assertSame(10, $calc->getTax(new Varien_Object(array('item' => $itemMock, 'address' => $addressMock))));
+
+		$tax = Mage::getModel('tax/sales_total_quote_tax');
+		$tax->collect($addressMock);
+		// assert the item->getTaxAmount is as expected.
+	}
+
+	public function testCollectNoItems()
+	{
+		// $this->markTestIncomplete('need to add some assertions');
+		$addressMock = $this->getModelMock('sales/quote_address', array('getQuote', 'getAllNonNominalItems', 'getId'));
+		$addressMock->expects($this->any())
+			->method('getId')
+			->will($this->returnValue(1));
+		$addressMock->expects($this->any())
+			->method('getAllNonNominalItems')
+			->will($this->returnValue(array()));
+
+		$quoteMock = $this->getModelMock('sales/quote', array('getStore', 'getId', 'getItemsCount', 'getBillingAddress', 'getAllVisibleItems'));
+		$quoteMock->expects($this->any())
+			->method('getStore')
+			->will($this->returnValue(Mage::app()->getStore()));
+
+		$addressMock->expects($this->any())
+			->method('getQuote')
+			->will($this->returnValue($quoteMock));
+
+		$tax = Mage::getModel('tax/sales_total_quote_tax');
+		$tax->collect($addressMock);
+	}
+
 	protected function _mockCalculator()
 	{
 		$calcMock = $this->getModelMock('tax/calculation', array('getAppliedRates', 'getTax', 'getTaxForAmount'));

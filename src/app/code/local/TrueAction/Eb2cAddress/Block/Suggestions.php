b288d5206a69a37ae1ed5e7593bb59eec610559c
Scott VanBrug <vanbrugs@trueaction.com>
2013-07-22 12:14:44 -0400

    Address validation - remove unused block message

--- src/app/code/local/TrueAction/Eb2cAddress/Block/Suggestions.php
+++ src/app/code/local/TrueAction/Eb2cAddress/Block/Suggestions.php
@@ -17,7 +17,6 @@ class TrueAction_Eb2cAddress_Block_Suggestions extends Mage_Core_Block_Template
 		'suggested_address' => 'Please choose one of the suggestions below.',
 		'suggestion_label' => 'Use this address',
 		'original_label' => 'Use original address',
-		'new_address_box' => 'Add a New Address',
 		'new_label' => 'Use new address',
 	);
 
@@ -159,15 +158,6 @@ class TrueAction_Eb2cAddress_Block_Suggestions extends Mage_Core_Block_Template
 	public function getOriginalAddressLabel()
 	{
 		return $this->_getMessage('original_label');
-	}
-
-	/**
-	 * Get the message to show in the box above the selection for a new address.
-	 * @return string
-	 */
-	public function getNewAddressBoxMessage()
-	{
-		return $this->_getMessage('new_address_box');
 	}
 
 	/**

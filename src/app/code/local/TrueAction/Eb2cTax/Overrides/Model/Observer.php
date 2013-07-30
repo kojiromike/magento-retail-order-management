02d9f2129e066f24586f5e91390551fdd8a8d50a
Scott VanBrug <vanbrugs@trueaction.com>
2013-07-30 11:37:12 -0400

    Eb2cTax - whitespace

--- src/app/code/local/TrueAction/Eb2cTax/Overrides/Model/Observer.php
+++ src/app/code/local/TrueAction/Eb2cTax/Overrides/Model/Observer.php
@@ -16,78 +16,78 @@ class TrueAction_Eb2cTax_Overrides_Model_Observer
 		return $this->_tax;
 	}
 
-    /**
-     * Put quote address tax information into order
-     *
-     * @param Varien_Event_Observer $observer
-     */
-    public function salesEventConvertQuoteAddressToOrder(Varien_Event_Observer $observer)
-    {
-        $address = $observer->getEvent()->getAddress();
-        $order = $observer->getEvent()->getOrder();
+	/**
+	 * Put quote address tax information into order
+	 *
+	 * @param Varien_Event_Observer $observer
+	 */
+	public function salesEventConvertQuoteAddressToOrder(Varien_Event_Observer $observer)
+	{
+		$address = $observer->getEvent()->getAddress();
+		$order = $observer->getEvent()->getOrder();
 
-        $taxes = $address->getAppliedTaxes();
-        if (is_array($taxes)) {
-            if (is_array($order->getAppliedTaxes())) {
-                $taxes = array_merge($order->getAppliedTaxes(), $taxes);
-            }
-            $order->setAppliedTaxes($taxes);
-            $order->setConvertingFromQuote(true);
-        }
-    }
+		$taxes = $address->getAppliedTaxes();
+		if (is_array($taxes)) {
+			if (is_array($order->getAppliedTaxes())) {
+				$taxes = array_merge($order->getAppliedTaxes(), $taxes);
+			}
+			$order->setAppliedTaxes($taxes);
+			$order->setConvertingFromQuote(true);
+		}
+	}
 
-    /**
-     * Save order tax information
-     *
-     * @param Varien_Event_Observer $observer
-     */
-    public function salesEventOrderAfterSave(Varien_Event_Observer $observer)
-    {
-        $order = $observer->getEvent()->getOrder();
+	/**
+	 * Save order tax information
+	 *
+	 * @param Varien_Event_Observer $observer
+	 */
+	public function salesEventOrderAfterSave(Varien_Event_Observer $observer)
+	{
+		$order = $observer->getEvent()->getOrder();
 
-        if (!$order->getConvertingFromQuote() || $order->getAppliedTaxIsSaved()) {
-            return;
-        }
+		if (!$order->getConvertingFromQuote() || $order->getAppliedTaxIsSaved()) {
+			return;
+		}
 
-        $getTaxesForItems   = $order->getQuote()->getTaxesForItems();
-        $taxes              = $order->getAppliedTaxes();
+		$getTaxesForItems   = $order->getQuote()->getTaxesForItems();
+		$taxes              = $order->getAppliedTaxes();
 
-        $ratesIdQuoteItemId = array();
-        if (!is_array($getTaxesForItems)) {
-            $getTaxesForItems = array();
-        }
-        foreach ($getTaxesForItems as $quoteItemId => $taxesArray) {
-            foreach ($taxesArray as $group) {
-                if (count($group['rates']) == 1) {
-                    $ratesIdQuoteItemId[$group['id']][] = array(
-                        'id'        => $quoteItemId,
-                        'percent'   => $group['percent'],
-                        'code'      => $group['rates'][0]['code']
-                    );
-                } else {
-                    $percentDelta   = $group['percent'];
-                    $percentSum     = 0;
-                    foreach ($group['rates'] as $rate) {
-                        $ratesIdQuoteItemId[$group['id']][] = array(
-                            'id'        => $quoteItemId,
-                            'percent'   => $rate['percent'],
-                            'code'      => $rate['code']
-                        );
-                        $percentSum += $rate['percent'];
-                    }
+		$ratesIdQuoteItemId = array();
+		if (!is_array($getTaxesForItems)) {
+			$getTaxesForItems = array();
+		}
+		foreach ($getTaxesForItems as $quoteItemId => $taxesArray) {
+			foreach ($taxesArray as $group) {
+				if (count($group['rates']) == 1) {
+					$ratesIdQuoteItemId[$group['id']][] = array(
+						'id'        => $quoteItemId,
+						'percent'   => $group['percent'],
+						'code'      => $group['rates'][0]['code']
+					);
+				} else {
+					$percentDelta   = $group['percent'];
+					$percentSum     = 0;
+					foreach ($group['rates'] as $rate) {
+						$ratesIdQuoteItemId[$group['id']][] = array(
+							'id'        => $quoteItemId,
+							'percent'   => $rate['percent'],
+							'code'      => $rate['code']
+						);
+						$percentSum += $rate['percent'];
+					}
 
-                    if ($percentDelta != $percentSum) {
-                        $delta = $percentDelta - $percentSum;
-                        foreach ($ratesIdQuoteItemId[$group['id']] as &$rateTax) {
-                            if ($rateTax['id'] == $quoteItemId) {
-                                $rateTax['percent'] = (($rateTax['percent'] / $percentSum) * $delta)
-                                        + $rateTax['percent'];
-                            }
-                        }
-                    }
-                }
-            }
-            // scratch notes
+					if ($percentDelta != $percentSum) {
+						$delta = $percentDelta - $percentSum;
+						foreach ($ratesIdQuoteItemId[$group['id']] as &$rateTax) {
+							if ($rateTax['id'] == $quoteItemId) {
+								$rateTax['percent'] = (($rateTax['percent'] / $percentSum) * $delta)
+										+ $rateTax['percent'];
+							}
+						}
+					}
+				}
+			}
+			// scratch notes
 			// 	$a = array('group_id'=>array( // if the number of rates in a group is 1
 			// 		array(
 			// 			'id'      => 'quote_item_id',
@@ -105,56 +105,56 @@ class TrueAction_Eb2cTax_Overrides_Model_Observer
 			// 	));
 			// 	$percentSum += 'group_rate_percent';
 			// }
-        }
+		}
 
 
 
-        foreach ($taxes as $id => $row) {
-            foreach ($row['rates'] as $tax) {
-                if (is_null($row['percent'])) {
-                    $baseRealAmount = $row['base_amount'];
-                } else {
-                    if ($row['percent'] == 0 || $tax['percent'] == 0) {
-                        continue;
-                    }
-                    $baseRealAmount = $row['base_amount'] / $row['percent'] * $tax['percent'];
-                }
-                $hidden = (isset($row['hidden']) ? $row['hidden'] : 0);
-                $data = array(
-                    'order_id'          => $order->getId(),
-                    'code'              => $tax['code'],
-                    'title'             => $tax['title'],
-                    'hidden'            => $hidden,
-                    'percent'           => $tax['percent'],
-                    'priority'          => $tax['priority'],
-                    'position'          => $tax['position'],
-                    'amount'            => $row['amount'],
-                    'base_amount'       => $row['base_amount'],
-                    'process'           => $row['process'],
-                    'base_real_amount'  => $baseRealAmount,
-                );
+		foreach ($taxes as $id => $row) {
+			foreach ($row['rates'] as $tax) {
+				if (is_null($row['percent'])) {
+					$baseRealAmount = $row['base_amount'];
+				} else {
+					if ($row['percent'] == 0 || $tax['percent'] == 0) {
+						continue;
+					}
+					$baseRealAmount = $row['base_amount'] / $row['percent'] * $tax['percent'];
+				}
+				$hidden = (isset($row['hidden']) ? $row['hidden'] : 0);
+				$data = array(
+					'order_id'          => $order->getId(),
+					'code'              => $tax['code'],
+					'title'             => $tax['title'],
+					'hidden'            => $hidden,
+					'percent'           => $tax['percent'],
+					'priority'          => $tax['priority'],
+					'position'          => $tax['position'],
+					'amount'            => $row['amount'],
+					'base_amount'       => $row['base_amount'],
+					'process'           => $row['process'],
+					'base_real_amount'  => $baseRealAmount,
+				);
 
-                $result = Mage::getModel('tax/sales_order_tax')->setData($data)->save();
+				$result = Mage::getModel('tax/sales_order_tax')->setData($data)->save();
 
-                if (isset($ratesIdQuoteItemId[$id])) {
-                    foreach ($ratesIdQuoteItemId[$id] as $quoteItemId) {
-                        if ($quoteItemId['code'] == $tax['code']) {
-                            $item = $order->getItemByQuoteItemId($quoteItemId['id']);
-                            if ($item) {
-                                $data = array(
-                                    'item_id'       => $item->getId(),
-                                    'tax_id'        => $result->getTaxId(),
-                                    'tax_percent'   => $quoteItemId['percent']
-                                );
-                                Mage::getModel('tax/sales_order_tax_item')->setData($data)->save();
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        $order->setAppliedTaxIsSaved(true);
-    }
+				if (isset($ratesIdQuoteItemId[$id])) {
+					foreach ($ratesIdQuoteItemId[$id] as $quoteItemId) {
+						if ($quoteItemId['code'] == $tax['code']) {
+							$item = $order->getItemByQuoteItemId($quoteItemId['id']);
+							if ($item) {
+								$data = array(
+									'item_id'       => $item->getId(),
+									'tax_id'        => $result->getTaxId(),
+									'tax_percent'   => $quoteItemId['percent']
+								);
+								Mage::getModel('tax/sales_order_tax_item')->setData($data)->save();
+							}
+						}
+					}
+				}
+			}
+		}
+		$order->setAppliedTaxIsSaved(true);
+	}
 
 
 

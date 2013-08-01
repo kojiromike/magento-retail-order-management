5dde8ef10da86dc0b548f5dc3b1d799ede59ff0a
Mike West <westm@trueaction.com>
2013-07-31 20:24:38 -0500

    Fix wrong code accessing event. No test yet.

--- src/app/code/local/TrueAction/Eb2cFraud/Model/Observer.php
+++ src/app/code/local/TrueAction/Eb2cFraud/Model/Observer.php
@@ -4,16 +4,23 @@
  */
 class TrueAction_Eb2cFraud_Model_Observer
 {
-	public function captureOrderContext($event)
+	public function captureOrderContext($observer)
 	{
-		$quote = $event['quote'];
-		$request = $event['request'];
+		$quote = $observer->getEvent->getQuote();
+		$request = $observer->getEvent->getRequest();
 
 		$context = Mage::getModel('eb2cfraud/context');
 
-		$quote->setEb2cFraudJavascriptData($request->getPost('eb2cszyvl',array()));
-		$quote->setEb2cFraudCharSet($context->getCharSet());
-		$quote->setEb2cFraudLanguage($context->getLanguage());
+		$quote->setEb2cFraudJavascriptData( $request->getPost('eb2cszyvl',array()) );
+		$quote->setEb2cFraudCharSet( $context->getCharSet() );
+		$quote->setEb2cFraudLanguage( $context->getLanguage() );
+		$quote->setEb2cFraudHostName( $context->getHostName() );
+		$quote->setEb2cFraudIpAddress( $context->getIpAddress() );
+		$quote->setEb2cFraudSessionId( $context->getSessionId() );
+		$quote->setEb2cFraudUserAgent( $context->getUserAgent() );
+		$quote->setEb2cFraudReferrer( $context->getReferrer() );
+		$quote->setEb2cFraudContentTypes( $context->getContentTypes() );
+		$quote->setEb2cFraudEncoding( $context->getEncoding() );
 
 		$quote->save();
 

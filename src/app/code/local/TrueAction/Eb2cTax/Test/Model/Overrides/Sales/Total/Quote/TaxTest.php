6bd81af00303cf90281167a3fa51b2553a63a05f
Scott VanBrug <vanbrugs@trueaction.com>
2013-08-06 16:11:31 -0400

    Eb2cTax - unregister helper before testCollect and testCollectChildItem tests. Ensures a new helper object is created instead of reusing what appears to be a partially mocked helper or config_registry object.

--- src/app/code/local/TrueAction/Eb2cTax/Test/Model/Overrides/Sales/Total/Quote/TaxTest.php
+++ src/app/code/local/TrueAction/Eb2cTax/Test/Model/Overrides/Sales/Total/Quote/TaxTest.php
@@ -741,6 +741,7 @@ class TrueAction_Eb2cTax_Test_Model_Overrides_Sales_Total_Quote_TaxTest extends 
 
 	public function testCollect()
 	{
+		Mage::unregister('_helper/tax');
 		$items = array(
 			$this->_mockItem(),
 		);
@@ -773,6 +774,7 @@ class TrueAction_Eb2cTax_Test_Model_Overrides_Sales_Total_Quote_TaxTest extends 
 
 	public function testCollectChildItem()
 	{
+		Mage::unregister('_helper/tax');
 		$items = array(
 			$this->_mockChildItem(),
 			$this->_mockItem(),

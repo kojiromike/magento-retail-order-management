ff0e22e2856d63ebdcb7001d5daa8c3f5c0e9891
Michael A. Smith <smithm@trueaction.com>
2013-08-15 16:42:24 -0400

    Fix docblocks to say the right http headers.

--- src/app/code/local/TrueAction/Eb2cFraud/Helper/Http.php
+++ src/app/code/local/TrueAction/Eb2cFraud/Helper/Http.php
@@ -1,28 +1,25 @@
 <?php
-/**
- * 
- */
 class TrueAction_Eb2cFraud_Helper_Http extends Mage_Core_Helper_Http
 {
-    /**
-     * Retrieve HTTP USER AGENT
-     *
-     * @param boolean $clean clean non UTF-8 characters
-     * @return string
-     */
-    public function getHttpAccept($clean = true)
-    {
-        return $this->_getHttpCleanValue('HTTP_ACCEPT', $clean);
-    }
+	/**
+	 * Retrieve HTTP Accept header
+	 * @see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1
+	 * @param boolean $clean clean non UTF-8 characters
+	 * @return string
+	 */
+	public function getHttpAccept($clean = true)
+	{
+		return $this->_getHttpCleanValue('HTTP_ACCEPT', $clean);
+	}
 
-    /**
-     * Retrieve HTTP ACCEPT LANGUAGE
-     *
-     * @param boolean $clean clean non UTF-8 characters
-     * @return string
-     */
-    public function getHttpAcceptEncoding($clean = true)
-    {
-        return $this->_getHttpCleanValue('HTTP_ACCEPT_ENCODING', $clean);
-    }
+	/**
+	 * Retrieve HTTP Accept-Encoding header
+	 * @see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.3
+	 * @param boolean $clean clean non UTF-8 characters
+	 * @return string
+	 */
+	public function getHttpAcceptEncoding($clean = true)
+	{
+		return $this->_getHttpCleanValue('HTTP_ACCEPT_ENCODING', $clean);
+	}
 }

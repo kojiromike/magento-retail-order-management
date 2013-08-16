a56c74b1f47165f3a42d668f72ed761ebad67fd9
Michael Phang <phangm@trueaction.com>
2013-08-16 16:31:13 -0400

    committing changes to the catalog_attribute_set_main block along with notes.

--- src/app/code/local/TrueAction/Eb2cProduct/Overrides/Block/Catalog/Product/Attribute/Set/Main.php
+++ src/app/code/local/TrueAction/Eb2cProduct/Overrides/Block/Catalog/Product/Attribute/Set/Main.php
@@ -9,11 +9,10 @@ class TrueAction_Eb2cProduct_Overrides_Block_Catalog_Product_Attribute_Set_Main
 	 */
 	public function getGroupTreeJson()
 	{
+		Mage::log('getGroupTreeJson called');
 		$items = array();
 		$setId = $this->_getSetId();
 
-/*		git list of default attributes that have a group
-*/
 		/* @var $groups Mage_Eav_Model_Mysql4_Entity_Attribute_Group_Collection */
 		$groups = Mage::getModel('eav/entity_attribute_group')
 			->getResourceCollection()
@@ -24,6 +23,10 @@ class TrueAction_Eb2cProduct_Overrides_Block_Catalog_Product_Attribute_Set_Main
 		$configurable = Mage::getResourceModel('catalog/product_type_configurable_attribute')
 			->getUsedAttributes($setId);
 
+		// get default attribute config as an array
+		// use array keys to get the list of attributes to add
+		// create a temporary group cache
+
 		/* @var $node Mage_Eav_Model_Entity_Attribute_Group */
 		foreach ($groups as $node) {
 			$item = array();
@@ -33,9 +36,6 @@ class TrueAction_Eb2cProduct_Overrides_Block_Catalog_Product_Attribute_Set_Main
 			$item['allowDrop']  = true;
 			$item['allowDrag']  = true;
 
-			/*
-			filter attribute list by group
-			 */
 			$nodeChildren = Mage::getResourceModel('catalog/product_attribute_collection')
 				->setAttributeGroupFilter($node->getId())
 				->addVisibleFilter()
@@ -57,86 +57,26 @@ class TrueAction_Eb2cProduct_Overrides_Block_Catalog_Product_Attribute_Set_Main
 						'is_configurable'   => (int)in_array($child->getAttributeId(), $configurable),
 						'entity_id'         => $child->getEntityAttributeId()
 					);
-					/*
-					if child is in the filtered list of default attributes for the group
-						remove it from the default attributes code list
-					*/
+					// add the group to the cache as $naode->getattributegroupname() => group model
+					// if $child->getEntityTypeId() === attributes->_getDefaultEntityTypeId()
+						// if isset config[$child->getAttributeCode()]
+								// remove it from the default attributes code list
 
 					$item['children'][] = $attr;
 				}
 			}
 			$items[] = $item;
-			/*
-			foreach of the remaining codes in list for this group
-				create and attach the attribute to the attribute set.
-				create array elements to add to the $items array.
-			*/
+
 		}
-
-		return Mage::helper('core')->jsonEncode($items);
-	}
-
-	/**
-	 * Retrieve Unused in Attribute Set Attribute Tree as JSON
-	 *
-	 * @return string
-	 */
-	public function getAttributeTreeJson()
-	{
-		$items = array();
-		$setId = $this->_getSetId();
-
-		/*
-		get list of default attributes that have no group.
-		 */
-
-		$collection = Mage::getResourceModel('catalog/product_attribute_collection')
-			->setAttributeSetFilter($setId)
-			->load();
-
-		$attributesIds = array('0');
-		/* @var $item Mage_Eav_Model_Entity_Attribute */
-		foreach ($collection->getItems() as $item) {
-			$attributesIds[] = $item->getAttributeId();
-		}
-
-		$attributes = Mage::getResourceModel('catalog/product_attribute_collection')
-			->setAttributesExcludeFilter($attributesIds)
-			->addVisibleFilter()
-			->load();
-
-		foreach ($attributes as $child) {
-			/*
-			if child is not in the list
-				remove it
-			 */
-			$attr = array(
-				'text'              => $child->getAttributeCode(),
-				'id'                => $child->getAttributeId(),
-				'cls'               => 'leaf',
-				'allowDrop'         => false,
-				'allowDrag'         => true,
-				'leaf'              => true,
-				'is_user_defined'   => $child->getIsUserDefined(),
-				'is_configurable'   => false,
-				'entity_id'         => $child->getEntityId()
-			);
-			$items[] = $attr;
-		}
-
-		if (count($items) == 0) {
-			$items[] = array(
-				'text'      => Mage::helper('catalog')->__('Empty'),
-				'id'        => 'empty',
-				'cls'       => 'folder',
-				'allowDrop' => false,
-				'allowDrag' => false,
-			);
-		}
-		/*
-		foreach attribute left in the list.
-			add the attribute to the set
-		 */
+		// foreach of the remaining codes in list for this group
+			// create empty attribute model
+			// update model
+				// get default data
+				// update the data array
+					// for each field/value pair in attribute config
+						// map field to data array key
+						// apply value to array element by key
+			// save the model
 		return Mage::helper('core')->jsonEncode($items);
 	}
 }

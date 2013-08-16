312d832b0ca6a10f24d43c11218292404ba62ba7
Michael Phang <phangm@trueaction.com>
2013-08-16 13:20:53 -0400

    added a test class for the setup resource model.

---
+++ src/app/code/local/TrueAction/Eb2cProduct/Test/Model/Resource/Setup.php
@@ -0,0 +1,10 @@
+<?php
+class TrueAction_Eb2cProduct_Test_Model_Resource_Setup
+	extends TrueAction_Eb2cCore_Test_Base
+{
+
+	public function testInstallDefaultAttributes()
+	{
+		$this->markTestIncomplete();
+	}
+}

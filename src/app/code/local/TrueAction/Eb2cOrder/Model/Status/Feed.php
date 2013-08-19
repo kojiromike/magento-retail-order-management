08fa306f5cf1fc5444af736405887a0eafb80993
Mike West <westm@trueaction.com>
2013-08-19 17:55:51 -0400

    Basic process flow is defined for the Feed, and tests exercise all the code that's been written. Very sketchy specs though, so far, so it doesn't really *do* much yet.

--- src/app/code/local/TrueAction/Eb2cOrder/Model/Status/Feed.php
+++ src/app/code/local/TrueAction/Eb2cOrder/Model/Status/Feed.php
@@ -3,10 +3,10 @@
  * Order Status processing Class.
  *	- Gets Order Status feeds from remote
  *	- For each file:
- * 		+ Verify it's in the correct sequence
- * 		+ Apply the specified updates
- * 		+ Note/ Remember which files have been processed
- *		+ Archive / Move to Error folders depending upon success/ failure
+ * 		* Verify it's in the correct sequence
+ * 		* Apply the specified updates
+ * 		* Note/ Remember which files have been processed
+ *		* Archive / Move to Error folders depending upon success/ failure
  */
 class TrueAction_Eb2cOrder_Model_Status_Feed extends Mage_Core_Model_Abstract
 {
@@ -16,6 +16,10 @@ class TrueAction_Eb2cOrder_Model_Status_Feed extends Mage_Core_Model_Abstract
 	private $_feedIo;
 	private $_helper;
 	private $_remoteIo;
+
+	private $_fileInfo;
+	private $_event;
+	private $_flatEvent;
 
 	public function _construct()
 	{
@@ -60,6 +64,7 @@ class TrueAction_Eb2cOrder_Model_Status_Feed extends Mage_Core_Model_Abstract
 	 */
 	public function processFile($xmlFile)
 	{
+		// Load the XML:
 		$dom = new TrueAction_Dom_Document();
 		try {
 			$dom->load($xmlFile);
@@ -68,13 +73,125 @@ class TrueAction_Eb2cOrder_Model_Status_Feed extends Mage_Core_Model_Abstract
 			Mage::logException($e);
 			return false;
 		}
+
+		// Validate Eb2c Header Information:
 		if (!$this->_coreFeedHelper->validateHeader($dom,
 				$this->_config->statusFeedEventType,
 				$this->_config->statusFeedHeaderVersion))
 		{
+			Mage::log('File ' . $xmlFile . ': Invalid header', Zend_Log::ERR);
 			return false;
 		}
+
+		// OrderStatusUpdate is the root nood of each Status Feed file:
+		foreach( $dom->getElementsByTagName('OrderStatusUpdate') as $orderStatusUpdate) {
+			// Load the attributes into the _fileInfo array, not yet well defined, but recordCount, for example, seems useful.
+			$this->_loadFileInfo($orderStatusUpdate, array('fileType', 'fileStartTime', 'fileEndTime', 'recordCount'));
+			// OrderStatusEvents wraps all of our OrderStatusEvents
+			foreach( $dom->getElementsByTagName('OrderStatusEvents') as $eventSets ) {
+				foreach ($eventSets->getElementsByTagName('OrderStatusEvent') as $eventNode ) {
+					$this->_processOneEvent($eventNode);
+				}
+			}
+		}
+
 		// Archive or move to Error here
+		return true;
+	}
+
+
+	/**
+	 * Processes a single eb2c event
+	 *
+	 * @parama eventNode - node containing a single event to process
+	 */
+	private function _processOneEvent($eventNode)
+	{
+		$this->_event = array();			// We aim to flatten an event into an array
+
+		$this->_loadNodes( 
+			$eventNode,
+			array(
+				'OrderStatusEventTimeStamp',
+				'StoreCode',
+				'OrderId',
+				'StatusId',
+				'ProcessTypeKey',
+				'StatusName',
+			)
+		);
+
+		$this->_loadNodes(
+			$eventNode->getElementsByTagName('OrderEventDetail')->item(0),
+			array(
+				'OrderLineId',
+				'ItemId',
+				'Qty',
+			)
+		);
+		$this->_runEventProcessor();				// The name of the method to process an event is based on ProcessTypeKey
+		$this->_fileInfo['recordsProcessed']++;
+	}
+
+	/**
+	 * Adds values to the event array
+	 *
+	 * @param node pointing at some XML parent for which we which to parse the children 
+	 * @tagSet array of tag names from which to get a value
+	 *
+	 */
+	private function _loadNodes($node, $tagSet)
+	{
+		foreach( $tagSet as $tag ) {
+			$this->_event[$tag] = $node->getElementsByTagName($tag)->item(0)->nodeValue;
+		}
+	}
+
+	/**
+	 * Get name of the function used to process this particular event. 
+	 *
+	 * @return string name of method to use to process this event
+	 */
+	private function _runEventProcessor()
+	{
+		$funcName =  '_process' . str_replace(' ','',ucwords(str_replace('_',' ',strtolower($this->_event['ProcessTypeKey']))));
+		if (method_exists($this, $funcName) ) {
+			return $this->$funcName();
+		}
+		else {
+			Mage::log('Error: ' . $funcName . ' is undefined, unprocessed record: ', print_r($this->_event, true));
+			$this->_fileInfo['recordsError']++;
+			return false;
+		}
+	}
+
+
+	/**
+	 * Load File information, taken from the root node's attributes
+	 * 
+	 * @param node pointing at root node of the OrderStatus document
+	 * @attrSet array of attributes from which to get a value
+	 *
+	 */
+	private function _loadFileInfo($node, $attrSet)
+	{
+		$this->_fileInfo = array();
+		foreach($attrSet as $attr ) {
+			$this->_fileInfo[$attr] = $node->getAttribute($attr);
+		}
+		$this->_fileInfo['recordsProcessed'] = 0;
+		$this->_fileInfo['recordsOk'] = 0;
+		$this->_fileInfo['recordsError'] = 0;
+	}
+
+	/**
+	 * Process an Order Fulfillment Event
+	 *
+	 * @return bool  
+	 */
+	private function _processOrderFulfillment()
+	{
+		$this->_fileInfo['recordsOk']++;		// I assume ... ?
 		return true;
 	}
 }

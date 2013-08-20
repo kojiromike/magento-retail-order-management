47a5aa10dbc929e3cb69a6d64b7a2f4793756a8f
Michael A. Smith <smithm@trueaction.com>
2013-08-20 11:35:30 -0400

    intermediate commit - trying to get feedtest to use a mock Varien_Io_File

--- src/app/code/local/TrueAction/Eb2cCore/Test/Model/FeedTest.php
+++ src/app/code/local/TrueAction/Eb2cCore/Test/Model/FeedTest.php
@@ -2,8 +2,8 @@
 class TrueAction_Eb2cCore_Test_Model_FeedTest extends EcomDev_PHPUnit_Test_Case
 {
 	const	TEST_FILE_PFX	=	'TestFeed';
-	const	TEST_XML_DATA =
-'<MessageHeader>
+	const	TEST_XML_DATA = <<<'XML'
+<MessageHeader>
 	<Standard>GSI</Standard>
 	<HeaderVersion>2.3</HeaderVersion>
 	<VersionReleaseNumber>2.3.1</VersionReleaseNumber>
@@ -21,38 +21,58 @@ class TrueAction_Eb2cCore_Test_Model_FeedTest extends EcomDev_PHPUnit_Test_Case
 		<CorrelationId>456789</CorrelationId>
 	</MessageData>
 	<CreateDateAndTime>2012-01-11T12:19:05-06:00</CreateDateAndTime>
-</MessageHeader>';
-	private $_tmpDirName;
-
-	public function setUp()
-	{
-		$this->_tmpDirName = tempnam(Mage::getBaseDir('tmp'), __CLASS__);
-	}
-
-	public function tearDown()
-	{
-		if( strncmp($this->_tmpDirName,'/tmp/tmp',8) ) {
-			throw new Exception( 'Cowardly refusing to recursively remove temp directory with a prefix unknown to me.');
-		}
-		Mage::getModel('eb2ccore/feed')->rmdirRecursive($this->_tmpDirName);
-	}
+</MessageHeader>
+XML;
 
 	/**
 	 * @test
 	 */
 	public function testFeedMethods()
 	{
-		$feed = Mage::getModel('eb2ccore/feed');
-		$feed->setBaseFolder($this->_tmpDirName);
+		$mockFsTool = $this->getMock('Varien_Io_File', array(
+			'cd',
+			'checkAndCreateFolder',
+			'mv',
+			'pwd',
+			'setAllowCreateFolders',
+		));
+		$mockFsTool
+			->expects($this->any())
+			->method('cd')
+			->with($this->identicalTo('foobar'))
+			->will($this->returnValue(true));
+		$mockFsTool
+			->expects($this->any())
+			->method('checkAndCreateFolder')
+			->with($this->identicalTo('wuzzit'))
+			->will($this->returnValue(true));
+		$mockFsTool
+			->expects($this->any())
+			->method('mv')
+			->with($this->logicalAnd($this->identicalTo('foo'), $this->identicalTo('bar')))
+			->will($this->returnValue(true));
+		$mockFsTool
+			->expects($this->any())
+			->method('pwd')
+			->will($this->returnValue('flibleci'));
+		$mockFsTool
+			->expects($this->any())
+			->method('setAllowCreateFolders')
+			->with()
+			->will($this->returnSelf());
 
+		$feed = Mage::getModel('eb2ccore/feed', array(
+			'fs_tool' => $mockFsTool,
+			'base_dir' => Mage::getBaseDir('tmp'),
+		));
 		$fileset = array();
-		for( $i = 0; $i < 4; $i++ ) {
-			$fileset[$i] = $feed->getInboundFolder() . $feed->dirsep() .  self::TEST_FILE_PFX  . $i . '.xml';
-			file_put_contents($fileset[$i],self::TEST_XML_DATA);
+		for ($i = 0; $i < 4; $i++) {
+			$fileset[$i] = $feed->getInboundFolder() . DS . self::TEST_FILE_PFX  . $i . '.xml';
+			file_put_contents($fileset[$i], self::TEST_XML_DATA);
 		}
 
 		// In real-life usage, your would setBaseFolder => run receiver into getInboundFolder() => loop via lsInboundFolder() => mvToXXXFolder.
-		foreach( $feed->lsInboundFolder() as $aFilePath ) {
+		foreach ($feed->lsInboundFolder() as $aFilePath) {
 			$this->assertFileExists($aFilePath);
 		}
 
@@ -60,27 +80,27 @@ class TrueAction_Eb2cCore_Test_Model_FeedTest extends EcomDev_PHPUnit_Test_Case
 		// Test moving a file to outbound
 		$feed->mvToOutboundFolder($fileset[0]);
 		$this->assertFileNotExists($fileset[0]);
-		$this->assertFileExists($feed->getOutboundFolder().$feed->dirsep().basename($fileset[0]));
+		$this->assertFileExists($feed->getOutboundFolder().DS.basename($fileset[0]));
 
 		// Test moving a file to archive
 		$feed->mvToArchiveFolder($fileset[1]);
 		$this->assertFileNotExists($fileset[1]);
-		$this->assertFileExists($feed->getArchiveFolder().$feed->dirsep().basename($fileset[1]));
+		$this->assertFileExists($feed->getArchiveFolder().DS.basename($fileset[1]));
 
 		// Test moving a file to error
 		$feed->mvToErrorFolder($fileset[2]);
 		$this->assertFileNotExists($fileset[2]);
-		$this->assertFileExists($feed->getErrorFolder().$feed->dirsep().basename($fileset[2]));
+		$this->assertFileExists($feed->getErrorFolder().DS.basename($fileset[2]));
 
 		// Test moving a file to tmp; we save to a var name because we're going to test moving it back.
 		$feed->mvToTmpFolder($fileset[3]);
 		$this->assertFileNotExists($fileset[3]);
-		$targetTempFile = $feed->getTmpFolder().$feed->dirsep().basename($fileset[3]);
+		$targetTempFile = $feed->getTmpFolder().DS.basename($fileset[3]);
 		$this->assertFileExists($targetTempFile);
 
 		// Test moving back into the Inbound Folder:
 		$feed->mvToInboundFolder($targetTempFile);
 		$this->assertFileNotExists($targetTempFile);
-		$this->assertFileExists($feed->getInboundFolder().$feed->dirsep().basename($targetTempFile));
+		$this->assertFileExists($feed->getInboundFolder().DS.basename($targetTempFile));
 	}
 }

4069ab070f2f274cf6e0a01ea645f5c1b9909296
Reginald Gabriel <gabrielr@TrueAction.com>
2013-08-22 17:03:54 -0400

    EB2C-PRODUCT-ITEM-MASTER: add new test and seperate feed file with different item content to make test run faster

--- src/app/code/local/TrueAction/Eb2cProduct/Test/Model/Feed/Item/MasterTest.php
+++ src/app/code/local/TrueAction/Eb2cProduct/Test/Model/Feed/Item/MasterTest.php
@@ -163,6 +163,45 @@ class TrueAction_Eb2cProduct_Test_Model_Feed_Item_MasterTest extends EcomDev_PHP
 	}
 
 	/**
+	 * testing processFeeds method - bundle product with invalid product id where operation type is 'Add' and Catalog Class 'nosale'
+	 *
+	 * @test
+	 * @large
+	 * @loadFixture loadConfig.yaml
+	 */
+	public function testProcessFeedsBundleProductAddNosaleWithInvalidProductId()
+	{
+		$mockItemMaster = new TrueAction_Eb2cProduct_Test_Mock_Model_Feed_Item_Master();
+		$mockItemMaster->replaceByMockWithInvalidProductId();
+
+		$master = Mage::getModel('eb2cproduct/feed_item_master');
+
+		$mockHelperObject = new TrueAction_Eb2cProduct_Test_Mock_Helper_Data();
+		$master->setHelper($mockHelperObject->buildEb2cProductHelper());
+
+		$mockCoreModelFeed = new TrueAction_Eb2cProduct_Test_Mock_Model_Core_Feed();
+		$master->setFeedModel($mockCoreModelFeed->buildEb2cCoreModelFeedWithBundleProductsAddNosale());
+
+		$mockCatalogInventoryModelStockItem = new TrueAction_Eb2cProduct_Test_Mock_Model_CatalogInventory_Stock_Item();
+		$master->setStockItem($mockCatalogInventoryModelStockItem->buildCatalogInventoryModelStockItem());
+
+		$mockEavModelConfg = new TrueAction_Eb2cProduct_Test_Mock_Model_Eav_Config();
+		$master->setEavConfig($mockEavModelConfg->buildEavModelConfig());
+
+		$mockEavModelEntityAttribute = new TrueAction_Eb2cProduct_Test_Mock_Model_Eav_Entity_Attribute();
+		$master->setEavEntityAttribute($mockEavModelEntityAttribute->buildEavModelEntityAttribute());
+
+		$mockCalogModelProductTypeConfigurableAttribute = new TrueAction_Eb2cProduct_Test_Mock_Model_Catalog_Product_Type_Configurable_Attribute();
+		$master->setProductTypeConfigurableAttribute($mockCalogModelProductTypeConfigurableAttribute->buildCatalogModelProductTypeConfigurableAttribute());
+
+		// to make the _clean method throw an exception we must mock it
+		$mockCatalogInventoryModelStockStatus = new TrueAction_Eb2cProduct_Test_Mock_Model_CatalogInventory_Stock_Status();
+		$master->setStockStatus($mockCatalogInventoryModelStockStatus->buildCatalogInventoryModelStockStatusWithException());
+
+		$this->assertNull($master->processFeeds());
+	}
+
+	/**
 	 * testing processFeeds method - bundle product with valid product id where operation type is 'Add'
 	 *
 	 * @test
@@ -290,6 +329,41 @@ class TrueAction_Eb2cProduct_Test_Model_Feed_Item_MasterTest extends EcomDev_PHP
 
 		$mockCoreModelFeed = new TrueAction_Eb2cProduct_Test_Mock_Model_Core_Feed();
 		$master->setFeedModel($mockCoreModelFeed->buildEb2cCoreModelFeedWithBundleProductsUpdate());
+
+		$mockCatalogInventoryModelStockItem = new TrueAction_Eb2cProduct_Test_Mock_Model_CatalogInventory_Stock_Item();
+		$master->setStockItem($mockCatalogInventoryModelStockItem->buildCatalogInventoryModelStockItem());
+
+		$mockEavModelConfg = new TrueAction_Eb2cProduct_Test_Mock_Model_Eav_Config();
+		$master->setEavConfig($mockEavModelConfg->buildEavModelConfig());
+
+		$mockEavModelEntityAttribute = new TrueAction_Eb2cProduct_Test_Mock_Model_Eav_Entity_Attribute();
+		$master->setEavEntityAttribute($mockEavModelEntityAttribute->buildEavModelEntityAttribute());
+
+		$mockCalogModelProductTypeConfigurableAttribute = new TrueAction_Eb2cProduct_Test_Mock_Model_Catalog_Product_Type_Configurable_Attribute();
+		$master->setProductTypeConfigurableAttribute($mockCalogModelProductTypeConfigurableAttribute->buildCatalogModelProductTypeConfigurableAttribute());
+
+		$this->assertNull($master->processFeeds());
+	}
+
+	/**
+	 * testing processFeeds method - bundle product with valid product id where operation type is 'Change' Catalog Class 'nosale'
+	 *
+	 * @test
+	 * @large
+	 * @loadFixture loadConfig.yaml
+	 */
+	public function testProcessFeedsBundleProductUpdateNosaleWithValidProductId()
+	{
+		$mockItemMaster = new TrueAction_Eb2cProduct_Test_Mock_Model_Feed_Item_Master();
+		$mockItemMaster->replaceByMockWithValidProductId();
+
+		$master = Mage::getModel('eb2cproduct/feed_item_master');
+
+		$mockHelperObject = new TrueAction_Eb2cProduct_Test_Mock_Helper_Data();
+		$master->setHelper($mockHelperObject->buildEb2cProductHelper());
+
+		$mockCoreModelFeed = new TrueAction_Eb2cProduct_Test_Mock_Model_Core_Feed();
+		$master->setFeedModel($mockCoreModelFeed->buildEb2cCoreModelFeedWithBundleProductsUpdateNosale());
 
 		$mockCatalogInventoryModelStockItem = new TrueAction_Eb2cProduct_Test_Mock_Model_CatalogInventory_Stock_Item();
 		$master->setStockItem($mockCatalogInventoryModelStockItem->buildCatalogInventoryModelStockItem());

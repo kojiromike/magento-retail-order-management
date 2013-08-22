b7193776c566ac24dde0259591de32f05c80c886
Michael Phang <phangm@trueaction.com>
2013-08-22 14:01:03 -0400

    changed the setup model to extend the gatalog entity setup class.

--- src/app/code/local/TrueAction/Eb2cProduct/Model/Resource/Setup.php
+++ src/app/code/local/TrueAction/Eb2cProduct/Model/Resource/Setup.php
@@ -1,34 +1,8 @@
 <?php
-class TrueAction_Eb2cProduct_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup
+class TrueAction_Eb2cProduct_Model_Resource_Setup
+	extends Mage_Catalog_Model_Resource_Eav_Mysql4_Setup
 {
-	/**
-	 * apply default attributes to all existing attribute sets.
-	 * @return $this
-	 */
-	public function installDefaultAttributes()
-	{
-		/*
-		load default attributes config.
-		// load attribute sets
-		// TODO: LOW_PRIORITY figure out filter to not load sets that already have defaults defined
-		//$attrSets = Mage::getModel('eav/entity_attribute_set')->getCollection();
-		// spin through each attribute set and associate the attributes to the attribute set
-		foreach attributeset
-			$this->applyDefaultAttributes($attributeset);
-		*/
-		return $this;
-	}
 
-	/**
-	 * get the attribute set collection.
-	 * @return Mage_Eav_Model_Resource_Entity_Attribute_Set_Collection
-	 */
-	protected function _getAttributeSetCollection()
-	{
-		$collection = Mage::getModel('eav/entity_attribute_set')
-			->getCollection();
-		return $collection;
-	}
 
 	/**
 	 * log an error message

2df6b9933299df8a215ddc8d44eefed4ee52b7dd
Mike West <westm@trueaction.com>
2013-08-27 18:05:45 -0400

    Uses (new optional) baseDir arg. We'll need to revisit this as needed - (it's a config option, really). Echo log messages if it's debug level and our STDOUT is a tty.

--- src/shell/eb2cOrderStatusFeed.php
+++ src/shell/eb2cOrderStatusFeed.php
@@ -5,8 +5,19 @@ require_once('abstract.php');
  */
 class TrueAction_Eb2cOrder_Shell_Status_Feed extends Mage_Shell_Abstract
 {
-	public function _construct()
+	/**
+	 * Log message and (if debug mode and stdout is a tty) echo it (Code review will tell me if echo is OK to do.)
+	 *
+	 * @param string $message to log
+	 * @param log level
+	 */
+	private function _log($message, $level)
 	{
+		$message = '[' . __CLASS__ . '] ' . $message;
+		Mage::log($message, $level);
+		if( $level === Zend_Log::DEBUG && posix_isatty(STDOUT)) {
+			echo $message . "\n";
+		}
 	}
 
 	/**
@@ -30,12 +41,11 @@ USAGE;
 	 */
 	public function run()
 	{
-		$scriptName = basename(__FILE__);
-		echo "Starting $scriptName, " . get_class($this) . '::' . __FUNCTION__ . "\n";
-		Mage::log( "Starting $scriptName, " . get_class($this) . '::' . __FUNCTION__, Zend_log::DEBUG );
-		$filesProcessed = Mage::getModel('eb2corder/status_feed', array('base_dir'=>'/tmp/'.basename(__FILE__,'.php')))->processFeeds();
-		echo  "Finished $scriptName, " . get_class($this) . '::' . __FUNCTION__, " $filesProcessed File(s) Processed\n";
-		Mage::log( "Finished $scriptName, " . get_class($this) . '::' . __FUNCTION__, " $filesProcessed File(s) Processed", Zend_log::DEBUG );
+		$scriptName = basename(__FILE__, '.php');
+		$this->_log( "Starting shell script $scriptName.", Zend_log::DEBUG );
+		$baseDir = Mage::getBaseDir('var') . DS . $scriptName;
+		$filesProcessed = Mage::getModel('eb2corder/status_feed', array('base_dir' => $baseDir))->processFeeds();
+		$this->_log( "Finished shell script $scriptName, $filesProcessed file(s) processed.", Zend_log::DEBUG );
 	}
 }
 $feedProcessor = new TrueAction_Eb2cOrder_Shell_Status_Feed();

e54eb6252032c94e0dd9def53b4edbd2e0c3e043
Michael Phang <phangm@trueaction.com>
2013-08-28 16:15:02 -0400

    removed unused functions and added annotations to ignore coverage for the logwarn and logdebug functions.

--- src/app/code/local/TrueAction/Eb2cProduct/Model/Resource/Eav/Entity/Setup.php
+++ src/app/code/local/TrueAction/Eb2cProduct/Model/Resource/Eav/Entity/Setup.php
@@ -26,7 +26,6 @@ class TrueAction_Eb2cProduct_Model_Resource_Eav_Entity_Setup
 					$message = 'existing attribute with id=\'%s\' will be replaced';
 					$this->_logDebug(sprintf($message, $attrId));
 				}
-
 				$this->addAttribute($entityTypeId, $attrCode, $attrConfig);
 				if ($attrId) {
 					$message = 'existing attribute with id=\'%s\' was replaced';
@@ -37,90 +36,10 @@ class TrueAction_Eb2cProduct_Model_Resource_Eav_Entity_Setup
 		return $this;
 	}
 
-
-	protected function _setCurrentEntityType($entityType)
-	{
-	}
-
-	protected function _setCurrentAttributeCode($attrCode)
-	{
-	}
-
-	/**
-	 * return an attributeset for the product entity type or null
-	 * @param  mixed $attributeSet
-	 * @return Mage_Eav_Model_Entity_Attribute_Set
-	 * @throws Mage_Core_Exception If $attributeSet references an invalid attribute set
-	 */
-	protected function _getAttributeSet($attributeSet = null)
-	{
-		$errorMessage = '';
-		// take either an id or a model.
-		if (!$attributeSet instanceof Mage_Eav_Model_Entity_Attribute_Set) {
-			if (is_int($attributeSet)) {
-				$attributeSet = Mage::getModel('eav/entity_attribute_set')
-					->load($attributeSet);
-			} else {
-				$errorMessage = 'unable to retrieve attribute set "' .
-					(string) $attributeSet .'"';
-				$this->_logWarn($errolrMessage);
-			}
-		}
-		if (!$errorMessage && !$this->_isValidEntityType($attributeSet)) {
-			$errorMessage = 'attribute set is unexpected entity type: typeId(' .
-				$attributeSet->getEntityTypeId() . ')';
-			$this->_logDebug($errorMessage);
-		}
-		if ($errorMessage) {
-			$attributeSet = null;
-		}
-		return $attributeSet;
-	}
-
-	/**
-	 * lookup an attribute by code and entity id.
-	 * @param  string $attributeCode
-	 * @param  int $entityTypeId
-	 * @return Mage_Catalog_Model_Resource_Eav_Attribute
-	 */
-	protected function _lookupAttribute($attributeCode, $entityTypeId)
-	{
-		$attrData = $this->getAttribute($entityTypeId, $attributeCode);
-		return $attr;
-	}
-
-	protected function _checkAttributeGroup($groupName, $attrSetId, $entityTypeId)
-	{
-		$groupData = $this->getAttributeGroup(
-			$entityTypeId,
-			$attrSetId,
-			$groupName
-		);
-		$isGroupSame = isset($groupData['attribute_group_id']) &&
-			$groupData['attribute_group_name'] === $groupName;
-		if (!$isGroupSame) {
-			$message = 'replacing attribute group (%s) with (%s)';
-			$this->logWarn(sprintf(
-				$message,
-				$groupData['attribute_group_name'],
-				$groupName
-			));
-		}
-		return $this;
-	}
-
-	/**
-	 * log an error message
-	 * @param  string $message
-	 */
-	protected function _logError($message)
-	{
-		Mage::log($message, Zend_Log::ERR);
-	}
-
 	/**
 	 * log a warning message
 	 * @param  string $message
+	 * @codeCoverageIgnore
 	 */
 	protected function _logWarn($message)
 	{
@@ -130,6 +49,7 @@ class TrueAction_Eb2cProduct_Model_Resource_Eav_Entity_Setup
 	/**
 	 * log an error message
 	 * @param  string $message
+	 * @codeCoverageIgnore
 	 */
 	protected function _logDebug($message)
 	{

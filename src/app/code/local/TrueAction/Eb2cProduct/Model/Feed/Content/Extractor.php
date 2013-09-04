1bb95fb4d4811dfcd80ef1bc8851fee41e7612ed
Reginald Gabriel <gabrielr@TrueAction.com>
2013-09-04 17:18:30 -0400

    EB2C-PRODUCT: fixing $feedXPath variable

--- src/app/code/local/TrueAction/Eb2cProduct/Model/Feed/Content/Extractor.php
+++ src/app/code/local/TrueAction/Eb2cProduct/Model/Feed/Content/Extractor.php
@@ -9,33 +9,33 @@ class TrueAction_Eb2cProduct_Model_Feed_Content_Extractor extends Mage_Core_Mode
 	/**
 	 * extract UniqueId data into a varien object
 	 *
-	 * @param DOMXPath $feedXpath, the xpath object
+	 * @param DOMXPath $feedXPath, the xpath object
 	 * @param int $contentIndex, the current content position
 	 * @param string $catalogId, the catalog id for the current xml node
 	 *
 	 * @return Varien_Object
 	 */
-	protected function _extractUniqueId(DOMXPath $feedXpath, $contentIndex, $catalogId)
+	protected function _extractUniqueId(DOMXPath $feedXPath, $contentIndex, $catalogId)
 	{
 		// Unique identifier for the item, SKU.
-		$nodeUniqueID = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/UniqueID");
+		$nodeUniqueID = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/UniqueID");
 		return ($nodeUniqueID->length)? (string) $nodeUniqueID->item(0)->nodeValue : null;
 	}
 
 	/**
 	 * extract StyleID data into a varien object
 	 *
-	 * @param DOMXPath $feedXpath, the xpath object
+	 * @param DOMXPath $feedXPath, the xpath object
 	 * @param int $contentIndex, the current content position
 	 * @param string $catalogId, the catalog id for the current xml node
 	 *
 	 * @return Varien_Object
 	 */
-	protected function _extractStyleID(DOMXPath $feedXpath, $contentIndex, $catalogId)
+	protected function _extractStyleID(DOMXPath $feedXPath, $contentIndex, $catalogId)
 	{
 		// The parent SKU, associated with this child item
 		// should be the same as UniqueID if this item doesn't have a parent product.
-		$nodeStyleID = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/StyleID");
+		$nodeStyleID = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/StyleID");
 
 		return ($nodeStyleID->length)? (string) $nodeStyleID->item(0)->nodeValue : null;
 	}
@@ -43,25 +43,25 @@ class TrueAction_Eb2cProduct_Model_Feed_Content_Extractor extends Mage_Core_Mode
 	/**
 	 * extract ProductLinks data into a varien object
 	 *
-	 * @param DOMXPath $feedXpath, the xpath object
+	 * @param DOMXPath $feedXPath, the xpath object
 	 * @param int $contentIndex, the current content position
 	 * @param string $catalogId, the catalog id for the current xml node
 	 *
 	 * @return array, a collection of Varien_Object
 	 */
-	protected function _extractProductLinks(DOMXPath $feedXpath, $contentIndex, $catalogId)
+	protected function _extractProductLinks(DOMXPath $feedXPath, $contentIndex, $catalogId)
 	{
 		$productLinks = array();
 
 		// Contents included if this Content is a link product.
-		$nodeProductLinks = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ProductLinks");
+		$nodeProductLinks = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ProductLinks");
 		if ($nodeProductLinks->length) {
 			// Child Content of this Content
-			$nodeProductLink = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ProductLinks/ProductLink");
+			$nodeProductLink = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ProductLinks/ProductLink");
 			if ($nodeProductLink->length) {
 				$productContentIndex = 1;
 				foreach ($nodeProductLink as $productContent) {
-					$linkToUniqueId = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ProductLinks/ProductLink[$productContentIndex]/LinkToUniqueId");
+					$linkToUniqueId = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ProductLinks/ProductLink[$productContentIndex]/LinkToUniqueId");
 					$productLinks[] = new Varien_Object(
 						array(
 							'link_type' => (string) $productContent->getAttribute('link_type'), // Type of link relationship.
@@ -80,25 +80,25 @@ class TrueAction_Eb2cProduct_Model_Feed_Content_Extractor extends Mage_Core_Mode
 	/**
 	 * extract CategoryLinks data into a varien object
 	 *
-	 * @param DOMXPath $feedXpath, the xpath object
+	 * @param DOMXPath $feedXPath, the xpath object
 	 * @param int $contentIndex, the current content position
 	 * @param string $catalogId, the catalog id for the current xml node
 	 *
 	 * @return array, a collection of Varien_Object
 	 */
-	protected function _extractCategoryLinks(DOMXPath $feedXpath, $contentIndex, $catalogId)
+	protected function _extractCategoryLinks(DOMXPath $feedXPath, $contentIndex, $catalogId)
 	{
 		$categoryLinks = array();
 
 		// Link the product into categories.
-		$nodeCategoryLinks = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/CategoryLinks");
+		$nodeCategoryLinks = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/CategoryLinks");
 		if ($nodeCategoryLinks->length) {
 			// Child Content of this Content
-			$nodeCategoryLink = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/CategoryLinks/CategoryLink");
+			$nodeCategoryLink = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/CategoryLinks/CategoryLink");
 			if ($nodeCategoryLink->length) {
 				$categoryContentIndex = 1;
 				foreach ($nodeCategoryLink as $categoryContent) {
-					$categoryName = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/CategoryLinks/CategoryLink[$categoryContentIndex]/Name");
+					$categoryName = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/CategoryLinks/CategoryLink[$categoryContentIndex]/Name");
 					$categoryLinks[] = new Varien_Object(
 						array(
 							'default' => (bool) $categoryContent->getAttribute('default'), // if category is the default
@@ -118,21 +118,21 @@ class TrueAction_Eb2cProduct_Model_Feed_Content_Extractor extends Mage_Core_Mode
 	/**
 	 * extract BaseAttributes data into a varien object
 	 *
-	 * @param DOMXPath $feedXpath, the xpath object
+	 * @param DOMXPath $feedXPath, the xpath object
 	 * @param int $contentIndex, the current content position
 	 * @param string $catalogId, the catalog id for the current xml node
 	 *
 	 * @return array, a collection of Varien_Object
 	 */
-	protected function _extractBaseAttributes(DOMXPath $feedXpath, $contentIndex, $catalogId)
+	protected function _extractBaseAttributes(DOMXPath $feedXPath, $contentIndex, $catalogId)
 	{
 		$baseAttributes = array();
 
 		// Link the product into categories.
-		$nodeBaseAttributes = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/BaseAttributes");
+		$nodeBaseAttributes = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/BaseAttributes");
 		if ($nodeBaseAttributes->length) {
 			// Child Content of this Content
-			$nodeTitle = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/BaseAttributes/Title");
+			$nodeTitle = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/BaseAttributes/Title");
 			if ($nodeTitle->length) {
 				foreach ($nodeTitle as $titleContent) {
 					$baseAttributes[] = new Varien_Object(
@@ -151,21 +151,21 @@ class TrueAction_Eb2cProduct_Model_Feed_Content_Extractor extends Mage_Core_Mode
 	/**
 	 * extract ExtendedAttributes data into a varien object
 	 *
-	 * @param DOMXPath $feedXpath, the xpath object
+	 * @param DOMXPath $feedXPath, the xpath object
 	 * @param int $contentIndex, the current content position
 	 * @param string $catalogId, the catalog id for the current xml node
 	 *
 	 * @return array, a collection of Varien_Object
 	 */
-	protected function _extractExtendedAttributes(DOMXPath $feedXpath, $contentIndex, $catalogId)
+	protected function _extractExtendedAttributes(DOMXPath $feedXPath, $contentIndex, $catalogId)
 	{
 		$extendedAttributes = array();
 
 		// Link the product into categories.
-		$nodeExtendedAttributes = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes");
+		$nodeExtendedAttributes = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes");
 		if ($nodeExtendedAttributes->length) {
 			// extract Gift Wrap attributes
-			$nodeGiftWrap = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/GiftWrap");
+			$nodeGiftWrap = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/GiftWrap");
 			$extendedAttributes['gift_wrap'] = new Varien_Object(
 				array(
 					'gift_wrap' => ($nodeGiftWrap->length)? (string) $nodeGiftWrap->item(0)->nodeValue : null, // Can this item be gift wrapped? ("Y", "N")
@@ -173,11 +173,11 @@ class TrueAction_Eb2cProduct_Model_Feed_Content_Extractor extends Mage_Core_Mode
 			);
 
 			// extract color attributes
-			$nodeColor = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/ColorAttributes/Color");
+			$nodeColor = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/ColorAttributes/Color");
 			if ($nodeColor->length) {
-				$nodeCode = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/ColorAttributes/Color/Code");
-				$nodeDescription = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/ColorAttributes/Color/Description");
-				$nodeSequence = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/ColorAttributes/Color/Sequence");
+				$nodeCode = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/ColorAttributes/Color/Code");
+				$nodeDescription = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/ColorAttributes/Color/Description");
+				$nodeSequence = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/ColorAttributes/Color/Sequence");
 
 				$colorDescriptionCollection = array();
 				if ($nodeDescription->length) {
@@ -200,7 +200,7 @@ class TrueAction_Eb2cProduct_Model_Feed_Content_Extractor extends Mage_Core_Mode
 			}
 
 			// extract long description attributes
-			$nodeLongDescription = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/LongDescription");
+			$nodeLongDescription = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/LongDescription");
 			if ($nodeLongDescription->length) {
 				foreach ($nodeLongDescription as $attributeLongContent) {
 					$extendedAttributes['long_description'][] = new Varien_Object(
@@ -213,7 +213,7 @@ class TrueAction_Eb2cProduct_Model_Feed_Content_Extractor extends Mage_Core_Mode
 			}
 
 			// extract short description attributes
-			$nodeShortDescription = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/ShortDescription");
+			$nodeShortDescription = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/ShortDescription");
 			if ($nodeShortDescription->length) {
 				foreach ($nodeShortDescription as $attributeShortContent) {
 					$extendedAttributes['short_description'][] = new Varien_Object(
@@ -226,7 +226,7 @@ class TrueAction_Eb2cProduct_Model_Feed_Content_Extractor extends Mage_Core_Mode
 			}
 
 			// extract short SearchKeywords attributes
-			$nodeSearchKeywords = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/SearchKeywords");
+			$nodeSearchKeywords = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/ExtendedAttributes/SearchKeywords");
 			if ($nodeSearchKeywords->length) {
 				foreach ($nodeSearchKeywords as $attributeSearchContent) {
 					$extendedAttributes['search_keywords'][] = new Varien_Object(
@@ -245,25 +245,25 @@ class TrueAction_Eb2cProduct_Model_Feed_Content_Extractor extends Mage_Core_Mode
 	/**
 	 * extract CustomAttributes data into a varien object
 	 *
-	 * @param DOMXPath $feedXpath, the xpath object
+	 * @param DOMXPath $feedXPath, the xpath object
 	 * @param int $contentIndex, the current content position
 	 * @param string $catalogId, the catalog id for the current xml node
 	 *
 	 * @return array, a collection of Varien_Object
 	 */
-	protected function _extractCustomAttributes(DOMXPath $feedXpath, $contentIndex, $catalogId)
+	protected function _extractCustomAttributes(DOMXPath $feedXPath, $contentIndex, $catalogId)
 	{
 		$customAttributes = array();
 
 		// List of additional attributes that may be used by the client system/Magento.
-		$nodeCustomAttributes = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/CustomAttributes");
+		$nodeCustomAttributes = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/CustomAttributes");
 		if ($nodeCustomAttributes->length) {
 			// attribute list
-			$nodeAttribute = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/CustomAttributes/Attribute");
+			$nodeAttribute = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/CustomAttributes/Attribute");
 			if ($nodeAttribute->length) {
 				$attributeContentIndex = 1;
 				foreach ($nodeAttribute as $customContent) {
-					$nodeValue = $feedXpath->query("//Content[$contentIndex][@catalog_id='$catalogId']/CustomAttributes/Attribute[$attributeContentIndex]/Value");
+					$nodeValue = $feedXPath->query("//Content[$contentIndex][@catalog_id='$catalogId']/CustomAttributes/Attribute[$attributeContentIndex]/Value");
 					$customAttributes[] = new Varien_Object(
 						array(
 							'name' => (string) $customContent->getAttribute('name'), // Custom attribute name.
@@ -290,9 +290,9 @@ class TrueAction_Eb2cProduct_Model_Feed_Content_Extractor extends Mage_Core_Mode
 	public function extractContentMasterFeed(DOMDocument $doc)
 	{
 		$collectionOfContents = array();
-		$feedXpath = new DOMXPath($doc);
+		$feedXPath = new DOMXPath($doc);
 
-		$nodeContent = $feedXpath->query('//Content');
+		$nodeContent = $feedXPath->query('//Content');
 		$contentIndex = 1; // start index
 		foreach ($nodeContent as $content) {
 			// setting catalog id
@@ -304,13 +304,13 @@ class TrueAction_Eb2cProduct_Model_Feed_Content_Extractor extends Mage_Core_Mode
 					'catalog_id' => $catalogId, // Catalog ID of the client or shared catalog.
 					'gsi_client_id' => (string) $content->getAttribute('gsi_client_id'), // Client ID assigned by GSI
 					'gsi_store_id' => (string) $content->getAttribute('gsi_store_id'), // Client store/channel.
-					'unique_id' => $this->_extractUniqueId($feedXpath, $contentIndex, $catalogId), // Unique identifier for the item, SKU.
-					'style_id' => $this->_extractStyleID($feedXpath, $contentIndex, $catalogId), // the parent sku related to the this item
-					'product_links' => $this->_extractProductLinks($feedXpath, $contentIndex, $catalogId), // List of related products.
-					'category_links' => $this->_extractCategoryLinks($feedXpath, $contentIndex, $catalogId), // Link the product into categories.
-					'base_attributes' => $this->_extractBaseAttributes($feedXpath, $contentIndex, $catalogId), // base product attributes (name/title)
-					'extended_attributes' => $this->_extractExtendedAttributes($feedXpath, $contentIndex, $catalogId), // Attributes known to eb2c.
-					'custom_attributes' => $this->_extractCustomAttributes($feedXpath, $contentIndex, $catalogId), // additional attributes
+					'unique_id' => $this->_extractUniqueId($feedXPath, $contentIndex, $catalogId), // Unique identifier for the item, SKU.
+					'style_id' => $this->_extractStyleID($feedXPath, $contentIndex, $catalogId), // the parent sku related to the this item
+					'product_links' => $this->_extractProductLinks($feedXPath, $contentIndex, $catalogId), // List of related products.
+					'category_links' => $this->_extractCategoryLinks($feedXPath, $contentIndex, $catalogId), // Link the product into categories.
+					'base_attributes' => $this->_extractBaseAttributes($feedXPath, $contentIndex, $catalogId), // base product attributes (name/title)
+					'extended_attributes' => $this->_extractExtendedAttributes($feedXPath, $contentIndex, $catalogId), // Attributes known to eb2c.
+					'custom_attributes' => $this->_extractCustomAttributes($feedXPath, $contentIndex, $catalogId), // additional attributes
 				)
 			);
 

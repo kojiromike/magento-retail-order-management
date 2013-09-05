8442faa77850c5c72cf93fb8f13f118ccac66c25
Michael Phang <mphang@mpvm.(none)>
2013-09-05 13:13:29 -0400

    fixed expected exception annotation.

--- src/app/code/local/TrueAction/Eb2cTax/Test/Helper/Overrides/DataTest.php
+++ src/app/code/local/TrueAction/Eb2cTax/Test/Helper/Overrides/DataTest.php
@@ -110,7 +110,7 @@ class TrueAction_Eb2cTax_Test_Helper_Overrides_DataTest
 
 	/**
 	 * @test
-	 * @expectedException Mage_Core_Exception
+	 * @expectedException Exception
 	 * @loadFixture sendRequestConfig.yaml
 	 */
 	public function testSendRequestWithExceptionThrown()

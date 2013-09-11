7b169827c6336bdd1450b3543ffa20d73c5920b8
Mike West <micwest@ebay.com>
2013-09-11 16:33:38 -0400

    EBC-59 Accientally had moved FeedTest.php into Feed subdirectory, moved it back out. Added comment to Feed/Interface.php

--- src/app/code/local/TrueAction/Eb2cCore/Model/Feed/Interface.php
+++ src/app/code/local/TrueAction/Eb2cCore/Model/Feed/Interface.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * A common interface for all eb2c classes which process feeds. 
+ * Note that phpcs insisted I indent the first brace '{'. As this file is so small and singular,
+ * I complied, but may be tempted later to figure out why phpcs wanted it this way.
  */
 interface TrueAction_Eb2cCore_Model_Feed_Interface
 	{

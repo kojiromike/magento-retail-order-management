f7fdf51d4c0c060fad7ed33626ac1603ec9a4fc1
Michael Phang <mphang@ebay.com>
2013-09-25 18:14:22 -0400

    started adding extra test functions.
    added a test for _getProductBySku.
    added the beginnings of an integration test for processFeeds.

--- src/app/code/local/TrueAction/Eb2cProduct/Test/Model/Feed/Item/PricingTest.php
+++ src/app/code/local/TrueAction/Eb2cProduct/Test/Model/Feed/Item/PricingTest.php
@@ -109,20 +109,77 @@ class TrueAction_Eb2cProduct_Test_Model_Feed_Item_PricingTest
 	}
 
 	/**
-	 * verify if product doesnt exit, dummy product is created
-	 * verify product prices are set.
-	 * verify last pricing event is used to update the product
-	 * verify item added to queue
+	 * verify the correct pricing information is written to the product
 	 * @loadExpectation
 	 * @dataProvider dataProvider
 	 */
-	/*
-
-	public function testProcessDom($expectation, $xml)
+	public function testGetProductBySku($expectation, $productId)
 	{
-		$this->markTestSkipped();
+		$product = $this->getModelMock('catalog/product', array(
+			'getId',
+		));
+		$product->expects($this->any())
+			->method('getId')
+			->will($this->returnValue($productId));
+
+		$collection = $this->getResourceModelMock('catalog/product_collection', array(
+			'addAttributeToSelect',
+			'getSelect',
+			'where',
+			'load',
+			'getFirstItem',
+		));
+		$collection->expects($this->any())
+			->method('addAttributeToSelect')
+			->will($this->returnSelf());
+		$collection->expects($this->any())
+			->method('getSelect')
+			->will($this->returnSelf());
+		$collection->expects($this->any())
+			->method('where')
+			->will($this->returnSelf());
+		$collection->expects($this->any())
+			->method('load')
+			->will($this->returnSelf());
+		$collection->expects($this->any())
+			->method('getFirstItem')
+			->will($this->returnValue(array($product)));
+
+		$model = $this->getModelMock('eb2cproduct/feed_item_pricing', array(
+			'getDefaultAttributeSetId',
+			'_getDefaultCategotyIds',
+			'getWebsiteIds',
+		));
+		$collection->expects($this->any())
+			->method('getDefaultAttributeSetId')
+			->will($this->returnValue(10));
+		$collection->expects($this->any())
+			->method('_getDefaultCategotyIds')
+			->will($this->returnValue(array(1, 2, 3)));
+		$collection->expects($this->any())
+			->method('getWebsiteIds')
+			->will($this->returnValue(array(1, 2)));
+
+		$this->_reflectMethod($model, '_loadProductBySku')->invoke($model, 'somesku');
+		$e = $this->expected($expectation);
+		$this->assertNotNull($itemData->getClientItemId());
+	}
+
+	/**
+	 * @large
+	 * @loadExpectation
+	 * @loadFixture
+	 * @dataProvider dataProvider
+	 */
+	public function testProcessFeedsIntegration($expectation, $xml)
+	{
 		$vfs = $this->getFixture()->getVfs();
 		$vfs->apply(array('var' => array('eb2c' => array('foo.txt' => $xml))));
+		$this->replaceCoreConfigRegistry(array(
+			'clientId' => 'MAGTNA',
+			'catalogId' => '45',
+			'gsiClientId' => 'MAGTNA',
+		));
 		$feedModel = $this->getModelMock('eb2ccore/feed', array(
 			'lsInboundDir',
 			'mvToArchiveDir',
@@ -143,91 +200,15 @@ class TrueAction_Eb2cProduct_Test_Model_Feed_Item_PricingTest
 			->will($this->returnValue(true));
 		$this->replaceByMock('helper', 'eb2ccore/feed', $coreFeedHelper);
 
-		$model = $this->getModelMock('eb2cproduct/feed_item_pricing', array(
-			'_getProductBySku',
-			'_clean',
-			'_processDom',
-			'_processQueue',
-		));
-		$model->expects($this->once())
-			->method('_processDom')
-			->will($this->returnSelf());
-		$model->expects($this->once())
-			->method('_processQueue')
-			->will($this->returnSelf());
-		$model->expects($this->any())
-			->method('_clean')
-			->will($this->returnSelf());
+		$model = Mage::getModel('eb2cproduct/feed_item_pricing');
 		$model->setFeedModel($feedModel);
 		$model->processFeeds();
-	}
-	*/
-	/**
-	 * verify the correct pricing information is written to the product
-	 * @loadExpectation
-	 * @dataProvider dataProvider
-	 */
-	public function testProcessItem($expectation, $data, $productId)
-	{
-		$this->markTestSkipped();
-		$product = $this->getModelMock('catalog/product', array(
-			'setBasePrice',
-			'setMSRP',
-			'setSpecialPrice',
-			'setSpecialFromDate',
-			'setSpecialToDate',
-			'setPriceIsVatInclusive',
-			'save',
-			'getId',
-		));
-
 		$e = $this->expected($expectation);
-		$product->expects($this->atLeastOnce())
-			->method('setBasePrice')
-			->with($this->identicalTo($e->getPrice()))
-			->will($this->returnSelf());
-		$product->expects($this->atLeastOnce())
-			->method('setMsrp')
-			->with($this->identicalTo($e->getMsrp()))
-			->will($this->returnSelf());
-		$product->expects($this->any())
-			->method('getId')
-			->will($this->returnValue($productId));
-		$product->expects($this->any())
-			->method('setSpecialPrice')
-			->with($this->identicalTo($e->getSpecialPrice()))
-			->will($this->returnSelf());
-		$product->expects($this->any())
-			->method('setSpecialFromDate')
-			->with($this->identicalTo($e->getSpecialFromDate()))
-			->will($this->returnSelf());
-		$product->expects($this->any())
-			->method('setSpecialToDate')
-			->with($this->identicalTo($e->getSpecialToDate()))
-			->will($this->returnSelf());
-		$product->expects($this->any())
-			->method('setPriceIsVatInclusive')
-			->with($this->identicalTo($e->getPriceIsVatInclusive()))
-			->will($this->returnSelf());
-		$product->expects($this->any())
-			->method('save')
-			->will($this->returnSelf());
-		$this->replaceByMock('model', 'catalog/product', $product);
-
-		$model = $this->getModelMock('eb2cproduct/feed_item_pricing', array(
-			'_loadProductBySku',
-			'applyDummyData',
-		));
-		$model->expects($this->any())
-			->method('_loadProductBySku')
-			->with($this->identicalTo($e->getSku()))
-			->will($this->returnValue($product));
-
-		$model->expects($productId ? $this->never() : $this->once())
-			->method('applyDummyData');
-
-		$itemData = new Varien_Object(array_combine($this->keys, $data));
-		$this->assertNotNull($itemData->getClientItemId());
-		$this->_reflectMethod($model, '_processItem')->invoke($model, $itemData);
+		$products = Mage::getResourceModel('catalog/product_collection');
+		$products->addAttributeToSelect('*')
+			->getSelect()
+			->where('e.sku = ?', $e->getSku());
+		$product = $products->getFirstItem();
+		$this->assertEquals($e->getData(), $product->getData());
 	}
 }

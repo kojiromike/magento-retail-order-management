1e4856d2dfbf96c1e85551d327276c689f15ad99
Michael Phang <mphang@ebay.com>
2013-09-27 18:03:59 -0400

    altered the _checkLength function to $string handle being null.

--- src/app/code/local/TrueAction/Eb2cTax/Model/Request.php
+++ src/app/code/local/TrueAction/Eb2cTax/Model/Request.php
@@ -635,13 +635,15 @@ class TrueAction_Eb2cTax_Model_Request extends Mage_Core_Model_Abstract
 	protected function _checkLength($string, $minLength=null, $maxLength=null, $truncate=true)
 	{
 		$result = null;
-		$len = strlen($string);
-		if (is_null($minLength) || $len >= $minLength) {
-			$result = $string;
-		}
-		if ($result && !is_null($maxLength)) {
-			if (($len > $maxLength)) {
-				$result = ($truncate) ? substr($string, 0, $maxLength) : null;
+		if (!is_null($string)) {
+			$len = strlen($string);
+			if (is_null($minLength) || $len >= $minLength) {
+				$result = $string;
+			}
+			if ($result && !is_null($maxLength)) {
+				if (($len > $maxLength)) {
+					$result = ($truncate) ? substr($string, 0, $maxLength) : null;
+				}
 			}
 		}
 		return $result;

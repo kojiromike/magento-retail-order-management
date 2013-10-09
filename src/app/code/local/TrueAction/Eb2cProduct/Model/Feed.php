52ff3cd7574a324bb19ffd48d9183acfe02b636e
Michael Phang <mphang@ebay.com>
2013-10-09 14:59:56 -0400

    add a feed class to be the primary entry point for the product feeds.
    added tests.

---
+++ src/app/code/local/TrueAction/Eb2cProduct/Model/Feed.php
@@ -0,0 +1,209 @@
+<?php
+class TrueAction_Eb2cProduct_Model_Feed
+	extends TrueAction_Eb2cCore_Model_Feed_Abstract
+{
+	const INVALID_EVENT_TYPE = 'the document is missing a valid event type node [%s]';
+	const DOCUMENT_START = 'processing file %s';
+
+	/**
+	 * xpath string to the eventtype node
+	 */
+	const EVENT_TYPE_XPATH = 'MessageHeader/EventType/text()';
+
+	protected $_eventTypes = array(
+		'Price' => 'feed_item_pricing',
+		'ItemMaster' => 'feed_item_master',
+		'Content' => 'feed_content_master',
+	);
+
+	/**
+	 * list of all attribute codes within the set identified by $_attributeCodesSetId
+	 * @var array
+	 */
+	private $_attributeCodes = null;
+
+	/**
+	 * attribute set id of the currently loaded attribute codes
+	 * @var int
+	 */
+	private $_attributeCodesSetId = null;
+
+	/**
+	 * list of attribute codes that are not setup on the system but were in the feed.
+	 * @var array
+	 */
+	private static $_missingAttributes = array();
+
+	protected $_feedFiles = array();
+
+	protected $_eventTypeModel = null;
+
+	protected $_queue = null;
+
+	public function processFeeds()
+	{
+		$filesProcessed = 0;
+		// fetch all files for all feeds.
+		foreach ($this->_eventTypes as $eventType => $modelAlias) {
+			$this->_eventTypeModel = Mage::getSingleton('eb2cproduct/' . $modelAlias);
+
+			// Set up local folders for receiving, processing
+			$coreFeedConstructorArgs = array(
+				'base_dir' => Mage::getBaseDir('var') . DS . $this->getFeedLocalPath()
+			);
+
+			// FileSystem tool can be supplied, esp. for testing
+			if ($this->getFsTool()) {
+				$coreFeedConstructorArgs['fs_tool'] = $this->getFsTool();
+			}
+
+			// Ready to set up the core feed helper, which manages files and directories:
+			$this->_coreFeed = Mage::getModel('eb2ccore/feed', $coreFeedConstructorArgs);
+
+			$this->_coreFeed->fetchFeedsFromRemote($this->getFeedRemotePath(), $this->getFeedFilePattern());
+			$this->_feedFiles = array_merge($this->_feedFiles, $this->_coreFeed->lsInboundDir());
+		}
+		foreach($this->_feedFiles as $xmlFeedFile ) {
+			$this->processFile($xmlFeedFile);
+			$this->_coreFeed->mvToArchiveDir($xmlFeedFile);
+			$filesProcessed++;
+		}
+		return $filesProcessed;
+	}
+
+	/**
+	 * Processes a single xml file.
+	 *
+	 */
+	public function processFile($xmlFile)
+	{
+		$dom = Mage::helper('eb2ccore')->getNewDomDocument();
+		try {
+			$dom->load($xmlFile);
+		}
+		catch(Exception $e) {
+			Mage::logException($e);
+			return;
+		}
+
+		$this->_eventTypeModel = $this->_getEventTypeModel($dom);
+
+		// Set up local folders for receiving, processing
+		$coreFeedConstructorArgs = array(
+			'base_dir' => Mage::getBaseDir('var') . DS . $this->getFeedLocalPath()
+		);
+
+		// FileSystem tool can be supplied, esp. for testing
+		if ($this->getFsTool()) {
+			$coreFeedConstructorArgs['fs_tool'] = $this->getFsTool();
+		}
+
+		// Ready to set up the core feed helper, which manages files and directories:
+		$this->_coreFeed = Mage::getModel('eb2ccore/feed', $coreFeedConstructorArgs);
+
+		// Validate Eb2c Header Information
+		if ( !Mage::helper('eb2ccore/feed')
+			->validateHeader($dom, $this->getFeedEventType() )
+		) {
+			Mage::log('File ' . $xmlFile . ': Invalid header', Zend_Log::ERR);
+			return;
+		}
+		$this->processDom($dom);
+	}
+
+	public function processDom(TrueAction_Dom_Document $doc)
+	{
+		$units = $this->_splitIntoUnits($doc);
+		foreach ($units as $unit) {
+			$operation = $this->getOperationType($unit);
+			$data = $this->_extractData($unit);
+			if ($operation = self::OPERATION_UPSERT) {
+				$this->_queue->add($data);
+			}
+			elseif ($operation = self::OPERATION_REMOVE) {
+				$this->_queue->remove($data);
+			}
+		}
+	}
+
+	public function getFeedConfig()
+	{
+		return $this->_config;
+	}
+
+	public function getFeedEventType()
+	{
+		return $this->_eventTypeModel->getFeedEventType();
+	}
+
+	public function getFeedFilePattern()
+	{
+		return $this->_eventTypeModel->getFeedFilePattern();
+	}
+
+	public function getFeedLocalPath()
+	{
+		return $this->_eventTypeModel->getFeedLocalPath();
+	}
+
+	public function getFeedRemotePath()
+	{
+		return $this->_eventTypeModel->getFeedRemotePath();
+	}
+
+	protected function _extractData($unit)
+	{
+	}
+
+	/**
+	 * @param  array  $attributeList list of attributes we want to exist
+	 * @return array                 subset of $attributeList that actually exist
+	 */
+	private function _getApplicableAttributes(array $attributeList)
+	{
+		$extraAttrs = array_diff($attributeList, self::$_attributeCodes);
+		if ($extraAttrs) {
+			self::$_missingAttributes = array_unique(array_merge(self::$_missingAttributes, $extraAttrs));
+		}
+		return array_intersect($attributeList, self::$_attributeCodes);
+	}
+
+	protected function _getEventTypeModel($doc)
+	{
+		$this->_xpath = new DomXPath($doc);
+		$nodeList = $this->_xpath->query(self::EVENT_TYPE_XPATH, $doc->documentElement);
+		$eventType = null;
+		if ($nodeList->item(0)) {
+			$eventType = $nodeList->item(0)->nodeValue;
+		}
+		if (array_search($eventType, array_keys($this->_eventTypes)) === false) {
+			$message = sprintf(self::INVALID_EVENT_TYPE, $eventType);
+			throw new Mage_Core_Exception($message);
+		}
+		return Mage::getSingleton('eb2cproduct/' . $this->_eventTypes[$eventType]);
+	}
+
+	/**
+	 * load all attribute codes
+	 * @return self
+	 */
+	private function _loadAttributeCodes($product)
+	{
+		if (is_null(self::$_attributeCodes) || self::$_attribeteCodesSetId != $product->getAttributeSetId()) {
+			self::$_attributeCodes = Mage::getSingleton('eav/config')
+				->getEntityAttributeCodes($product->getResource()->getEntityType(), $product);
+		}
+		return $this;
+	}
+
+	protected function _prepareForFeed()
+	{
+		return $this;
+	}
+
+	protected function _construct()
+	{
+		$this->_config = Mage::helper('eb2cproduct')->getConfigModel();
+		$this->_queue = Mage::getSingleton('eb2cproduct/feed_queue');
+	}
+}
---
+++ src/app/code/local/TrueAction/Eb2cProduct/Test/Model/FeedTest.php
@@ -0,0 +1,187 @@
+<?php
+class TrueAction_Eb2cProduct_Test_FeedTest
+	extends TrueAction_Eb2cCore_Test_Base
+{
+	const VFS_ROOT = 'var/eb2c';
+
+	/**
+	 * @loadFixture
+	 * @loadExpectation
+	 * @dataProvider dataProvider
+	 */
+	public function testExtraction($scenario, $feedFile)
+	{
+		$vfs = $this->getFixture()->getVfs();
+		$this->replaceCoreConfigRegistry(array(
+			'clientId' => 'MAGTNA',
+			'catalogId' => '45',
+			'gsiClientId' => 'MAGTNA',
+			'pricingFeedLocalPath' => dirname($feedFile),
+			'pricingFeedRemotePath' => '/',
+			'pricingFeedFilePattern' => '*.xml',
+			'pricingFeedEventType' => 'Price',
+		));
+
+		$filesList = array($vfs->url($feedFile));
+		$coreFeed = $this->getModelMock('eb2ccore/feed', array(
+			'lsInboundDir',
+			'fetchFeedsFromRemote'
+		));
+		$coreFeed->expects($this->atLeastOnce())
+			->method('lsInboundDir')
+			->will($this->returnValue($filesList));
+		$coreFeed->expects($this->atLeastOnce())
+			->method('fetchFeedsFromRemote')
+			->will($this->returnSelf());
+		$this->replaceByMock('model', 'eb2ccore/feed', $coreFeed);
+
+		$e = $this->expected($scenario);
+		$queue = $this->getModelMock('eb2cproduct/feed_queue', array(
+			'addUpsert',
+		));
+		$queue->expects($this->atLeastOnce())
+			->method('addUpsert')
+			->with($this->equalTo($e->getData()));
+		$testModel = $this->getModelMock('eb2cproduct/feed');
+		$testModel->processFeeds();
+	}
+
+	/**
+	 * @dataProvider dataProvider
+	 * @loadFixture
+	 */
+	public function testFeedModelSelection($feedFile, $model)
+	{
+		$this->replaceCoreConfigRegistry(array(
+			'clientId' => 'MAGTNA',
+			'catalogId' => '45',
+			'gsiClientId' => 'MAGTNA',
+		));
+		$vfs = $this->getFixture()->getVfs();
+		$filesList = array($vfs->url($feedFile));
+
+		$feedTypeA = $this->getModelMock('eb2cproduct/feed_item_master', array(
+			'_construct',
+			'getFeedRemotePath',
+			'getFeedLocalPath',
+			'getFeedFilePattern',
+			'getFeedEventType',
+		));
+		$feedTypeA->expects($this->atLeastOnce())
+			->method('getFeedRemotePath')
+			->will($this->returnValue('some/remote/path'));
+		$feedTypeA->expects($this->atLeastOnce())
+			->method('getFeedLocalPath')
+			->will($this->returnValue('vfs/var/eb2c/itemmaster'));
+		$feedTypeA->expects($this->atLeastOnce())
+			->method('getFeedFilePattern')
+			->will($this->returnValue('*.xml'));
+		$feedTypeA->expects($this->any())
+			->method('getFeedEventType')
+			->will($this->returnValue('ItemMaster'));
+		$this->replaceByMock('singleton', 'eb2cproduct/feed_item_master', $feedTypeA);
+
+		$feedTypeB = $this->getModelMock('eb2cproduct/feed_content_master', array(
+			'_construct',
+			'getFeedRemotePath',
+			'getFeedLocalPath',
+			'getFeedFilePattern',
+			'getFeedEventType',
+		));
+		$feedTypeB->expects($this->atLeastOnce())
+			->method('getFeedRemotePath')
+			->will($this->returnValue('some/remote/path'));
+		$feedTypeB->expects($this->atLeastOnce())
+			->method('getFeedLocalPath')
+			->will($this->returnValue('vfs/var/eb2c/contentmaster'));
+		$feedTypeB->expects($this->atLeastOnce())
+			->method('getFeedFilePattern')
+			->will($this->returnValue('*.xml'));
+		$feedTypeB->expects($this->any())
+			->method('getFeedEventType')
+			->will($this->returnValue('Content'));
+		$this->replaceByMock('singleton', 'eb2cproduct/feed_content_master', $feedTypeB);
+
+		$testModel = $this->getModelMock('eb2cproduct/feed', array(
+			'_splitIntoUnits',
+		));
+		$testModel->expects($this->atLeastOnce())
+			->method('_splitIntoUnits')
+			->will($this->returnValue(array()));
+		$coreFeed = $this->getModelMock('eb2ccore/feed', array(
+			'fetchFeedsFromRemote',
+			'lsInboundDir',
+		));
+		$coreFeed->expects($this->atLeastOnce())
+			->method('lsInboundDir')
+			->will($this->returnValue($filesList));
+		$this->replaceByMock('model', 'eb2ccore/feed', $coreFeed);
+		$coreHelper = $this->getHelperMock('eb2ccore/data', array(
+			'validateHeader',
+		));
+		$coreHelper->expects($this->any())
+			->method('validateHeader')
+			->will($this->returnValue(true));
+
+		$testModel->ProcessFeeds();
+		$feedModel = $this->_reflectProperty($testModel, '_eventTypeModel')->getValue($testModel);
+		$this->assertInstanceOf($model, $feedModel);
+	}
+
+	/**
+	 * @loadFixture
+	 * @loadExpectation
+	 */
+	public function testItemMasterFeedIntegration()
+	{
+		$this->markTestIncomplete();
+		$vfs = $this->getFixture()->getVfs();
+		$this->replaceCoreConfigRegistry(array(
+			'itemFeedLocalPath' => 'var/eb2c/itemmaster',
+			'itemFeedRemotePath' => '/',
+			'itemFeedEventType' => 'ItemMaster',
+			'itemFeedFilePattern' => '*.xml',
+		));
+		$coreFeed = $this->getModelMock('eb2ccore/feed');
+		$coreFeed->expects($this->any())
+			->method('fetchFeedsFromRemote')
+			->will($this->returnSelf());
+		$coreFeed->expects($this->any())
+			->method('lsInboundDir')
+			->will($this->returnValue(array()));
+		$this->replaceByMock('model', 'eb2ccore/feed', $coreFeed);
+
+		$testModel = $this->getModelMock('eb2cproduct/feed', array(
+		));
+		$testModel->ProcessFeeds();
+
+		$e = $this->expected('1-1');
+		foreach ($e->getSkus() as $sku) {
+			// check the results
+			$products = Mage::getResourceModel('catalog/product_collection');
+			$products->addAttributeToSelect('*')
+				->getSelect()
+				->where('e.sku = ?', $sku);
+			$product = $products->getFirstItem();
+			$e = $this->expected($sku);
+			$this->assertSame($e->getTypeId(), $product->getTypeId());
+			$this->assertSame($e->getSku(), $product->getSku());
+			$this->assertSame($e->getName(), $product->getName());
+			$this->assertSame($e->getDescription(), $product->getDescription());
+			$this->assertSame($e->getShortDescription(), $product->getShortDescription());
+			$this->assertSame($e->getWeight(), $product->getWeight());
+			$this->assertSame($e->getUrlKey(), $product->getUrlKey());
+			$this->assertEquals($e->getWebsiteIds(), $product->getWebsiteIds());
+			$this->assertEquals($e->getCategoryIds(), $product->getCategoryIds());
+			$this->assertSame($e->getSpecialPrice(), $product->getSpecialPrice());
+			$this->assertSame($e->getSpecialFromDate(), $product->getSpecialFromDate());
+			$this->assertSame($e->getSpecialToDate(), $product->getSpecialToDate());
+			$this->assertSame($e->getPrice(), $product->getPrice());
+			$this->assertSame($e->getMsrp(), $product->getMsrp());
+			$this->assertSame($e->getTaxClassId(), $product->getTaxClassId());
+			$this->assertSame($e->getStatus(), $product->getStatus());
+			$this->assertSame($e->getVisibility(), $product->getVisibility());
+			$this->assertSame($e->getPriceIsVatInclusive(), $product->getPriceIsVatInclusive());
+		}
+	}
+}

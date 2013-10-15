729e837192efa2cb2a1d8af8056661b10288b116
Michael Phang <mphang@ebay.com>
2013-10-15 17:39:40 -0400

    converted the contentmaster.
    uncommented parts of the expectation for itemmaster.

--- src/app/code/local/TrueAction/Eb2cProduct/Model/Feed/Content/Master.php
+++ src/app/code/local/TrueAction/Eb2cProduct/Model/Feed/Content/Master.php
@@ -5,46 +5,110 @@
  * @copyright  Copyright (c) 2013 True Action Network (http://www.trueaction.com)
  */
 class TrueAction_Eb2cProduct_Model_Feed_Content_Master
-	extends Mage_Core_Model_Abstract
-	implements TrueAction_Eb2cCore_Model_Feed_Interface
+	extends TrueAction_Eb2cProduct_Model_Feed_Abstract
 {
-	/**
-	 * Initialize model
-	 */
-	protected function _construct()
+	public function __construct()
 	{
-		// get config
-		$cfg = Mage::helper('eb2cproduct')->getConfigModel();
-
-		// set up base dir if it hasn't been during instantiation
-		if (!$this->hasBaseDir()) {
-			$this->setBaseDir(Mage::getBaseDir('var') . DS . Mage::helper('eb2cproduct')->getConfigModel()->contentFeedLocalPath);
-		}
-
-		// Set up local folders for receiving, processing
-		$coreFeedConstructorArgs = array();
-		$coreFeedConstructorArgs['base_dir'] = $this->getBaseDir();
-		if ($this->hasFsTool()) {
-			$coreFeedConstructorArgs['fs_tool'] = $this->getFsTool();
-		}
-
-		$prod = Mage::getModel('catalog/product');
-		return $this->addData(array(
-			// Magically setting an instantiated extractor object
-			'extractor' => Mage::getModel('eb2cproduct/feed_content_extractor'),
-			'product' => $prod,
-			'stock_status' => Mage::getSingleton('cataloginventory/stock_status'),
-			'category' => Mage::getModel('catalog/category'), // magically setting catalog/category model object
-			'default_store_language_code' => Mage::app()->getLocale()->getLocaleCode(), // setting default store language
-			'default_root_category_id' => $this->_getDefaultParentCategoryId(), // default root category id
-			'default_category_attribute_set_id' => $this->_getCategoryAttributeSetId(), // default category attribute set
-			'default_store_id' => Mage::app()->getWebsite()->getDefaultGroup()->getDefaultStoreId(), // default store id
-			'feed_model' => Mage::getModel('eb2ccore/feed', $coreFeedConstructorArgs),
-			// setting default attribute set id
-			'default_attribute_set_id' => $prod->getResource()->getEntityType()->getDefaultAttributeSetId(),
-			'website_ids' => Mage::getModel('core/website')->getCollection()->getAllIds(),
-		));
+		parent::__construct();
+		$this->_extractors = array(
+			Mage::getModel('eb2cproduct/feed_extractor_xpath', array($this->_extractMap)),
+			Mage::getModel('eb2cproduct/feed_extractor_color', array(
+				array('color' => 'ExtendedAttributes/ColorAttributes/Color'),
+				array('code' => 'Code/text()')
+			)),
+			Mage::getModel('eb2cproduct/feed_extractor_mappinglist', array(
+				array('product_links' => 'ExtendedAttributes/ProductLinks/ProductLink'),
+				array(
+					// Type of link relationship.
+					'link_type' => './@link_type',
+					// Operation to take with the product link. ("Add", "Delete")
+					'operation_type' => './@operation_type',
+					// Unique ID (SKU) for the linked product.
+					'link_to_unique_id' => 'LinkToUniqueId/text()',
+				)
+			)),
+			Mage::getModel('eb2cproduct/feed_extractor_mappinglist', array(
+				array('category_links' => 'ExtendedAttributes/CategoryLinks/CategoryLink'),
+				array(
+					// if category is the default
+					'default' => './@default', // (bool)
+					// Used to link products across catalogs.
+					'catalog_id' => './@catalog_id',
+					// Operation to take with the category.
+					'import_mode' => './@import_mode',
+					// Unique ID (SKU) for the linked product.
+					'name' => 'Name/text()',
+				)
+			)),
+			Mage::getModel('eb2cproduct/feed_extractor_mappinglist', array(
+				array('title' => 'BaseAttributes/Title'),
+				array(
+					// Targeted store language
+					'lang' => './@xml:lang',
+					// Localized product title
+					'title' => './text()',
+				)
+			)),
+			Mage::getModel('eb2cproduct/feed_extractor_color', array(
+				array('color' => 'ExtendedAttributes/ColorAttributes/Color'),
+				array('code' => 'Code/text()')
+			)),
+			Mage::getModel('eb2cproduct/feed_extractor_mappinglist', array(
+				array('long_description' => 'ExtendedAttributes/LongDescription'),
+				array(
+					// Targeted store language
+					'lang' => './@xml:lang',
+					// Localized product title
+					'description' => './text()',
+				)
+			)),
+			Mage::getModel('eb2cproduct/feed_extractor_mappinglist', array(
+				array('short_description' => 'ExtendedAttributes/ShortDescription'),
+				array(
+					// Targeted store language
+					'lang' => './@xml:lang',
+					// short description of the item.
+					'description' => './text()',
+				)
+			)),
+			Mage::getModel('eb2cproduct/feed_extractor_mappinglist', array(
+				array('title' => 'ExtendedAttributes/SearchKeywords'),
+				array(
+					// Targeted store language
+					'lang' => './@xml:lang',
+					// search keywords of the item.
+					'search_keywords' => './text()',
+				)
+			)),
+			Mage::getModel('eb2cproduct/feed_extractor_mappinglist', array(
+				array('title' => 'CustomAttributes/Attribute'),
+				array(
+					// Custom attribute name.
+					'name' => './@name',
+					// Operation to take with the attribute. ("Add", "Change", "Delete")
+					'operation_type' => './@operation_type',
+					// Operation to take with the product link. ("Add", "Delete")
+					'lang' => './@xml:lang',
+					// Unique ID (SKU) for the linked product.
+					'value' => 'Value/text()',
+				)
+			)),
+		);
+		$this->_baseXpath = '/ContentMaster/Content';
+		$this->_feedLocalPath = $this->_config->contentFeedLocalPath;
+		$this->_feedRemotePath = $this->_config->contentFeedRemotePath;
+		$this->_feedFilePattern = $this->_config->contentFeedFilePattern;
+		$this->_feedEventType = $this->_config->contentFeedEventType;
 	}
+
+	protected $_extractMap = array(
+		'gift_wrapping_available' => 'ExtendedAttributes/GiftWrap/text()', // bool
+		'catalog_id' => './@catalog_id',
+		'gsi_client_id' => './@gsi_client_id',
+		'gsi_store_id' => './@gsi_store_id',
+		'unique_id' => 'UniqueID/text()',
+		'style_id' => 'StyleID/text()',
+	);
 
 	/**
 	 * getting category attribute set id.

08010d75fa603e2ce785878700f706a071554b0f
Michael Phang <mphang@ebay.com>
2013-10-17 16:05:36 -0400

    changed the applyDummyData function to use setData instead of addData.

--- src/app/code/local/TrueAction/Eb2cProduct/Helper/Feed.php
+++ src/app/code/local/TrueAction/Eb2cProduct/Helper/Feed.php
@@ -10,27 +10,26 @@ class TrueAction_Eb2cProduct_Helper_Feed
 	public function applyDummyData($product, $sku)
 	{
 		try{
-			$product->setId(null)
-				->addData(
-					array(
-						'type_id' => 'simple', // default product type
-						'visibility' => Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE, // default not visible
-						'attribute_set_id' => $this->getDefaultAttributeSetId(),
-						'name' => 'Invalid Product: ' . $sku,
-						'status' => 0, // default - disabled
-						'sku' => $sku,
-						'website_ids' => $this->_getAllWebsiteIds(),
-						'category_ids' => $this->_getDefaultCategoryIds(),
-						'description' => 'This product is invalid. If you are seeing this product, please do not attempt to purchase and contact customer service.',
-						'short_description' => 'Invalid product. Please do not attempt to purchase.',
-						'price' => 0,
-						'weight' => 0,
-						'url_key' => $sku,
-						'store_ids' => array($this->_getDefaultStoreId()),
-						'stock_data' => array('is_in_stock' => 1, 'qty' => 999, 'manage_stock' => 1),
-						'tax_class_id' => 0,
-					)
-				);
+			$product->setData(
+				array(
+					'type_id' => 'simple', // default product type
+					'visibility' => Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE, // default not visible
+					'attribute_set_id' => $this->getDefaultAttributeSetId(),
+					'name' => 'Invalid Product: ' . $sku,
+					'status' => 0, // default - disabled
+					'sku' => $sku,
+					'website_ids' => $this->_getAllWebsiteIds(),
+					'category_ids' => $this->_getDefaultCategoryIds(),
+					'description' => 'This product is invalid. If you are seeing this product, please do not attempt to purchase and contact customer service.',
+					'short_description' => 'Invalid product. Please do not attempt to purchase.',
+					'price' => 0,
+					'weight' => 0,
+					'url_key' => $sku,
+					'store_ids' => array($this->_getDefaultStoreId()),
+					'stock_data' => array('is_in_stock' => 1, 'qty' => 999, 'manage_stock' => 1),
+					'tax_class_id' => 0,
+				)
+			);
 		} catch (Mage_Core_Exception $e) {
 			Mage::log(
 				sprintf('[ %s ] The following error has occurred while creating dummy product for iShip Feed (%d)',	__CLASS__, $e->getMessage()),

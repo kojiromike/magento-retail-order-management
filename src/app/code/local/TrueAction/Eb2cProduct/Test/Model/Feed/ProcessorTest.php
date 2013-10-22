216336c0fb6b455712e29f381ae9674e1cb02646
Michael Phang <mphang@ebay.com>
2013-10-22 17:27:22 -0400

    rewrote the transformation test to work with the changes the transformed data structure.

--- src/app/code/local/TrueAction/Eb2cProduct/Test/Model/Feed/ProcessorTest.php
+++ src/app/code/local/TrueAction/Eb2cProduct/Test/Model/Feed/ProcessorTest.php
@@ -13,12 +13,62 @@ class TrueAction_Eb2cProduct_Test_Model_ProcessorTest
 	{
 		$e = $this->expected($scenario);
 		$checkData = function($dataObj) use ($e) {
-			foreach ($e->getData() as $key => $value) {
-				PHPUnit_Framework_Assert::assertSame(
-					$value,
-					$dataObj->getData($key),
-					"[$key]"
+			$keys = $e->getData('keys');
+			$rootData = $dataObj->getData();
+			foreach ($keys as $key) {
+				PHPUnit_Framework_Assert::assertArrayHasKey(
+					$key,
+					$rootData,
+					"missing [$key]"
 				);
+			}
+			foreach (array('catalog_id', 'gsi_store_id', 'gsi_client_id') as $key) {
+				PHPUnit_Framework_Assert::assertSame(
+					$e->getData($key),
+					$dataObj->getData($key),
+					"value of [$key] is not as expected"
+				);
+			}
+			$expData = $e->getData('item_id');
+			$actData = $dataObj->getData('item_id');
+			foreach (array_keys($expData) as $key) {
+				PHPUnit_Framework_Assert::assertSame(
+					$expData[$key],
+					$actData->getData($key),
+					"value of [$key] is not as expected"
+				);
+			}
+			$expData = $e->getData('extended_attributes');
+			$actData = $dataObj->getData('extended_attributes');
+			foreach (array_keys($expData) as $key) {
+				PHPUnit_Framework_Assert::assertSame(
+					$expData[$key],
+					$actData->getData($key),
+					"value of [$key] is not as expected"
+				);
+			}
+			if ($e->hasData('color_attributes')) {
+				$expData = $e->getData('color_attributes');
+				$actData = $dataObj->getData('extended_attributes');
+				$actData = $actData['color_attributes'];
+				foreach (array_keys($expData) as $key) {
+					PHPUnit_Framework_Assert::assertSame(
+						$expData[$key],
+						$actData->getData($key),
+						"value of [$key] is not as expected"
+					);
+				}
+			}
+			if ($e->hasData('configurable_attributes')) {
+				$expData = $e->getData('configurable_attributes');
+				$actData = $dataObj->getData('configurable_attributes');
+				foreach (array_keys($expData) as $key) {
+					PHPUnit_Framework_Assert::assertSame(
+						$expData[$key],
+						$actData[$key],
+						"value of [$key] is not as expected"
+					);
+				}
 			}
 		};
 		$testModel = $this->getModelMock('eb2cproduct/feed_processor', array('_synchProduct', '_isAtLimit'));

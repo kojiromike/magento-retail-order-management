d0a82d1ac78322cbd08213a9e82255577ce874bd
Michael Phang <mphang@ebay.com>
2013-10-28 17:38:58 -0400

    EBC-202: added a precondition check to the non default store test.

--- src/app/code/local/TrueAction/Eb2cInventory/Test/Helper/DataTest.php
+++ src/app/code/local/TrueAction/Eb2cInventory/Test/Helper/DataTest.php
@@ -69,6 +69,7 @@ class TrueAction_Eb2cInventory_Test_Helper_DataTest extends TrueAction_Eb2cCore_
 	 */
 	public function testGetOperationUriNonDefaultStore()
 	{
+		$this->assertSame('store_id2', Mage::getStoreConfig('eb2ccore/general/store_id', 'canada'), 'storeid for canada not retrieved');
 		// check to make sure that if the current store has another value for store id,
 		// the store level value is chosen over the default.
 		$this->assertSame(

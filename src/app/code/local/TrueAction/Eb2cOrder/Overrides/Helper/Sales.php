7fe53cbbd329c0f63ccd3efd8e104046c8ae985a
Michael Phang <mphang@ebay.com>
2013-11-21 14:37:12 -0500

    EBC-331 Reenable creating an order in the admin

--- src/app/code/local/TrueAction/Eb2cOrder/Overrides/Helper/Sales.php
+++ src/app/code/local/TrueAction/Eb2cOrder/Overrides/Helper/Sales.php
@@ -1,6 +1,18 @@
 <?php
 class TrueAction_Eb2cOrder_Overrides_Helper_Sales extends Mage_Core_Helper_Data
 {
+	const MAXIMUM_AVAILABLE_NUMBER = Mage_Sales_Helper_Data::MAXIMUM_AVAILABLE_NUMBER;
+
+	/**
+	 * name of the module that is used by ancestor methods.
+	 * @var string
+	 */
+	protected $_moduleName = 'Mage_Sales';
+
+	/**
+	 * list of methods that are overridden.
+	 * @var array
+	 */
 	private static $_overLoadedMethods = array(
 		'canSendNewOrderConfirmationEmail' => true,
 		'canSendNewOrderEmail' => true,
@@ -24,15 +36,10 @@ class TrueAction_Eb2cOrder_Overrides_Helper_Sales extends Mage_Core_Helper_Data
 
 	public function __call($name, $args)
 	{
-		if (isset(self::$_overLoadedMethods[$name])) {
-			if ($this->_config->transactionalEmailer === 'eb2c') {
-				Mage::log("Suppressing email triggered by [{$name}]");
-				return false;
-			} else {
-				return call_user_func_array(array($this->_helper, $name), $args);
-			}
-		} else {
-			return call_user_func_array(array($this->_helper, $name), $args);
+		if ($this->_config->transactionalEmailer === 'eb2c') {
+			Mage::log("Suppressing email triggered by [{$name}]");
+			return false;
 		}
+		return call_user_func_array(array($this->_helper, $name), $args);
 	}
 }

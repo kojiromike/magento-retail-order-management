1f31dca632c2bedd21194c10431ef271eef19ba3
Reginald Gabriel <rgabriel@ebay.com>
2014-03-17 10:03:54 -0400

    DE925 Call Correct Secure3D Method in Admin
    DE925 Fixing Pbridge Error In Admin Order Creation
    
    DE925 Ignoring Unittest Coverage On This Method Because It Return A Hardcode false Value

---
+++ src/app/code/local/TrueAction/Eb2cPayment/Overrides/Block/Adminhtml/Sales/Order/Create/Abstract.php
@@ -0,0 +1,16 @@
+<?php
+class TrueAction_Eb2cPayment_Overrides_Block_Adminhtml_Sales_Order_Create_Abstract
+	extends Enterprise_Pbridge_Block_Adminhtml_Sales_Order_Create_Abstract
+{
+	/**
+	 * @see Enterprise_Pbridge_Block_Adminhtml_Sales_Order_Create_Abstract::is3dSecureEnabled
+	 * in order to resolved issue in pbridge/cc model class where creating order in admin
+	 * is calling a non-existing method in the pbridge class
+	 * @return bool false because we hardcoded to always return false
+	 * @codeCoverageIgnore
+	 */
+	public function is3dSecureEnabled(array $data)
+	{
+		return false;
+	}
+}

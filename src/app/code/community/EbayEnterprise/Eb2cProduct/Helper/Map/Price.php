e6f573d73c15a8d519af44f3b01052877b7e5ff2
Scott VanBrug <svanbrug@ebay.com>
2014-06-11 17:28:49 -0400

    DE1461 Product Price Import
    Add mappings for product pricing related attributes: price,
    special_price, special_from_date, special_to_date and msrp.
    
    Create a price_event model to extract pricing data from price `Event`
    nodes and enforcing business logic for which feed elements to use for
    which price attributes.
    
    Add helper to provide mapping methods for price data. Helper uses the
    new model to retrieve data from the `Event` nodes.

---
+++ src/app/code/community/EbayEnterprise/Eb2cProduct/Helper/Map/Price.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Copyright (c) 2013-2014 eBay Enterprise, Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @copyright   Copyright (c) 2013-2014 eBay Enterprise, Inc. (http://www.ebayenterprise.com/)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class EbayEnterprise_Eb2cProduct_Helper_Map_Price
+{
+	/**
+	 * Extract the product "Price".
+	 * @param  DOMNodeList $nodeList Price feed `Event` node
+	 * @return float
+	 */
+	public function extractPrice(DOMNodeList $nodeList)
+	{
+		return Mage::getModel(
+			'eb2cproduct/price_event',
+			array('event_node' => $nodeList->item(0))
+		)->getPrice();
+	}
+	/**
+	 * Extract the product "Special Price".
+	 * @param  DOMNodeList $nodeList Price feed `Event` node
+	 * @return float|null
+	 */
+	public function extractSpecialPrice(DOMNodeList $nodeList)
+	{
+		return Mage::getModel(
+			'eb2cproduct/price_event',
+			array('event_node' => $nodeList->item(0))
+		)->getSpecialPrice();
+	}
+	/**
+	 * Get the product "Special From Date".
+	 * @param  DOMNodeList $nodeList Price feed `Event` node
+	 * @return string
+	 */
+	public function extractPriceEventFromDate(DOMNodeList $nodeList)
+	{
+		return Mage::getModel(
+			'eb2cproduct/price_event',
+			array('event_node' => $nodeList->item(0))
+		)->getSpecialFromDate();
+	}
+	/**
+	 * Get the product "Sepcial To Date".
+	 * @param  DOMNodeList $nodeList Price feed `Event` node
+	 * @return string
+	 */
+	public function extractPriceEventToDate(DOMNodeList $nodeList)
+	{
+		return Mage::getModel(
+			'eb2cproduct/price_event',
+			array('event_node' => $nodeList->item(0))
+		)->getSpecialToDate();
+	}
+}

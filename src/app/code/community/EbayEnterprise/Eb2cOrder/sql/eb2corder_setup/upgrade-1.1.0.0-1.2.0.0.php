718dd0af606cbd784f0ede8c89c959bc107f9a9f
Scott VanBrug <svanbrug@ebay.com>
2014-06-30 17:20:26 -0400

    DE1491 Extensible Order Increment Prefix
    Remove EAV Entity Type model rewrite.
    
    Create custom increment model for the order entity. Model will handle
    properly prefixing order increment ids with the configured order id
    prefix.
    
    Add a helper method to remove an order id prefix from an increment id.
    
    Correct Eb2cOrder module version.
    
    Have the Eb2cOrder 1.1.0.0-1.2.0.0 upgrade script to also:
    
    - Set the order entity to use the custom increment model.
    - Ensure an eav_entity_store record for orders exists for the
      admin/global level.
    - Set the prefix of the eav_entity_store for orders at the global level
      to not hvae a prefix - set column to NULL.

--- src/app/code/community/EbayEnterprise/Eb2cOrder/sql/eb2corder_setup/upgrade-1.1.0.0-1.2.0.0.php
+++ src/app/code/community/EbayEnterprise/Eb2cOrder/sql/eb2corder_setup/upgrade-1.1.0.0-1.2.0.0.php
@@ -14,15 +14,38 @@
  */
 
 /**
- * Upgrade for Eb2cOrder
- * Disables `increment_per_store` setting for the
- * sales/order "entity"
+ * Update the order entity to share increment ids across all stores and use
+ * a custom increment model for generating entity increment ids
  */
 
-Mage::log(sprintf('[%s] Upgrading Eb2cOrder 1.1.0.0 -> 1.2.0.0', __CLASS__), Zend_Log::INFO);
-// use the sales setup model to make it much easier to manage the EAV entity
+// use the sales setup model to make it easier to manage the EAV entity
 // type table
 $installer = Mage::getResourceModel('sales/setup', 'sales_setup');
 $installer->startSetup();
+// set the order entity to use a single pool of increment ids - increment_per_store
+// set to false (prevents duplicate increment ids when multiple stores use same
+// client order id prefix) - and a custom increment model to add the configured
+// client order id prefixes - increment_model set to alias of the desired model.
 $installer->updateEntityType(Mage_Sales_Model_Order::ENTITY, 'increment_per_store', false);
+$installer->updateEntityType(Mage_Sales_Model_Order::ENTITY, 'increment_model', 'eb2corder/eav_entity_increment_order');
+
+/*
+ * update the eav_entity_store table to not include a prefix for the
+ * admin store as it is not used for anything in the custom increment model
+ */
+$orderTypeId = Mage::getSingleton('eav/config')->getEntityType(Mage_Sales_Model_Order::ENTITY)->getId();
+// when not being incremented by store, entity store table uses the admin store
+$adminId = Mage_Core_Model_App::ADMIN_STORE_ID;
+$entityStore = Mage::getModel('eav/entity_store')->loadByEntityStore($orderTypeId, $adminId);
+// if the record doesn't exist yet, fill in necessary data for the record to be saved
+// and associated with the admin store and order entity type
+if (!$entityStore->getId()) {
+	$entityStore->setData(array(
+		'entity_type_id' => $orderTypeId,
+		'store_id' => $adminId,
+	));
+}
+// null out the prefix and save the record
+$entityStore->setIncrementPrefix(null)->save();
+
 $installer->endSetup();

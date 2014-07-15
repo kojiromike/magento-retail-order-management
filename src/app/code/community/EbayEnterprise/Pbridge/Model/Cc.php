faf3a58ca2254a3d1bb8c97cbff652005447a449
Michael A. Smith <msmith3@ebay.com>
2014-07-15 11:20:03 -0400

    Add License Header to Files

--- src/app/code/community/EbayEnterprise/Pbridge/Model/Cc.php
+++ src/app/code/community/EbayEnterprise/Pbridge/Model/Cc.php
@@ -1,5 +1,19 @@
 <?php
 /**
+ * Copyright (c) 2013-2014 eBay Enterprise, Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @copyright   Copyright (c) 2013-2014 eBay Enterprise, Inc. (http://www.ebayenterprise.com/)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
  * Implementation of eBay Enterprise credit card processing via Payment Bridge
  */
 class EbayEnterprise_Pbridge_Model_Cc extends Mage_Payment_Model_Method_Cc

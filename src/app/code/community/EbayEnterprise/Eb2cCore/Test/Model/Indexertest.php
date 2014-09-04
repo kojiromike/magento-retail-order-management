ca8a5cb0b4f8c4bc45b3351dd13794e00eb6fe8c
Michael A. Smith <msmith3@ebay.com>
2014-09-04 18:37:59 +0000

    DE1761 Do Not Log Index Rebuilds
    Conflicts:
    
    	src/app/code/community/EbayEnterprise/Eb2cCore/Model/Indexer.php
    
    Conflicts:
    	src/app/code/community/EbayEnterprise/Eb2cCore/Model/Indexer.php

--- src/app/code/community/EbayEnterprise/Eb2cCore/Test/Model/Indexertest.php
+++ src/app/code/community/EbayEnterprise/Eb2cCore/Test/Model/Indexertest.php
@@ -22,36 +22,17 @@ class EbayEnterprise_Eb2cCore_Test_Model_Indexertest extends EbayEnterprise_Eb2c
 	public function setUp()
 	{
 		// Indexer belongs to a Process
-		$this->_mockOneIndexer = $this->getModelMock(
-			'enterprise_catalogsearch/indexer_fulltext',
-			array(
-				'getName',
-				'isVisible',
-			)
-		);
+		$this->_mockOneIndexer = $this->getModelMock('enterprise_catalogsearch/indexer_fulltext', array('isVisible'));
 		$this->_mockOneIndexer
 			->expects($this->once())
 			->method('isVisible')
 			->will($this->returnValue(true));
-		$this->_mockOneIndexer
-			->expects($this->once())
-			->method('getName')
-			->will($this->returnValue('Mock Catalog Search Index'));
-		$this->replaceByMock(
-			'model',
-			'enterprise_catalogsearch/indexer_fulltext',
-			$this->_mockOneIndexer
-		);
+		$this->replaceByMock('model', 'enterprise_catalogsearch/indexer_fulltext', $this->_mockOneIndexer);
 
-		// A Process is a single instance of index work to be peformed
-		$this->_mockOneProcess = $this->getModelMock(
-			'enterprise_index/process',
-			array(
-				'getIndexer',
-				'getIndexerCode',
-				'reindexEverything',
-			)
-		);
+		// A process is a single instance of index work to be done
+		$this->_mockOneProcess = $this->getModelMockBuilder('enterprise_index/process')
+			->setMethods(array('getIndexer', 'getIndexerCode', 'reindexEverything'))
+			->getMock();
 		$this->_mockOneProcess
 			->expects($this->any())
 			->method('getIndexer')
@@ -64,33 +45,15 @@ class EbayEnterprise_Eb2cCore_Test_Model_Indexertest extends EbayEnterprise_Eb2c
 			->expects($this->once())
 			->method('reindexEverything')
 			->will($this->returnValue(true));
-		$this->replaceByMock(
-			'model',
-			'enterprise_index/process',
-			$this->_mockOneProcess
-		);
+		$this->replaceByMock('model', 'enterprise_index/process', $this->_mockOneProcess);
 
 		// getProcessesCollection returns an associative array of Process
-		$this->_mockEnterpriseIndexer = $this->getModelMock(
-			'enterprise_index/indexer',
-			array(
-				'getProcessesCollection',
-			)
-		);
+		$this->_mockEnterpriseIndexer = $this->getModelMock('enterprise_index/indexer', array('getProcessesCollection'));
 		$this->_mockEnterpriseIndexer
 			->expects($this->any())
 			->method('getProcessesCollection')
-			->will($this->returnValue(
-				array(
-					'catalogsearch_fulltext' => $this->_mockOneProcess,
-				)
-			)
-			);
-		$this->replaceByMock(
-			'model',
-			'enterprise_index/indexer',
-			$this->_mockEnterpriseIndexer
-		);
+			->will($this->returnValue(array('catalogsearch_fulltext' => $this->_mockOneProcess)));
+		$this->replaceByMock('model', 'enterprise_index/indexer', $this->_mockEnterpriseIndexer);
 	}
 
 	/**

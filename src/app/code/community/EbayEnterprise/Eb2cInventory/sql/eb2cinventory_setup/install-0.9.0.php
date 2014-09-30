a4aca187a3c69ebf81705f39c4eff113094fe643
Mike West <micwest@ebay.com>
2014-09-30 17:47:22 -0400

    DE407 Clean up Inventory Setup Script
    * Use config.xml for correct setup class instead of instantiating in script
    * Change the setup of the numerous attributes into a simpler array.

--- src/app/code/community/EbayEnterprise/Eb2cInventory/sql/eb2cinventory_setup/install-0.9.0.php
+++ src/app/code/community/EbayEnterprise/Eb2cInventory/sql/eb2cinventory_setup/install-0.9.0.php
@@ -12,139 +12,40 @@
  * @copyright   Copyright (c) 2013-2014 eBay Enterprise, Inc. (http://www.ebayenterprise.com/)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-
-$installer = new Mage_Sales_Model_Resource_Setup('core_setup');
+/*
+	@var $installer Mage_Sales_Model_Resource_Setup - Uses a Mage_Sales_Model_Resource_Setup because
+	that's the class responsible for setting up and managing each of the entities we are adding to,
+	including doing the right things if there's a "flat_" version of that entity.
+*/
+$installer = $this;
 $installer->startSetup();
 $entities = array('order_item', 'quote_address_item', 'quote_item');
 
 $inventoryAttributes = array(
-	array(
-		'name' => 'reservation_id',
-			'options' => array(
-				'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
-				'visible' => true,
-				'required' => false,
-			)
-	), array(
-		'name' => 'reservation_expires',
-		'options' => array(
-			'type' => Varien_Db_Ddl_Table::TYPE_DATETIME,
-			'visible' => true,
-			'required' => false,
-		)
-	), array(
-		'name' => 'qty_reserved',
-		'options' => array(
-			'type' => Varien_Db_Ddl_Table::TYPE_INTEGER,
-			'visible' => true,
-			'required' => false,
-		)
-	), array(
-		'name' => 'creation_time',
-		'options' => array(
-			'type' => Varien_Db_Ddl_Table::TYPE_DATETIME,
-			'visible' => true,
-			'required' => false,
-		)
-	), array(
-		'name' => 'display',
-		'options' => array(
-			'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
-			'visible' => true,
-			'required' => false,
-		)
-	), array(
-		'name' => 'delivery_window_from',
-		'options' => array(
-			'type' => Varien_Db_Ddl_Table::TYPE_DATETIME,
-			'visible' => true,
-			'required' => false,
-		)
-	), array(
-		'name' => 'delivery_window_to',
-		'options' => array(
-			'type' => Varien_Db_Ddl_Table::TYPE_DATETIME,
-			'visible' => true,
-			'required' => false,
-		)
-	), array(
-			'name' => 'shipping_window_from',
-			'options' => array(
-					'type' => Varien_Db_Ddl_Table::TYPE_DATETIME,
-					'visible' => true,
-					'required' => false,
-			)
-	), array(
-		'name' => 'shipping_window_to',
-		'options' => array(
-			'type' => Varien_Db_Ddl_Table::TYPE_DATETIME,
-			'visible' => true,
-			'required' => false,
-		)
-	), array(
-		'name' => 'ship_from_address_line1',
-		'options' => array(
-			'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
-			'visible' => true,
-			'required' => false,
-		)
-	), array(
-		'name' => 'ship_from_address_line2',
-		'options' => array(
-			'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
-			'visible' => true,
-			'required' => false,
-		)
-	), array(
-		'name' => 'ship_from_address_line3',
-		'options' => array(
-			'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
-			'visible' => true,
-			'required' => false,
-		)
-	), array(
-		'name' => 'ship_from_address_line4',
-		'options' => array(
-			'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
-			'visible' => true,
-			'required' => false,
-		)
-	), array(
-		'name' => 'ship_from_address_city',
-		'options' => array(
-			'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
-			'visible' => true,
-			'required' => false,
-		)
-	), array(
-		'name' => 'ship_from_address_main_division',
-		'options' => array(
-			'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
-			'visible' => true,
-			'required' => false,
-		)
-	), array(
-		'name' => 'ship_from_address_country_code',
-		'options' => array(
-			'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
-			'visible' => true,
-			'required' => false,
-		)
-	), array(
-		'name' => 'ship_from_address_postal_code',
-		'options' => array(
-			'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
-			'visible' => true,
-			'required' => false,
-		)
-	),
+	'reservation_id'                  => Varien_Db_Ddl_Table::TYPE_TEXT,
+	'reservation_expires'             => Varien_Db_Ddl_Table::TYPE_DATETIME,
+	'qty_reserved'                    => Varien_Db_Ddl_Table::TYPE_INTEGER,
+	'creation_time'                   => Varien_Db_Ddl_Table::TYPE_DATETIME,
+	'display'                         => Varien_Db_Ddl_Table::TYPE_TEXT,
+	'delivery_window_from'            => Varien_Db_Ddl_Table::TYPE_DATETIME,
+	'delivery_window_to'              => Varien_Db_Ddl_Table::TYPE_DATETIME,
+	'shipping_window_from'            => Varien_Db_Ddl_Table::TYPE_DATETIME,
+	'shipping_window_to'              => Varien_Db_Ddl_Table::TYPE_DATETIME,
+	'ship_from_address_line1'         => Varien_Db_Ddl_Table::TYPE_TEXT,
+	'ship_from_address_line2'         => Varien_Db_Ddl_Table::TYPE_TEXT,
+	'ship_from_address_line3'         => Varien_Db_Ddl_Table::TYPE_TEXT,
+	'ship_from_address_line4'         => Varien_Db_Ddl_Table::TYPE_TEXT,
+	'ship_from_address_city'          => Varien_Db_Ddl_Table::TYPE_TEXT,
+	'ship_from_address_main_division' => Varien_Db_Ddl_Table::TYPE_TEXT,
+	'ship_from_address_country_code'  => Varien_Db_Ddl_Table::TYPE_TEXT,
+	'ship_from_address_postal_code'   => Varien_Db_Ddl_Table::TYPE_TEXT,
 );
 
-$pfx = 'eb2c_';
-
+$attributePrefix = 'eb2c_';
 foreach ($entities as $entity) {
-	foreach ($inventoryAttributes as $a) {
-		$installer->addAttribute($entity, $pfx . $a['name'], $a['options']);
+	foreach ($inventoryAttributes as $name => $type) {
+		$options = array('type' => $type, 'visible'  => true, 'required' => false);
+		$installer->addAttribute($entity, $attributePrefix . $name, $options);
 	}
 }
 $installer->endSetup();

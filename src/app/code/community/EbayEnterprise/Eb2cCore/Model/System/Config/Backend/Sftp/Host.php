38371754cc28d092dbe051ecd44d402811975fae
Reginald Gabriel <rgabriel@ebay.com>
2015-01-05 10:51:26 -0500

    DE1549 Resolved SFTP Test Button in the Backend
    Add a backend model for the remote host config field
    to ensure trailing white spaces are striped out.
    Remove the trimming of the SFTP URL from the test button
    code.

---
+++ src/app/code/community/EbayEnterprise/Eb2cCore/Model/System/Config/Backend/Sftp/Host.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright (c) 2013-2014 eBay Enterprise, Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @copyright   Copyright (c) 2013-2014 eBay Enterprise, Inc. (http://www.ebayenterprise.com/)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class EbayEnterprise_Eb2cCore_Model_System_Config_Backend_Sftp_Host extends Mage_Core_Model_Config_Data
+{
+	/**
+	 * strip any trailing white space before attempting to save the SFTP Host.
+	 * @return self
+	 */
+	public function _beforeSave()
+	{
+		parent::_beforeSave();
+		$this->setValue(trim($this->getValue()));
+		return $this;
+	}
+}

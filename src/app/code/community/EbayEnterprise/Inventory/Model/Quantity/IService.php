7b3b2238fabbfc252b254b5cec7b681bd6795174
Scott VanBrug <svanbrug@ebay.com>
2015-05-21 09:56:16 -0400

    US35395 Inventory Quantity Service Interface
    Interface for checking if a quote's items are available to be fulfilled
    and if any single quote item is currently available to be fulfilled.

---
+++ src/app/code/community/EbayEnterprise/Inventory/Model/Quantity/IService.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright (c) 2013-2014 eBay Enterprise, Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @copyright   Copyright (c) 2013-2014 eBay Enterprise, Inc. (http://www.ebayenterprise.com/)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+interface EbayEnterprise_Inventory_Model_Quantity_IService
+{
+	/**
+	 * Check the inventory status of each item in the quote. Will add errors
+	 * to the quote and items if any are not currently available at the
+	 * requested quantity. Will throw an exception if any item not yet added
+	 * to the quote should be prevented from being added.
+	 *
+	 * @param Mage_Sales_Model_Quote
+	 * @return self
+	 * @throws EbayEnterprise_Inventory_Exception_Quantity_Unavailable_Exception If any items should not be added to the quote.
+	 */
+	public function checkQuoteInventory(Mage_Sales_Model_Quote $quote);
+
+	/**
+	 * Check if a given item is currently available to be fulfilled.
+	 *
+	 * @param Mage_Sales_Model_Quote_Item_Abstract
+	 * @return bool
+	 */
+	public function isItemAvailable(Mage_Sales_Model_Quote_Item_Abstract $item);
+}

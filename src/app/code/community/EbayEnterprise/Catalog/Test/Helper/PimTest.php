557c7fd9b8e05591eb7a4d8bc2688ebb3b3b4f7a
Michael A. Smith <msmith3@ebay.com>
2015-10-14 10:28:04 -0400

    test

--- src/app/code/community/EbayEnterprise/Catalog/Test/Helper/PimTest.php
+++ src/app/code/community/EbayEnterprise/Catalog/Test/Helper/PimTest.php
@@ -908,6 +908,9 @@ class EbayEnterprise_Catalog_Test_Helper_PimTest extends EbayEnterprise_Eb2cCore
      */
     public function testPassGiftCard($useConfig, $prodAllowMessage, $configAllowMessage, $isVirtual, $allowMessage)
     {
+        if (!class_exists('Enterprise_GiftCard_Model_Giftcard')) {
+            $this->markTestSkipped('Test Requires Enterprise_GiftCard_Model_Giftcard');
+        }
         $doc = Mage::helper('eb2ccore')->getNewDomDocument();
         $doc->loadXML('<root/>');
         $name = 'the giftcard name';
